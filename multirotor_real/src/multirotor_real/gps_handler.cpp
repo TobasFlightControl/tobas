fcd17e6805cc0b7924d251b6a8f61c223bec1235
dohi-navio <masa0u0masa1215@gmail.com>
2023-03-21 10:30:39 +0000

    .

--- multirotor_real/src/multirotor_real/gps_handler.cpp
+++ multirotor_real/src/multirotor_real/gps_handler.cpp
@@ -19,11 +19,12 @@ GpsHandler::GpsHandler(ros::NodeHandle& nh) : cov_received_(false)
 
   if (!gps_.configureSolutionRate(SOLUTION_RATE))
   {
-    dh_ros::rosError("Failed to set solution rate.");
+    throw dh_ros::RuntimeError("Failed to set solution rate.");
   }
 
   string drone_name = dh_ros::getParam<string>("/drone_name");
   gps_pub_ = nh.advertise<GpsMsg>("/" + drone_name + "/gps", 1);
+  vel_pub_ = nh.advertise<VelMsg>("/" + drone_name + "/ground_speed", 1);
 
   timer_ = nh.createTimer(ros::Duration(TIMER_PERIOD), &GpsHandler::timerCb, this);
 }
@@ -43,18 +44,18 @@ void GpsHandler::timerCb(const ros::TimerEvent&)
     gps_msg_.position_covariance[5] = data_[4];  // ED
     gps_msg_.position_covariance[6] = data_[2];  // DN
     gps_msg_.position_covariance[7] = data_[4];  // DE
-    gps_msg_.position_covariance[8] = data_[8];  // DD
+    gps_msg_.position_covariance[8] = data_[5];  // DD
 
     // Update GPS velocity covariance
-    vel_msg_.vel.covariance[0] = data_[0];  // NN
-    vel_msg_.vel.covariance[1] = data_[1];  // NE
-    vel_msg_.vel.covariance[2] = data_[2];  // ND
-    vel_msg_.vel.covariance[3] = data_[1];  // EN
-    vel_msg_.vel.covariance[4] = data_[3];  // EE
-    vel_msg_.vel.covariance[5] = data_[4];  // ED
-    vel_msg_.vel.covariance[6] = data_[2];  // DN
-    vel_msg_.vel.covariance[7] = data_[4];  // DE
-    vel_msg_.vel.covariance[8] = data_[8];  // DD
+    vel_msg_.vel.covariance[0] = data_[6];  // NN
+    vel_msg_.vel.covariance[1] = data_[7];  // NE
+    vel_msg_.vel.covariance[2] = data_[8];  // ND
+    vel_msg_.vel.covariance[3] = data_[7];  // EN
+    vel_msg_.vel.covariance[4] = data_[9];  // EE
+    vel_msg_.vel.covariance[5] = data_[10];  // ED
+    vel_msg_.vel.covariance[6] = data_[8];  // DN
+    vel_msg_.vel.covariance[7] = data_[10];  // DE
+    vel_msg_.vel.covariance[8] = data_[11];  // DD
   }
 
   if (gps_.decodeSingleMessage(Ublox::NAV_PVT, data_) == 1)
