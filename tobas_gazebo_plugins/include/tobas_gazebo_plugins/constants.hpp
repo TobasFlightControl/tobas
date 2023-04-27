029e62d5c2f1325fa8b92564eeecb5e7acf0e0b4
Masa0u0 <masa0u0masa1215@gmail.com>
2023-04-27 23:38:18 +0900

    add: constants.hpp

---
+++ tobas_gazebo_plugins/include/tobas_gazebo_plugins/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace gazebo
+{
+static constexpr char kDefaultWindSubTopic[] = "wind_speed";
+static constexpr double kDefaultRotorSpeedSlowdownSim = 10.;
+}  // namespace gazebo
