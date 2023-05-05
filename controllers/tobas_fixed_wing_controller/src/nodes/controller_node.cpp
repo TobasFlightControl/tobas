b196318028721a9043324007842138896d735935
Masa0u0 <masa0u0masa1215@gmail.com>
2023-05-05 20:14:55 +0900

    add: controller_node.cpp

---
+++ controllers/tobas_fixed_wing_controller/src/nodes/controller_node.cpp
@@ -0,0 +1,8 @@
+#include "../../include/tobas_fixed_wing_controller/controller_ros.hpp"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "tobas_fixed_wing_controller");
+  tobas_fixed_wing_controller::ControllerRos node;
+  ros::spin();
+}
