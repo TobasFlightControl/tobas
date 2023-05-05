cd2de43cfc4e7733c682cc7426ea3bc28c726024
Masa0u0 <masa0u0masa1215@gmail.com>
2023-05-05 18:06:08 +0900

    edit: constants.hpp

--- controllers/tobas_fixed_wing_controller/include/tobas_fixed_wing_controller/constants.hpp
+++ controllers/tobas_fixed_wing_controller/include/tobas_fixed_wing_controller/constants.hpp
@@ -9,16 +9,16 @@ static constexpr double warnPeriod = 1.;              // [s]
 static constexpr double checkTopicsTimerPeriod = 5.;  // [s]
 
 static constexpr int dStateSize = 8;
-
 static constexpr int dStateIdx_u = 0;
 static constexpr int dStateIdx_alpha = 1;
 static constexpr int dStateIdx_beta = 2;
 static constexpr int dStateIdx_phi = 3;
-static constexpr int dStateIdx_pitch = 4;
+static constexpr int dStateIdx_theta = 4;
 static constexpr int dStateIdx_p = 5;
 static constexpr int dStateIdx_q = 6;
 static constexpr int dStateIdx_r = 7;
 
+static constexpr int ctrlSize = 3;
 static constexpr int ctrlIdx_beta = 0;
 static constexpr int ctrlIdx_phi = 1;
 static constexpr int ctrlIdx_theta = 2;
