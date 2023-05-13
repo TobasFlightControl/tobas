9d2b2c8cdb4182aab695057563173bb84afdb0d1
dohi-H5 <masa0u0masa1215@gmail.com>
2023-05-13 14:31:32 +0900

    .

--- teleoperations/tobas_gui_teleop/scripts/gui_teleop_node.py
+++ teleoperations/tobas_gui_teleop/scripts/gui_teleop_node.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import QApplication
 
 from tobas_gui_teleop import GuiTeleopWidget
 
+
 if __name__ == '__main__':
     node_name = osp.splitext(osp.basename(__file__))[0]
     rospy.init_node(node_name)
