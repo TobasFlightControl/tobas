f5fccb23be62cee58784631446dc15a674faa3c8
Masa0u0 <masa0u0masa1215@gmail.com>
2023-05-24 23:03:37 +0900

    edit: constants.hpp

--- controllers/tobas_fixed_wing_lqr/include/tobas_fixed_wing_lqr/constants.hpp
+++ controllers/tobas_fixed_wing_lqr/include/tobas_fixed_wing_lqr/constants.hpp
@@ -11,5 +11,5 @@ static constexpr double kStandardAirDensity = 1.225;               // [kg/m^3]
 static const std::string kCtrlName = "tobas_fixed_wing_lqr";
 static constexpr double kWarnPeriod = 1.;                          // [s]
 static constexpr double kCheckTopicsTimerPeriod = 5.;              // [s]
-static constexpr double kInitialDeltaPitch = dh_std::deg2rad(0.);  // TODO: 初期ピッチ角 (NED)
+static constexpr double kInitialDeltaPitch = dh_std::deg2rad(5.);  // TODO: 初期ピッチ角 (NED)
 }  // namespace tobas_fixed_wing_lqr
