c95beab749ae10ba806020e68d3d2e891a69e046
dohi-H5 <masa0u0masa1215@gmail.com>
2023-07-16 13:22:36 +0900

    edit: common.hpp

--- tobas_state_checker/include/tobas_state_checker/common.hpp
+++ tobas_state_checker/include/tobas_state_checker/common.hpp
@@ -2,6 +2,13 @@
 
 namespace tobas_state_checker
 {
-static constexpr double kUpdateRate = 10.;
-static constexpr double kWarnPeriod = 3.;
+static constexpr double kUpdateRate = 10.;                               // [Hz]
+static constexpr double kWarnPeriod = 3.;                                // [s]
+static constexpr double kWaitForActionServer = 3.;                       // [s]
+static constexpr double kPositionStddevThreshold = 0.5;                  // [m]
+static constexpr double kAttitudeStddevThreshold = dh_std::deg2rad(5.);  // [rad]
+static constexpr double kHeadingStddevThreshold = dh_std::deg2rad(30.);  // [rad] // TODO
+static constexpr double kBaseStateTimeout = 0.5;                         // [s]
+static constexpr double kCommandTimeout = 0.5;                           // [s]
+static constexpr double kAttitudeThreshold = dh_std::deg2rad(80.);       // [rad]
 }  // namespace tobas_state_checker
