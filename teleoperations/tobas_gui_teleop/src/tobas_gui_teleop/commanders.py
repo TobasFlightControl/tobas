b0b6ff9e18b1261246a2676177567a694216d2df
dohi-H5 <masa0u0masa1215@gmail.com>
2023-07-20 18:30:55 +0900

    edit: commanders.py
    初期状態を初期コマンドにする

--- teleoperations/tobas_gui_teleop/src/tobas_gui_teleop/commanders.py
+++ teleoperations/tobas_gui_teleop/src/tobas_gui_teleop/commanders.py
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 from dh_rqt_tools.widgets import Slider, add_expanding_widget
-from tobas_msgs.msg import PositionYaw, CommandLevel
+from tobas_msgs.msg import PositionYaw, CommandLevel, BaseState
 
 from .utils import remap
 
@@ -24,11 +24,7 @@ class CommandersWidget(QScrollArea):
     LABEL_PSIZE = 12
     CONTROL_RATE = 30.  # [Hz]
 
-    INITIAL_X = 0.  # [m]
-    INITIAL_Y = 0.  # [m]
-    INITIAL_Z = 1.  # [m]
-    INITIAL_YAW = 0.  # [rad]
-
+    DEFAULT_INITIAL_ELEVATION = 1.5  # [m]
     DEFAULT_MINIMUM_X = -10.  # [m]
     DEFAULT_MAXIMUM_X = 10.  # [m]
     DEFAULT_MINIMUM_Y = -10.  # [m]
@@ -42,6 +38,19 @@ class CommandersWidget(QScrollArea):
         super().__init__()
         self._main = main
 
+        # RosParams
+        self._x_min = 0.
+        self._x_max = 0.
+        self._y_min = 0.
+        self._y_max = 0.
+        self._z_min = 0.
+        self._z_max = 0.
+        self._yaw_min = 0.
+        self._yaw_max = 0.
+        self._init_elevation = 0.
+        self._joint_names: List[str] = []
+        self._get_params()
+
         self.setWidgetResizable(True)  # この設定が必須．無いとオブジェクトが潰れてしまう．
 
         # QScrollAreaを使う際は，QLayoutの前にQWidgetを挟む必要がある．
@@ -53,52 +62,41 @@ class CommandersWidget(QScrollArea):
         self._drone_cmd = PositionYaw()
         self._drone_cmd.level.data = CommandLevel.NORMAL
 
+        self._bs_received = False
+
         # ドローンの位置姿勢
         drone_label = QLabel("Multirotor Command")
         drone_label.setFont(QFont("Default", self.LABEL_PSIZE, QFont.Bold))
         drone_label.setAlignment(Qt.AlignCenter)
         self._rows.addWidget(drone_label)
 
-        x_min = rospy.get_param("~pose_limit/x/min", self.DEFAULT_MINIMUM_X)
-        x_max = rospy.get_param("~pose_limit/x/max", self.DEFAULT_MAXIMUM_X)
-        assert x_min <= x_max
-        self.drone_cmd_x = Commander("multirotor/x", x_min, x_max)
-        self.drone_cmd_x.set_value(self.INITIAL_X)
+        # X, Y, Z, Yawに対応するバーを追加
+        self.drone_cmd_x = Commander("multirotor/x", self._x_min, self._x_max)
+        self.drone_cmd_y = Commander("multirotor/y", self._y_min, self._y_max)
+        self.drone_cmd_z = Commander("multirotor/z", self._z_min, self._z_max)
+        self.drone_cmd_yaw = Commander("multirotor/yaw", self._yaw_min, self._yaw_max)
         self._rows.addWidget(self.drone_cmd_x)
-
-        y_min = rospy.get_param("~pose_limit/y/min", self.DEFAULT_MINIMUM_Y)
-        y_max = rospy.get_param("~pose_limit/y/max", self.DEFAULT_MAXIMUM_Y)
-        assert y_min <= y_max
-        self.drone_cmd_y = Commander("multirotor/y", y_min, y_max)
-        self.drone_cmd_y.set_value(self.INITIAL_Y)
         self._rows.addWidget(self.drone_cmd_y)
-
-        z_min = rospy.get_param("~pose_limit/z/min", self.DEFAULT_MINIMUM_Z)
-        z_max = rospy.get_param("~pose_limit/z/max", self.DEFAULT_MAXIMUM_Z)
-        assert z_min <= z_max
-        self.drone_cmd_z = Commander("multirotor/z", z_min, z_max)
-        self.drone_cmd_z.set_value(self.INITIAL_Z)
         self._rows.addWidget(self.drone_cmd_z)
-
-        yaw_min = rospy.get_param("~pose_limit/yaw/min", self.DEFAULT_MINIMUM_YAW)
-        yaw_max = rospy.get_param("~pose_limit/yaw/max", self.DEFAULT_MAXIMUM_YAW)
-        assert yaw_min <= yaw_max
-        self.drone_cmd_yaw = Commander("multirotor/yaw", yaw_min, yaw_max)
-        self.drone_cmd_yaw.set_value(self.INITIAL_YAW)
         self._rows.addWidget(self.drone_cmd_yaw)
 
+        # 最初はバーを無効化
+        self.drone_cmd_x.setEnabled(False)
+        self.drone_cmd_y.setEnabled(False)
+        self.drone_cmd_z.setEnabled(False)
+        self.drone_cmd_yaw.setEnabled(False)
+
         # その他の可動関節
-        joint_names = rospy.get_param("active_joint_names")
         robot: Robot = Robot.from_parameter_server("robot_description")
         self.joint_cmds: List[Commander] = []
 
-        if len(joint_names) > 0:
+        if len(self._joint_names) > 0:
             joint_label = QLabel("Joint Command")
             joint_label.setFont(QFont("Default", self.LABEL_PSIZE, QFont.Bold))
             joint_label.setAlignment(Qt.AlignCenter)
             self._rows.addWidget(joint_label)
 
-        for joint_name in joint_names:
+        for joint_name in self._joint_names:
             joint: Joint = robot.joint_map[joint_name]
             commander = Commander(
                 joint_name,
@@ -110,8 +108,9 @@ class CommandersWidget(QScrollArea):
             self.joint_cmds.append(commander)
             self._rows.addWidget(commander)
 
-        # Publisher
+        # PubSub
         self._drone_cmd_pub = rospy.Publisher("command/position_yaw", PositionYaw, queue_size=1)
+        self._bs_sub = rospy.Subscriber("base_state", BaseState, self._base_state_cb, queue_size=1)
 
         add_expanding_widget(self._rows)
 
@@ -128,6 +127,24 @@ class CommandersWidget(QScrollArea):
         for joint_cmd in self.joint_cmds:
             joint_cmd.publish()
 
+    def _get_params(self) -> None:
+        self._x_min = rospy.get_param("~pose_limit/x/min", self.DEFAULT_MINIMUM_X)
+        self._x_max = rospy.get_param("~pose_limit/x/max", self.DEFAULT_MAXIMUM_X)
+        self._y_min = rospy.get_param("~pose_limit/y/min", self.DEFAULT_MINIMUM_Y)
+        self._y_max = rospy.get_param("~pose_limit/y/max", self.DEFAULT_MAXIMUM_Y)
+        self._z_min = rospy.get_param("~pose_limit/z/min", self.DEFAULT_MINIMUM_Z)
+        self._z_max = rospy.get_param("~pose_limit/z/max", self.DEFAULT_MAXIMUM_Z)
+        self._yaw_min = rospy.get_param("~pose_limit/yaw/min", self.DEFAULT_MINIMUM_YAW)
+        self._yaw_max = rospy.get_param("~pose_limit/yaw/max", self.DEFAULT_MAXIMUM_YAW)
+        self._init_elevation = rospy.get_param("~initial_elevation", self.DEFAULT_INITIAL_ELEVATION)
+        self._joint_names = rospy.get_param("active_joint_names")
+
+        assert self._x_min <= self._x_max
+        assert self._y_min <= self._y_max
+        assert self._z_min <= self._z_max
+        assert self._yaw_min <= self._yaw_max
+        assert self._init_elevation >= 0.
+
     @pyqtSlot()
     def _publish_drone_cmd(self) -> None:
         self._drone_cmd.pos.x = self.drone_cmd_x.get_value()
@@ -136,6 +153,26 @@ class CommandersWidget(QScrollArea):
         self._drone_cmd.yaw = self.drone_cmd_yaw.get_value()
 
         self._drone_cmd_pub.publish(self._drone_cmd)
+
+    def _base_state_cb(self, bs: BaseState) -> None:
+        if self._bs_received:
+            return
+
+        # 最初に受け取った状態を初期コマンドにする
+        self.drone_cmd_x.set_value(bs.pose.pos.x)
+        self.drone_cmd_y.set_value(bs.pose.pos.y)
+        self.drone_cmd_z.set_value(bs.pose.pos.z + self._init_elevation)
+        self.drone_cmd_yaw.set_value(bs.pose.euler.yaw)
+
+        # バーを有効化
+        self.drone_cmd_x.setEnabled(True)
+        self.drone_cmd_y.setEnabled(True)
+        self.drone_cmd_z.setEnabled(True)
+        self.drone_cmd_yaw.setEnabled(True)
+
+        self._bs_received = True
+
+        rospy.loginfo("GUI teleoperation is ready.")
 
 
 class Commander(QWidget):
