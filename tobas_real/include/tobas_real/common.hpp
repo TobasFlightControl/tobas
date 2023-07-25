107d8e591d29a2fe920e733d67ac0b3660e699e3
dohi-H5 <masa0u0masa1215@gmail.com>
2023-07-25 15:21:49 +0900

    モータの最小回転数を設定

--- tobas_real/include/tobas_real/common.hpp
+++ tobas_real/include/tobas_real/common.hpp
@@ -18,12 +18,17 @@ static constexpr double kPwmMin = 1000.;      // [us]
 static constexpr double kPwmMax = 2000.;      // [us]
 static constexpr double kPwmDisarm = 900.;    // [us]
 
-static constexpr double kDisarmDuration = 3.;   // [s]
-static constexpr double kDisarmInterval = 0.1;  // [s]
+// モータが停止して静止摩擦が発生することを防ぐために，最低でも10%のスロットルで回転させる
+// cf. https://ardupilot.org/copter/docs/set-motor-range.html
+static constexpr double kMotorSpinArm = 0.1;                                      // [-]
+static constexpr double kPwmArm = kPwmMin + (kPwmMax - kPwmMin) * kMotorSpinArm;  // [us]
 
-static constexpr double kErrorPeriod = 1.;             // [s]
-static constexpr double kCheckTopicsTimerPeriod = 5.;  // [s]
-static constexpr double kCheckDelayThreshold = 0.02;   // [s]
+static constexpr double kDisarmDuration = 3.;                                     // [s]
+static constexpr double kDisarmInterval = 0.1;                                    // [s]
+
+static constexpr double kErrorPeriod = 1.;                                        // [s]
+static constexpr double kCheckTopicsTimerPeriod = 5.;                             // [s]
+static constexpr double kCheckDelayThreshold = 0.02;                              // [s]
 
 void setupRCOutput(RCOutput_Navio2& pwm, uint32_t channel);
 
