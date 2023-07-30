1e1ab420673470f8f708ee71c113216e7bc99b2e
dohi-H5 <masa0u0masa1215@gmail.com>
2023-07-30 18:34:02 +0900

    add: RCInput Calibration

--- tobas_real/include/tobas_real/common.hpp
+++ tobas_real/include/tobas_real/common.hpp
@@ -9,6 +9,17 @@ namespace tobas_real
 static constexpr char kConfigPath[] = "/home/pi/.config/tobas/config.ini";
 
 static constexpr char kConfigKey_AdcCoef[] = "DEFAULT.adc_coef";
+static constexpr char kConfigKey_RcRollNeutoral[] = "DEFAULT.rc_input/roll/neutoral";
+static constexpr char kConfigKey_RcRollLeft[] = "DEFAULT.rc_input/roll/left";
+static constexpr char kConfigKey_RcRollRight[] = "DEFAULT.rc_input/roll/right";
+static constexpr char kConfigKey_RcPitchNeutoral[] = "DEFAULT.rc_input/pitch/neutoral";
+static constexpr char kConfigKey_RcPitchUp[] = "DEFAULT.rc_input/pitch/up";
+static constexpr char kConfigKey_RcPitchDown[] = "DEFAULT.rc_input/pitch/down";
+static constexpr char kConfigKey_RcYawNeutoral[] = "DEFAULT.rc_input/yaw/neutoral";
+static constexpr char kConfigKey_RcYawLeft[] = "DEFAULT.rc_input/yaw/left";
+static constexpr char kConfigKey_RcYawRight[] = "DEFAULT.rc_input/yaw/right";
+static constexpr char kConfigKey_RcThrustUp[] = "DEFAULT.rc_input/thrust/up";
+static constexpr char kConfigKey_RcThrustDown[] = "DEFAULT.rc_input/thrust/down";
 
 // https://docs.emlid.com/navio2/dev/adc/
 static constexpr uint32_t kPowerModuleVoltageChannel = 2;
@@ -21,12 +32,18 @@ static constexpr double kPwmMax = 2000.;       // [us]
 static constexpr double kPwmDisarm = 900.;     // [us]
 static constexpr double kPwmArm = kPwmMin + (kPwmMax - kPwmMin) * tobas::kMotorSpinArm;  // [us]
 
-static constexpr double kDisarmDuration = 3.;                                            // [s]
-static constexpr double kDisarmInterval = 0.1;                                           // [s]
+static constexpr uint32_t kRCInputChannelYaw = 0;
+static constexpr uint32_t kRCInputChannelPitch = 1;
+static constexpr uint32_t kRCInputChannelThrust = 2;
+static constexpr uint32_t kRCInputChannelRoll = 3;
+static constexpr uint32_t kRCInputChannelSwitch = 4;
 
-static constexpr double kErrorPeriod = 1.;                                               // [s]
-static constexpr double kCheckTopicsTimerPeriod = 5.;                                    // [s]
-static constexpr double kCheckDelayThreshold = 0.02;                                     // [s]
+static constexpr double kDisarmDuration = 3.;   // [s]
+static constexpr double kDisarmInterval = 0.1;  // [s]
+
+static constexpr double kErrorPeriod = 1.;             // [s]
+static constexpr double kCheckTopicsTimerPeriod = 5.;  // [s]
+static constexpr double kCheckDelayThreshold = 0.02;   // [s]
 
 void setupRCOutput(RCOutput_Navio2& pwm, uint32_t channel);
 
---
+++ tobas_real/src/tobas_real/rcin_calibration.cpp
@@ -0,0 +1,142 @@
+#include <boost/property_tree/ini_parser.hpp>
+
+#include <dh_std_tools/fstream.hpp>
+#include <dh_ros_tools/console_message.hpp>
+#include <dh_ros_tools/exception.hpp>
+
+#include "../../include/tobas_real/rcin_calibration.hpp"
+#include "../../include/tobas_real/common.hpp"
+
+#define SLEEP_TIME 3  // [s]
+
+using namespace std;
+
+namespace tobas_real
+{
+RCInputCalibrator::RCInputCalibrator()
+{
+}
+
+void RCInputCalibrator::run()
+{
+  rcin_.initialize();
+
+  // Roll Neutoral
+  rosInfo("Please set the ROLL lever to NEUTORAL and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto roll_neutoral = readRCInput(kRCInputChannelRoll);
+
+  // Roll Left
+  rosInfo("Please set the ROLL lever to LEFT and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto roll_left = readRCInput(kRCInputChannelRoll);
+
+  // Roll Right
+  rosInfo("Please set the ROLL lever to RIGHT and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto roll_right = readRCInput(kRCInputChannelRoll);
+
+  if (!(roll_left < roll_neutoral && roll_neutoral < roll_right))
+  {
+    rosthrow("Invalid value on roll lever. 'LEFT < NEUTORAL < RIGHT' must be satisfied.");
+  }
+
+  // Pitch Neutoral
+  rosInfo("Please set the PITCH lever to NEUTORAL and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto pitch_neutoral = readRCInput(kRCInputChannelPitch);
+
+  // Pitch Up
+  rosInfo("Please set the PITCH lever to UP and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto pitch_up = readRCInput(kRCInputChannelPitch);
+
+  // Pitch Down
+  rosInfo("Please set the PITCH lever to DOWN and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto pitch_down = readRCInput(kRCInputChannelPitch);
+
+  if (!(pitch_up < pitch_neutoral && pitch_neutoral < pitch_down))
+  {
+    rosthrow("Invalid period on pitch lever. 'UP < NEUTORAL < DOWN' must be satisfied.");
+  }
+
+  // Yaw Neutoral
+  rosInfo("Please set the YAW lever to NEUTORAL and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto yaw_neutoral = readRCInput(kRCInputChannelYaw);
+
+  // Yaw Left
+  rosInfo("Please set the YAW lever to LEFT and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto yaw_left = readRCInput(kRCInputChannelYaw);
+
+  // Yaw Right
+  rosInfo("Please set the YAW lever to RIGHT and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto yaw_right = readRCInput(kRCInputChannelYaw);
+
+  if (!(yaw_left < yaw_neutoral && yaw_neutoral < yaw_right))
+  {
+    rosthrow("Invalid value on yaw lever. 'LEFT < NEUTORAL < RIGHT' must be satisfied.");
+  }
+
+  // Thrust Up
+  rosInfo("Please set the THRUST lever to UP and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto thrust_up = readRCInput(kRCInputChannelThrust);
+
+  // Thrust Down
+  rosInfo("Please set the THRUST lever to DOWN and press Enter when ready.");
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  const auto thrust_down = readRCInput(kRCInputChannelThrust);
+
+  if (!(thrust_down < thrust_up))
+  {
+    rosthrow("Invalid period on thrust lever. 'DOWN < UP' must be satisfied.");
+  }
+
+  // Configに保存
+  // 設定ファイルに係数を書き込む
+  boost::property_tree::ptree pt;
+  if (dh_std::fileExists(kConfigPath))
+  {
+    boost::property_tree::ini_parser::read_ini(kConfigPath, pt);
+  }
+  pt.put(kConfigKey_RcRollNeutoral, roll_neutoral);
+  pt.put(kConfigKey_RcRollLeft, roll_left);
+  pt.put(kConfigKey_RcRollRight, roll_right);
+  pt.put(kConfigKey_RcPitchNeutoral, pitch_neutoral);
+  pt.put(kConfigKey_RcPitchUp, pitch_up);
+  pt.put(kConfigKey_RcPitchDown, pitch_down);
+  pt.put(kConfigKey_RcYawNeutoral, yaw_neutoral);
+  pt.put(kConfigKey_RcYawNeutoral, yaw_left);
+  pt.put(kConfigKey_RcYawNeutoral, yaw_right);
+  pt.put(kConfigKey_RcThrustUp, thrust_up);
+  pt.put(kConfigKey_RcThrustDown, thrust_down);
+  boost::property_tree::ini_parser::write_ini(kConfigPath, pt);
+  rosInfo("RC input periods are saved to '" << kConfigPath << "'.");
+}
+
+double RCInputCalibrator::readRCInput(uint32_t channel)
+{
+  // RC入力を取得
+  int period_sum = 0;
+  for (uint32_t _ = 0; _ < kDataCount; ++_)
+  {
+    const auto period = rcin_.read(kRCInputChannelRoll);
+    if (period < 0)
+    {
+      rosthrow("Failed to read RC input.");
+    }
+    rosInfoThrottle(kInfoPeriod, "Period on channel " << channel << ": " << period);
+    period_sum += period;
+    usleep(kSleepTime);
+  }
+
+  // 平均を計算
+  const auto period_mean = static_cast<double>(period_sum) / kDataCount;
+  rosInfo("Finished reading. Mean period on channel " << channel << " is: " << period_mean);
+  return period_mean;
+}
+}  // namespace tobas_real
