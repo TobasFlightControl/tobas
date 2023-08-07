bd25dca636854dad23a9d03964cddaa4451d53aa
dohi-H5 <masa0u0masa1215@gmail.com>
2023-08-07 11:04:21 +0900

    .

--- tobas_real/include/tobas_real/calibration/rcin_calibration.hpp
+++ tobas_real/include/tobas_real/calibration/rcin_calibration.hpp
@@ -8,8 +8,8 @@ namespace tobas_real
 class RCInputCalibrator
 {
   static constexpr uint32_t kDataCount = 500;
-  static constexpr uint32_t kSleepTime = 10000;
-  static constexpr double kInfoPeriod = 0.5;  // [s]
+  static constexpr uint32_t kSleepTime = 10000;  // [us]
+  static constexpr double kInfoPeriod = 0.5;     // [s]
   static constexpr double kPeriodMargin = 100;   // [us]
 
 public:
