709469a3a8b61581d1934e8e64e88be23b2d8a49
dohi-H5 <masa0u0masa1215@gmail.com>
2023-08-08 23:35:03 +0900

    edit: setup_assistant.py
    メインウィンドウの位置とサイズをconfigに保存

--- tobas_setup_assistant/src/tobas_setup_assistant/setup_assistant.py
+++ tobas_setup_assistant/src/tobas_setup_assistant/setup_assistant.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+from configparser import ConfigParser
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -14,6 +15,11 @@ from .constants import *
 
 
 class SetupAssistant(QWidget):
+
+    POS_X_KEY = "main_window/pos_x"
+    POS_Y_KEY = "main_window/pos_y"
+    WIDTH_KEY = "main_window/width"
+    HEIGHT_KEY = "main_window/height"
 
     def __init__(self) -> None:
         super().__init__()
@@ -40,6 +46,16 @@ class SetupAssistant(QWidget):
         config_dir = osp.dirname(CONFIG_PATH)
         os.makedirs(config_dir, exist_ok=True)
 
+        # 最新のウィンドウの位置とサイズを反映
+        self._config = ConfigParser()
+        self._config.read(CONFIG_PATH)
+        pos_x = self._config.getint("DEFAULT", self.POS_X_KEY, fallback=-1)
+        pos_y = self._config.getint("DEFAULT", self.POS_Y_KEY, fallback=-1)
+        width = self._config.getint("DEFAULT", self.WIDTH_KEY, fallback=-1)
+        height = self._config.getint("DEFAULT", self.HEIGHT_KEY, fallback=-1)
+        if pos_x >= 0 and pos_y >= 0 and width > 0 and height > 0:
+            self.setGeometry(pos_x, pos_y, width, height)
+
         # "no attribute"エラーを防ぐため，コンストラクタの最後に再帰的にシグナルスロット接続を定義する
         self.define_connections()
 
@@ -51,3 +67,25 @@ class SetupAssistant(QWidget):
 
         # パッケージの作成が完了したら閉じる
         self.pkg_generator.generated.connect(self.close)
+
+    def moveEvent(self, event: QMoveEvent) -> None:
+        # 現在のウィンドウ位置を保存
+        self._config.read(CONFIG_PATH)
+        cur_pos = self.pos()
+        self._config["DEFAULT"][self.POS_X_KEY] = str(cur_pos.x())
+        self._config["DEFAULT"][self.POS_Y_KEY] = str(cur_pos.y())
+        with open(CONFIG_PATH, "w") as f:
+            self._config.write(f)
+
+        return super().moveEvent(event)
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        # 現在のウィンドウサイズを保存
+        self._config.read(CONFIG_PATH)
+        cur_size = self.size()
+        self._config["DEFAULT"][self.WIDTH_KEY] = str(cur_size.width())
+        self._config["DEFAULT"][self.HEIGHT_KEY] = str(cur_size.height())
+        with open(CONFIG_PATH, "w") as f:
+            self._config.write(f)
+
+        return super().resizeEvent(event)
