398d4b265a7e2b41e333a68c69cccc7a53b6a316
dohi-H5 <masa0u0masa1215@gmail.com>
2023-08-21 18:17:07 +0900

    .

--- tobas_tools/include/tobas_tools/utils.hpp
+++ tobas_tools/include/tobas_tools/utils.hpp
@@ -6,7 +6,16 @@
 
 namespace tobas
 {
+/* rosparamに登録されているdescriptionから質量 [kg] を求める． */
 double getMass();
 
+/**
+ * @brief 経緯高度から地磁気の参照値を求める．
+ *
+ * @param lat 北緯 [deg]
+ * @param lon 東経 [deg]
+ * @param height WGS84楕円体上の高度 [m]
+ * @return geomag::Elements
+ */
 geomag::Elements geomag(double lat, double lon, double height);
 }  // namespace tobas
