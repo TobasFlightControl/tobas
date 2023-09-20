20fa581bb8c984d4e605d393774edee713547fba
Masa0u0 <masa0u0masa1215@gmail.com>
2023-09-20 14:13:14 +0900

    加速度制御器で姿勢角の限界を考慮

--- controllers/tobas_mr_rotation_mpc/src/acceleration_controller.cpp
+++ controllers/tobas_mr_rotation_mpc/src/acceleration_controller.cpp
@@ -5,6 +5,7 @@
 #include <tobas_tools/constants.hpp>
 
 #include "../include/tobas_mr_rotation_mpc/acceleration_controller.hpp"
+#include "../include/tobas_mr_rotation_mpc/constants.hpp"
 
 using namespace std;
 using namespace KDL;
@@ -29,15 +30,21 @@ void AccelerationController::update(
   dh_std::clamp2d(tar_ax, tar_ay, max_hor_acc_);
   const auto tar_az = clamp(tar_acc.z(), -max_ver_acc_, max_ver_acc_);
 
-  // 3元非線形方程式の解析解を計算
-  // TODO: 姿勢制御と同様にH-forceを考慮する
-  const auto x = mass_ * tar_ax;
-  const auto y = mass_ * tar_ay;
+  // 並進のEoMの左辺
+  auto x = mass_ * tar_ax;
+  auto y = mass_ * tar_ay;
   const auto z = mass_ * (tar_az + tobas::kGravity);
 
+  // 姿勢の限界を考慮してx, yを制限
+  // さもないと姿勢制御器での目標姿勢角のクランプにより推力だけ異常に大きくなる恐れがある
+  const auto tan_max_atti = tan(kMaxAttitude);
+  const auto max_xy_norm = z * tan_max_atti * sqrt(2 + tan_max_atti);  // sqrt(x^2 + y^2)の最大値
+  dh_std::clamp2d(x, y, max_xy_norm);
+
+  // 3元非線形方程式の解析解を計算
+  // TODO: 姿勢制御と同様にH-forceを考慮する
   const auto cos_yaw = cos(yaw);
   const auto sin_yaw = sin(yaw);
-
   pitch_out = atan2(x * cos_yaw + y * sin_yaw, z);
   roll_out = atan2(cos(pitch_out) * (x * sin_yaw - y * cos_yaw), z);
   U_out = z / (cos(pitch_out) * cos(roll_out));
