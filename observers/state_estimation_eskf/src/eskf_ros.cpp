c3ae5c1c180f12a09b86054f6847da452cb9ac7b
Masa0u0 <masa0u0masa1215@gmail.com>
2023-10-24 01:58:59 +0900

    .

--- observers/state_estimation_eskf/src/eskf_ros.cpp
+++ observers/state_estimation_eskf/src/eskf_ros.cpp
@@ -67,6 +67,11 @@ void ErrorStateKalmanFilterRos::getRosParams()
   dh_ros::getParam(
     pnh_, "gps_vertical_position_stddev_threshold", gps_ver_pos_stddev_thr_,
     kDefaultGpsVerPosStddevThreshold, dh_ros::POSITIVE);
+
+  // 加速度バイアスのZ成分と重力加速度の分離は困難だと思われるため，どちらか一方のみを許容
+  if (do_acc_bias_estimation_ && do_grav_estimation_)
+    ROS_THROW_NAMED(
+      name_, "You cannot enable both accelerometer bias estimation and gravity estimation.");
 }
 
 void ErrorStateKalmanFilterRos::registerPublishers()
