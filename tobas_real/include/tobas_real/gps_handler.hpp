ef97096966cbb190d9f1b20b590c282668898b41
dh_navio2_2 <masa0u0masa1215@gmail.com>
2023-10-30 02:32:05 +0000

    .

--- tobas_real/include/tobas_real/gps_handler.hpp
+++ tobas_real/include/tobas_real/gps_handler.hpp
@@ -14,7 +14,7 @@ class GpsHandler : public tobas::BaseNode
   // GPSレシーバの更新周期 [ms]
   // 周波数が高すぎるとFIFOにデータが溜まってタイムシフトが生じるため，そんなに大きくできない
   static constexpr uint32_t kMeasurementRate = 1000 / 5;
-  static constexpr uint32_t kMainTimerRate = 1000;         // [Hz]
+  static constexpr uint32_t kMainTimerRate = 1000;  // [Hz]
 
   using self = GpsHandler;
   using super = tobas::BaseNode;
