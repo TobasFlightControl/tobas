e2e408619c77bcce1a8dcf1672472f98a8511a18
Masa0u0 <masa0u0masa1215@gmail.com>
2023-11-07 23:49:22 +0900

    .

--- controllers/tobas_mr_arducopter/scripts/sim_vehicle_node.py
+++ controllers/tobas_mr_arducopter/scripts/sim_vehicle_node.py
@@ -5,6 +5,7 @@ import os.path as osp
 import subprocess
 import signal
 
+
 class SimVehicleLauncher:
     def __init__(self) -> None:
         self._timer = rospy.Timer(
@@ -15,11 +16,16 @@ class SimVehicleLauncher:
         # ArduCopterのシミュレータを起動
         # フレームタイプ (-fオプション) の頭に"gazebo-"とつく場合はGazeboインターフェースが起動する模様
         # Iris固有の設定は"ardupilot/Tools/autotest/default_params/gazebo-iris.parm"に書いてある
-        subprocess.run(
-            "sim_vehicle.py -v ArduCopter -f gazebo-iris -d 0 -w --ekf-single",
-            shell=True,
-            check=True,
-        )
+        try:
+            subprocess.run(
+                ". ~/.profile && sim_vehicle.py -v ArduCopter -f gazebo-iris -d 0 -w --ekf-single",
+                shell=True,
+                check=True,
+            )
+        except Exception as e:
+            reason = "Failed to launch ArduPilot SITL"
+            rospy.logerr(f"{reason}: {e}")
+            rospy.signal_shutdown(reason)
 
 
 if __name__ == "__main__":
