94393144925f64969526ffd8d451653b7be5000e
Masa0u0 <masa0u0masa1215@gmail.com>
2023-11-07 19:56:58 +0900

    .

--- ardupilot_dynamic_configuration/src/ardupilot_dynamic_configuration/dynamic_configuration.py
+++ ardupilot_dynamic_configuration/src/ardupilot_dynamic_configuration/dynamic_configuration.py
@@ -15,6 +15,7 @@ from dh_rqt_tools.path import get_proj_path
 from dh_rqt_tools.messages import q_info, q_error
 
 from .common import *
+from .param_holders import *
 
 
 class DynamicConfigurationWidget(MainWidget):
@@ -62,6 +63,104 @@ class DynamicConfigurationWidget(MainWidget):
     def define_connections(self) -> None:
         self._load_button.clicked.connect(self._on_load_button_clicked)
         self._save_button.clicked.connect(self._on_save_button_clicked)
+
+    def _load_params(self, file_path: str) -> None:
+        fail_params: List[str] = []  # ロードに失敗したパラメータ
+        req = ParamSetRequest()
+
+        with open(file_path, "r") as file:
+            # CSVリーダーを使用してタブ区切りでデータを解析する
+            reader = csv.reader(file, delimiter="\t")
+
+            for row in reader:
+                # 空行とコメント行を無視
+                if not row or row[0].startswith("#"):
+                    continue
+
+                # 行からパラメータデータを抽出
+                try:
+                    vehicle_id, component_id, name, value, type_ = row
+                except ValueError as e:
+                    rospy.logerr(f"Failed to parse line: {row}. Error: {e}")
+                    continue
+
+                # パラメータを辞書に保存
+                self._params[name] = {
+                    "vehicle_id": vehicle_id,
+                    "component_id": component_id,
+                    "name": name,
+                    "value": value,
+                    "type": type_,
+                }
+
+                # 使用するセクションでない場合はスキップ
+                section = name.split("_")[0]
+                if section not in self.SECTIONS:
+                    continue
+
+                if int(type_) == 9:
+                    # パラメータをフォームに追加
+                    param = FloatParam(name)
+                    param.setDecimals(FLOAT_DECIMALS)
+                    param.setValue(float(value))
+                    param.value_changed.connect(self._on_float_param_changed)
+                    self._form.addRow(QLabel(name), param)
+
+                    # サービスリクエストを埋める
+                    req.value.integer = 0
+                    req.value.real = float(value)
+
+                else:
+                    # パラメータをフォームに追加
+                    param = IntParam(name)
+                    param.setValue(int(value))
+                    param.value_changed.connect(self._on_int_param_changed)
+                    self._form.addRow(QLabel(name), param)
+
+                    # サービスリクエストを埋める
+                    req.value.integer = int(value)
+                    req.value.real = 0.0
+
+                # ArduPilot SITLに反映
+                req.param_id = name
+                try:
+                    res: ParamSetResponse = self._param_set_sc(req)
+                except rospy.ServiceException as e:
+                    rospy.logerr(f"Failed to set {name}: {e}")
+                    self._form.removeRow(self._form.rowCount() - 1)  # 先程追加した最後の行を削除
+                    fail_params.append(name)
+                    continue
+
+                if not res.success:
+                    rospy.logerr(f"Failed to set {name}.")
+                    fail_params.append(name)
+                    continue
+
+        if len(fail_params) == 0:
+            q_info(self, "Parameters are successfully loaded.")
+        else:
+            fail_params_str = "\n".join(fail_params)
+            q_error(self, f"Failed to set following parameters:\n\n{fail_params_str}")
+
+    def _send_param_set_request(self, req: ParamSetRequest) -> bool:
+        try:
+            res: ParamSetResponse = self._param_set_sc(req)
+        except rospy.ServiceException as e:
+            q_error(self, f"Failed to set {req.param_id}: {e}")
+            return False
+
+        if not res.success:
+            q_error(self, f"Failed to set {req.param_id}.")
+            return False
+
+        rospy.loginfo(f"{req.param_id} is updated.")
+        return True
+
+    def _local_pose_cb(self, _: PoseStamped) -> None:
+        self._load_button.setEnabled(True)
+        self._save_button.setEnabled(True)
+        rospy.loginfo("Dynamic configuration is ready.")
+        self._local_pose_sub.unregister()
 
     @pyqtSlot()
     def _on_load_button_clicked(self) -> None:
@@ -112,82 +211,24 @@ class DynamicConfigurationWidget(MainWidget):
     def _on_save_button_clicked(self) -> None:
         pass  # TODO
 
-    def _load_params(self, file_path: str) -> None:
-        fail_params: List[str] = []  # ロードに失敗したパラメータ
+    @pyqtSlot(str, int)
+    def _on_int_param_changed(self, name: str, value: int):
         req = ParamSetRequest()
+        req.param_id = name
+        req.value.integer = value
 
-        with open(file_path, "r") as file:
-            # CSVリーダーを使用してタブ区切りでデータを解析する
-            reader = csv.reader(file, delimiter="\t")
-
-            for row in reader:
-                # 空行とコメント行を無視
-                if not row or row[0].startswith("#"):
-                    continue
-
-                # 行からパラメータデータを抽出
-                try:
-                    vehicle_id, component_id, name, value, type_ = row
-                except ValueError as e:
-                    rospy.logerr(f"Failed to parse line: {row}. Error: {e}")
-                    continue
-
-                # パラメータを辞書に保存
-                self._params[name] = {
-                    "vehicle_id": vehicle_id,
-                    "component_id": component_id,
-                    "name": name,
-                    "value": value,
-                    "type": type_,
-                }
-
-                # 使用するセクションでない場合はスキップ
-                section = name.split("_")[0]
-                if section not in self.SECTIONS:
-                    continue
-
-                # ArduPilot SITLに反映
-                req.param_id = name
-                try:
-                    res: ParamSetResponse = self._param_set_sc(req)
-                except rospy.ServiceException as e:
-                    rospy.logerr(f"Failed to set {name}: {e}")
-                    fail_params.append(name)
-                    continue
-
-                if not res.success:
-                    rospy.logerr(f"Failed to set {name}.")
-                    fail_params.append(name)
-                    continue
-
-                if int(type_) == 9:
-                    # パラメータをフォームに追加
-                    spin_box = DoubleSpinBox()
-                    spin_box.setDecimals(FLOAT_DECIMALS)
-                    spin_box.setValue(float(value))
-                    self._form.addRow(QLabel(name), spin_box)
-
-                    # サービスリクエストを埋める
-                    req.value.integer = 0
-                    req.value.real = float(value)
-                else:
-                    # パラメータをフォームに追加
-                    spin_box = SpinBox()
-                    spin_box.setValue(int(value))
-                    self._form.addRow(QLabel(name), spin_box)
-
-                    # サービスリクエストを埋める
-                    req.value.integer = int(value)
-                    req.value.real = 0.0
-
-        if len(fail_params) == 0:
-            q_info(self, "Parameters are successfully loaded.")
+        if self._send_param_set_request(req):
+            self._params[name]["value"] = str(value)
         else:
-            fail_params_str = "\n".join(fail_params)
-            q_error(self, f"Failed to set following parameters:\n\n{fail_params_str}")
+            pass  # TODO: 設定に失敗したパラメータをリセット
 
-    def _local_pose_cb(self, _: PoseStamped) -> None:
-        self._load_button.setEnabled(True)
-        self._save_button.setEnabled(True)
-        rospy.loginfo("Dynamic configuration is ready.")
-        self._local_pose_sub.unregister()
+    @pyqtSlot(str, float)
+    def _on_float_param_changed(self, name: str, value: float):
+        req = ParamSetRequest()
+        req.param_id = name
+        req.value.real = value
+
+        if self._send_param_set_request(req):
+            self._params[name]["value"] = str(value)
+        else:
+            pass  # TODO: 設定に失敗したパラメータをリセット
---
+++ ardupilot_dynamic_configuration/src/ardupilot_dynamic_configuration/param_holders.py
@@ -0,0 +1,29 @@
+from dh_rqt_tools.widgets import *
+
+from .common import *
+
+
+class IntParam(SpinBox):
+    value_changed = pyqtSignal(str, int)
+
+    def __init__(self, name: str):
+        super().__init__()
+        self._name = name
+        self.valueChanged.connect(self._on_value_changed)
+
+    @pyqtSlot(int)
+    def _on_value_changed(self, value: int) -> None:
+        self.value_changed.emit(self._name, value)
+
+
+class FloatParam(DoubleSpinBox):
+    value_changed = pyqtSignal(str, float)
+
+    def __init__(self, name: str):
+        super().__init__()
+        self._name = name
+        self.valueChanged.connect(self._on_value_changed)
+
+    @pyqtSlot(float)
+    def _on_value_changed(self, value: float) -> None:
+        self.value_changed.emit(self._name, value)
