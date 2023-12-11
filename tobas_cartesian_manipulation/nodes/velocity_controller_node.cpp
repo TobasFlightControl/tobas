7f465fd7acebd56268f0f0959fac78229d28fd59
Masa0u0 <masa0u0masa1215@gmail.com>
2023-12-11 14:31:59 +0900

    .

--- tobas_cartesian_manipulation/nodes/velocity_controller_node.cpp
+++ tobas_cartesian_manipulation/nodes/velocity_controller_node.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "velocity_controller");
+  ros::init(argc, argv, "joint_controller_velocity");
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
   tobas_cartesian_manipulation::VelocityControllerRos node(nh, pnh);
