6a107ad1167278f3dfaf96c34ea219f21042c45a
Masa0u0 <masa0u0masa1215@gmail.com>
2023-12-14 13:01:32 +0900

    .

--- tobas_setup_assistant/src/tobas_setup_assistant/setting_widgets/propulsion_system/aerodynamics.py
+++ tobas_setup_assistant/src/tobas_setup_assistant/setting_widgets/propulsion_system/aerodynamics.py
@@ -147,7 +147,7 @@ class AerodynamicsWidget_Base(QWidget):
             max_model_error_rate_description,
             minimum=0,
             maximum=1000,
-            default=10,
+            default=5,
             suffix=" %",
         )
         self._rows.addWidget(self._max_model_error_rate)
