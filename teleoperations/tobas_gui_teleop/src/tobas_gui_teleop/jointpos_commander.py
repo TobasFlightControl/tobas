0e612f55763084921e3bed9d7040cf98ccfd11a3
Masa0u0 <masa0u0masa1215@gmail.com>
2023-12-26 19:21:36 +0900

    fix: jointpos_commander.py

--- teleoperations/tobas_gui_teleop/src/tobas_gui_teleop/jointpos_commander.py
+++ teleoperations/tobas_gui_teleop/src/tobas_gui_teleop/jointpos_commander.py
@@ -26,7 +26,7 @@ class JointPositionsCommander(MainWidget):
 
         icon_path = osp.join(rospkg.RosPack().get_path(PKG_NAME), "resources/icon.png")
         self.setWindowIcon(QIcon(icon_path))
-        self.setWindowTitle("Joint State Commander")
+        self.setWindowTitle("Joint Positions Commander")
 
         # rosparams
         self._home_positions: Dict[str, float] = {}
@@ -38,19 +38,20 @@ class JointPositionsCommander(MainWidget):
         self._tar_js_vel = JointState()
         self._tar_js_eff = JointState()
         for jnt_name, cmd_type in self._cmd_types.items():
+            home_pos = self._home_positions[jnt_name]
             if cmd_type == self.POSITION:
                 self._tar_pos.name.append(jnt_name)
-                self._tar_pos.data.append(0)
+                self._tar_pos.data.append(home_pos)
             elif cmd_type == self.VELOCITY:
                 self._tar_js_vel.name.append(jnt_name)
-                self._tar_js_vel.position.append(0)
-                self._tar_js_vel.velocity.append(0)
-                self._tar_js_vel.effort.append(0)
+                self._tar_js_vel.position.append(home_pos)
+                self._tar_js_vel.velocity.append(0.0)
+                self._tar_js_vel.effort.append(0.0)
             elif cmd_type == self.EFFORT:
                 self._tar_js_eff.name.append(jnt_name)
-                self._tar_js_eff.position.append(0)
-                self._tar_js_eff.velocity.append(0)
-                self._tar_js_eff.effort.append(0)
+                self._tar_js_eff.position.append(home_pos)
+                self._tar_js_eff.velocity.append(0.0)
+                self._tar_js_eff.effort.append(0.0)
             else:
                 raise RuntimeError(f"Unknown joint command type: {cmd_type}")
 
@@ -102,9 +103,7 @@ class JointPositionsCommander(MainWidget):
 
         add_spacer(rows)
 
-        self._tar_pos_pub.publish(self._tar_pos)
-        self._tar_js_vel_pub.publish(self._tar_js_vel)
-        self._tar_js_eff_pub.publish(self._tar_js_eff)
+        self._publish_current_commands()
 
     def _get_params(self) -> None:
         num_joints = rospy.get_param("num_joints")
@@ -114,6 +113,11 @@ class JointPositionsCommander(MainWidget):
             cmd_type = rospy.get_param(f"joint_{i}/command_type")
             self._home_positions[jnt_name] = home_pos
             self._cmd_types[jnt_name] = cmd_type
+
+    def _publish_current_commands(self):
+        self._tar_pos_pub.publish(self._tar_pos)
+        self._tar_js_vel_pub.publish(self._tar_js_vel)
+        self._tar_js_eff_pub.publish(self._tar_js_eff)
 
     @pyqtSlot(float)
     def _on_value_changed(self, value: float, jnt_name: str) -> None:
@@ -137,22 +141,18 @@ class JointPositionsCommander(MainWidget):
     @pyqtSlot()
     def _on_home_button_clicked(self) -> None:
         """全ての関節角をホームポジションに設定する．"""
-        self.setEnabled(False)
-
         for jnt_name, home_pos in self._home_positions.items():
             self._commanders[jnt_name].set_value(home_pos)
 
-        self.setEnabled(True)
+        self._publish_current_commands()
 
     @pyqtSlot()
     def _on_center_button_clicked(self) -> None:
         """全ての関節角を中央の値に設定する．"""
-        self.setEnabled(False)
-
         for joint_cmd in self._commanders.values():
             joint_cmd.set_center_value()
 
-        self.setEnabled(True)
+        self._publish_current_commands()
 
     @pyqtSlot()
     def _on_random_button_clicked(self) -> None:
