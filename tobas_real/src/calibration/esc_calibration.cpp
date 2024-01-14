f63eeb652eaf67beb23df5381fd2f0a51c4b7082
Masa0u0 <masa0u0masa1215@gmail.com>
2024-01-14 22:33:54 +0900

    edit: esc_alibration.cpp

--- tobas_real/src/calibration/esc_calibration.cpp
+++ tobas_real/src/calibration/esc_calibration.cpp
@@ -17,24 +17,27 @@ namespace tobas_real
 EscCalibration::EscCalibration()
 {
   if (!tobas_std::isSuperUser())
-  {
     throw runtime_error("Please execute with root privileges.");
-  }
 
   for (size_t channel = 0; channel < kServoRailSize; ++channel)
-  {
     setupRCOutput(pwm_, channel);
-  }
 
   adc_.initialize();
 }
 
 void EscCalibration::run()
 {
-  TOBAS_WARN("Please make sure propellers are memoved from motors and press Enter:");
+  TOBAS_WARN("Please make sure propellers are memoved from motors.");
+
+  cout << "Please press Enter:";
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-  waitForBatteryDisconnected();
+  if (isBatteryConnected())
+  {
+    cout << "Please disconnect battery." << endl;
+    waitForBatteryDisconnected();
+  }
+
   sendMaximum();
   sendMinimum();
 
@@ -43,59 +46,41 @@ void EscCalibration::run()
 
 void EscCalibration::waitForBatteryDisconnected()
 {
-  cout << "Please disconnect battery." << endl;
-
   while (isBatteryConnected())
-  {
     usleep(kInterval);
-  }
 
   cout << "Battery disconnection detected." << endl;
 }
 
 void EscCalibration::sendMaximum()
 {
-  cout << "Sending maximum throttle." << endl;
   cout << "Please connect battery." << endl;
 
   while (!isBatteryConnected())
-  {
     setPeriodAndSleep(kPwmMax);
-  }
 
   cout << "Battery connection detected." << endl;
-  cout << "Wait for " << tobas_std::secondsFromMicroSeconds(kSleepHigh) << "seconds." << endl;
+  cout << "Sending maximum throttle." << endl;
 
   const auto start_time = system_clock::now();
   while (duration_cast<microseconds>(system_clock::now() - start_time).count() < kSleepHigh)
-  {
     setPeriodAndSleep(kPwmMax);
-  }
-
-  cout << "Finished setting maximum value." << endl;
 }
 
 void EscCalibration::sendMinimum()
 {
   cout << "Sending minimum throttle." << endl;
-  cout << "Wait for " << tobas_std::secondsFromMicroSeconds(kSleepLow) << "seconds." << endl;
 
   const auto start_time = system_clock::now();
   while (duration_cast<microseconds>(system_clock::now() - start_time).count() < kSleepLow)
-  {
     setPeriodAndSleep(kPwmMin);
-  }
-
-  cout << "Finished setting minimum value." << endl;
 }
 
 void EscCalibration::setPeriod(const double& period)
 {
   for (size_t channel = 0; channel < kServoRailSize; ++channel)
-  {
     if (!pwm_.setDutyCycle(channel, period))
       throw runtime_error("Failed to set PWM duty cycle.");
-  }
 }
 
 void EscCalibration::setPeriodAndSleep(const double& period)
