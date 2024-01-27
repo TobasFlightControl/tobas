a78a3d48520cbf7512e391a38caa33012bd2b3c5
Masa0u0 <masa0u0masa1215@gmail.com>
2024-01-27 11:21:03 +0900

    add: time_reference_server

---
+++ tobas_real/src/time_reference_server.cpp
@@ -0,0 +1,57 @@
+#include <sensor_msgs/TimeReference.h>
+
+#include <tobas_tools/constants.hpp>
+
+#include "../include/tobas_real/time_reference_server.hpp"
+
+using namespace std;
+
+namespace tobas_real
+{
+TimeReferenceServer::TimeReferenceServer(
+  const ros::NodeHandle& nh,
+  const ros::NodeHandle& pnh,
+  const string& name)
+  : super(nh, pnh, name)
+{
+  getRosParams();
+  registerPublishers();
+  registerSubscribers();
+
+  main_timer_ = nh_.createTimer(kUpdateRate, &self::mainTimerCb, this);
+}
+
+void TimeReferenceServer::getRosParams()
+{
+}
+
+void TimeReferenceServer::registerPublishers()
+{
+  time_ref_pub_ = nh_.advertise<sensor_msgs::TimeReference>(tobas::kTimeReferenceTopic, 1);
+}
+
+void TimeReferenceServer::registerSubscribers()
+{
+}
+
+void TimeReferenceServer::eventCb(const tobas_msgs::EventConstPtr& event)
+{
+  switch (event->data)
+  {
+    case tobas_msgs::Event::STOP:
+      nh_.shutdown();
+      main_timer_.stop();
+      break;
+    default:
+      break;
+  }
+}
+
+void TimeReferenceServer::mainTimerCb(const ros::TimerEvent& event)
+{
+  const auto time_ref = boost::make_shared<sensor_msgs::TimeReference>();
+  time_ref->header.stamp = event.current_real;
+  time_ref->time_ref = event.current_real;
+  time_ref_pub_.publish(time_ref);
+}
+}  // namespace tobas_real
--- tobas_tools/include/tobas_tools/constants.hpp
+++ tobas_tools/include/tobas_tools/constants.hpp
@@ -27,6 +27,7 @@ static constexpr double kMaxThrottle = 1.;
 static constexpr char kRobotDescriptionParam[] = "robot_description";
 
 // ROS topics
+static constexpr char kTimeReferenceTopic[] = "/ntpd_shm/time_ref";
 static constexpr char kBatteryTopic[] = "battery";
 static constexpr char kBatteryLpfTopic[] = "battery_filtered";
 static constexpr char kCpuTopic[] = "cpu";
