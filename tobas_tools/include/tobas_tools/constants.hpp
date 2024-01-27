1c1c22c3ef75791c3969e93011804e15e144a66c
Masa0u0 <masa0u0masa1215@gmail.com>
2024-01-27 11:55:23 +0900

    .

--- tobas_tools/include/tobas_tools/constants.hpp
+++ tobas_tools/include/tobas_tools/constants.hpp
@@ -27,7 +27,7 @@ static constexpr double kMaxThrottle = 1.;
 static constexpr char kRobotDescriptionParam[] = "robot_description";
 
 // ROS topics
-static constexpr char kTimeReferenceTopic[] = "/ntpd_shm/time_ref";
+static constexpr char kTimeReferenceTopic[] = "/shm_driver/time_ref";
 static constexpr char kBatteryTopic[] = "battery";
 static constexpr char kBatteryLpfTopic[] = "battery_filtered";
 static constexpr char kCpuTopic[] = "cpu";
