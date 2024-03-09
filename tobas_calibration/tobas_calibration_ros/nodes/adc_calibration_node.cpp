5ba3d0902b146d5fadc9b9ffae30390388862d54
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-09 18:57:22 +0900

    fix: adc_calibration_node.cpp

--- tobas_calibration/tobas_calibration_ros/nodes/adc_calibration_node.cpp
+++ tobas_calibration/tobas_calibration_ros/nodes/adc_calibration_node.cpp
@@ -1,9 +1,9 @@
-#include "../include/tobas_calibration/accel_calibration.hpp"
+#include "../include/tobas_calibration/adc_calibration.hpp"
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "accel_calibration");
+  ros::init(argc, argv, "adc_calibration");
   ros::NodeHandle nh;
-  tobas_calibration::AccelCalibrationRos node(nh);
+  tobas_calibration::AdcCalibrationRos node(nh);
   ros::spin();
 }
