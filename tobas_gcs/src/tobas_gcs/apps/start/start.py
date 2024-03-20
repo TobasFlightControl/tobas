ca9441af1ebfcf1df46490c0830f464ea935e3eb
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-20 21:34:27 +0900

    edit: start.py

--- tobas_gcs/src/tobas_gcs/apps/start/start.py
+++ tobas_gcs/src/tobas_gcs/apps/start/start.py
@@ -8,13 +8,33 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+from tobas_rqt_tools.utils import place_center
+
 
 class StartWidget(QWidget):
     NAME = "Start"
 
+    TITLE_PSIZE = 50
+    SUBTITLE_PSIZE = 20
+
     def __init__(self, main: GroundControlStationWidget) -> None:
-        super().__init__()
+        super().__init__(self._main)
         self._main = main
+
+        rows = QVBoxLayout()
+        self.setLayout(rows)
+
+        title = QLabel("Tobas", self)
+        title.setFont(QFont("Default", pointSize=self.TITLE_PSIZE, weight=QFont.Bold))
+        place_center(title, rows)
+
+        sub_title = QLabel("— The Flight Controller for All Drones —", self)  # Em dash: Ctrl+Shift+u -> 2014 -> Enter
+        sub_title.setFont(QFont("Default", pointSize=self.SUBTITLE_PSIZE, weight=QFont.Bold))
+        place_center(sub_title, rows)
+
+        # TODO: ニュース，リリースノート，関連リンクなど
+
+        rows.addStretch()
 
     def define_connections(self) -> None:
         pass  # TODO
