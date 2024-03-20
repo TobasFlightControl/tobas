5be21360da2c0f26deb4efac76724f779a0a3292
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-20 19:00:22 +0900

    edit: create_deb_pkg.sh

--- tobas_deb/tobas_deb_tools/create_deb_pkg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-DEB_WS=$(realpath ${SCRIPT_DIR}/../tobas_deb_ws)
-
-cd ${DEB_WS}
-catkin build tobas -DCMAKE_INSTALL_PREFIX=${DEB_WS}/opt/tobas/
-fakeroot dpkg-deb --build ${DEB_WS} ~/Downloads
