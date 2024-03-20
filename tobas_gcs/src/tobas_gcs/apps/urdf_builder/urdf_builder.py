db49561498c32976a18aa5f82fed42c388a34f75
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-20 21:45:29 +0900

    edit: urdf_builder.py

--- tobas_gcs/src/tobas_gcs/apps/urdf_builder/urdf_builder.py
+++ tobas_gcs/src/tobas_gcs/apps/urdf_builder/urdf_builder.py
@@ -4,9 +4,13 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ...gcs import GroundControlStationWidget
 
+import os.path as osp
+import rospkg
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+
+from tobas_rqt_tools.widgets import create_rviz_frame
 
 
 class UrdfBuilderWidget(QWidget):
@@ -16,5 +20,14 @@ class UrdfBuilderWidget(QWidget):
         super().__init__()
         self._main = main
 
+        rows = QVBoxLayout()
+        self.setLayout(rows)
+
+        pkg_path = rospkg.RosPack().get_path("urdf_builder")
+        rviz_config_path = osp.join(pkg_path, "config/urdf_builder.rviz")
+        self._frame = create_rviz_frame(rviz_config_path)
+
+        rows.addWidget(self._frame)
+
     def define_connections(self) -> None:
-        pass  # TODO
+        pass
