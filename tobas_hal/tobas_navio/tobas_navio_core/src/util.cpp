5c4a0e2c21429f9d260ce5cd8a35ff14662879d8
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-28 23:03:52 +0900

    .

--- tobas_hal/tobas_navio/tobas_navio_core/src/util.cpp
+++ tobas_hal/tobas_navio/tobas_navio_core/src/util.cpp
@@ -81,6 +81,8 @@ bool checkAPM()
 
 float decodeBinary32(uint32_t bin)
 {
+  // exponentは符号付き整数型でなければならない
+  // exponentが符号なしだと続く(exponent - 150)がオーバーフローしてしまう
   const int sign = bin >> 31 ? -1 : 1;
   const int exponent = (bin >> 23) & 0xFF;
   const int mantissa = (exponent == 0) ? (bin & 0x7FFFFF) << 1 : (bin & 0x7FFFFF) | 0x800000;
