d6be73136f07ddefd096dbe496f9ea92ac8a471d
Masa0u0 <masa0u0masa1215@gmail.com>
2024-03-31 19:14:24 +0900

    wip: status_viewer

--- tobas_gcs/src/tobas_gcs/apps/control_system/sections/status_viewer.py
+++ tobas_gcs/src/tobas_gcs/apps/control_system/sections/status_viewer.py
@@ -14,7 +14,7 @@ from PyQt5.QtGui import *
 
 from tobas_rqt_tools.widgets import LEDColor, LampWidget
 from tobas_tools_py.drone import Drone
-from tobas_msgs.msg import Gps
+from tobas_msgs.msg import Gps, RCInput, RCInputError, Odometry
 
 from ....common import TO_DO
 from .base_section import BaseControlSystemSectionWidget
@@ -27,15 +27,24 @@ class StatusViewerWidget(BaseControlSystemSectionWidget):
         super().__init__(main, drone)
 
         self._gps_status = GpsStatus(main, drone)
+        self._rcin_status = RCInputStatus(main, drone)
+        self._ready_status = ReadyToFlightStatus(main, drone)
+
         self._rows.addWidget(self._gps_status)
+        self._rows.addWidget(self._rcin_status)
+        self._rows.addWidget(self._ready_status)
 
     @override
     def define_connections(self) -> None:
         self._gps_status.define_connections()
+        self._rcin_status.define_connections()
+        self._ready_status.define_connections()
 
     @override
     def update_internal_data_structures(self) -> None:
         self._gps_status.update_internal_data_structures()
+        self._rcin_status.update_internal_data_structures()
+        self._ready_status.update_internal_data_structures()
 
 
 class BaseStatusWidget(QWidget):
@@ -77,10 +86,6 @@ class BaseStatusWidget(QWidget):
 
     @final
     def set_no(self) -> None:
-        self._led.set_color(LEDColor.RED)
-
-    @final
-    def set_unknown(self) -> None:
         self._led.set_color(LEDColor.BLACK)
 
 
@@ -98,7 +103,7 @@ class GpsStatus(BaseStatusWidget):
 
     @override
     def update_internal_data_structures(self) -> None:
-        self.set_unknown()
+        self.set_no()
 
         if self._gps_sub is not None:
             self._gps_sub.unregister()
@@ -109,3 +114,55 @@ class GpsStatus(BaseStatusWidget):
             self.set_yes()
         else:
             self.set_no()
+
+
+class RCInputStatus(BaseStatusWidget):
+    TEXT = "Radio input received"
+
+    def __init__(self, main: GroundControlStationWidget, drone: Drone) -> None:
+        super().__init__(main, drone)
+
+        self._rcin_sub = None
+
+    @override
+    def define_connections(self) -> None:
+        pass
+
+    @override
+    def update_internal_data_structures(self) -> None:
+        self.set_no()
+
+        if self._rcin_sub is not None:
+            self._rcin_sub.unregister()
+        self._rcin_sub = rospy.Subscriber(f"/{self._drone.drone_name}/rc_input", Gps, self._rcin_cb, queue_size=1)
+
+    def _rcin_cb(self, rcin: RCInput) -> None:
+        if rcin.error == RCInputError.E_NO_ERROR:
+            self.set_yes()
+        else:
+            self.set_no()
+
+
+class ReadyToFlightStatus(BaseStatusWidget):
+    TEXT = "Ready to flight"
+
+    def __init__(self, main: GroundControlStationWidget, drone: Drone) -> None:
+        super().__init__(main, drone)
+
+        self._odom_sub = None
+
+    @override
+    def define_connections(self) -> None:
+        pass
+
+    @override
+    def update_internal_data_structures(self) -> None:
+        self.set_no()
+
+        if self._odom_sub is not None:
+            self._odom_sub.unregister()
+        self._odom_sub = rospy.Subscriber(f"/{self._drone.drone_name}/odom", Gps, self._odom_cb, queue_size=1)
+
+    def _odom_cb(self, _: Odometry) -> None:
+        # TODO: 状態推定だけでなく，アクチュエータのチェックも行う
+        self.set_yes()
