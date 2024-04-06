f15281e4a6fbd8c5c81b03ff6b5a659904e498a9
dohi-navio2 <masa0u0masa1215@gmail.com>
2024-04-06 16:36:45 +0100

    edit: ublox.hpp

--- tobas_hal/tobas_navio/tobas_navio_core/include/tobas_navio_core/ublox.hpp
+++ tobas_hal/tobas_navio/tobas_navio_core/include/tobas_navio_core/ublox.hpp
@@ -17,7 +17,7 @@ static constexpr uint32_t kSpiSpeedHz = 5500000;  // Maximum frequency is 5.5MHz
 static constexpr uint32_t kConfigureMessageSize = 11;
 static constexpr uint32_t kMinMaxTrkChForMajorGnss = 4;
 static constexpr uint32_t kWaitForGnssAck = 1000000;  // [us]
-static constexpr size_t kSpiInterval = 60;            // [us]
+static constexpr size_t kSpiInterval = 50;            // [us]
 
 class UBXScanner
 {
