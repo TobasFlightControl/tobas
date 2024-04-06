25c6b54e6d0dc6a8af8f5e58507df5b7a5a6809c
Masa0u0 <masa0u0masa1215@gmail.com>
2024-04-06 11:19:31 +0900

    CPU使用率を発行

--- tobas_real/tobas_real_ros/include/tobas_real_ros/cpu_handler.hpp
+++ tobas_real/tobas_real_ros/include/tobas_real_ros/cpu_handler.hpp
@@ -11,6 +11,7 @@ class CpuHandler : public tobas::BaseNode
 {
   static constexpr size_t kSamplingRate = 1;  // [Hz]
   static constexpr char kTemperatureFilePath[] = "/sys/class/thermal/thermal_zone0/temp";
+  static constexpr char kStatisticsFilePath[] = "/proc/stat";
 
   using self = CpuHandler;
   using super = tobas::BaseNode;
@@ -23,6 +24,8 @@ public:
 
 private:
   int temp_millidegrees_;
+  std::string cpu_line_, token_;
+  uint64_t prev_user_time_ = 0, prev_nice_time_ = 0, prev_system_time_ = 0, prev_idle_time_ = 0;
 
   // Publisher
   ros::Publisher cpu_pub_;
@@ -33,6 +36,10 @@ private:
   void getRosParams() override;
   void registerPublishers() override;
   void registerSubscribers() override;
+
+  bool getTemperature(double& temp);
+  bool getFrequency(uint64_t& freq);
+  bool getLoad(double& load);
 
   void mainTimerCb(const ros::TimerEvent& event);
 };
--- tobas_real/tobas_real_ros/src/cpu_handler.cpp
+++ tobas_real/tobas_real_ros/src/cpu_handler.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <sstream>
 
 #include <tobas_std_tools/unix.hpp>
 #include <tobas_std_tools/string.hpp>
@@ -35,28 +36,120 @@ void CpuHandler::registerSubscribers()
 {
 }
 
-void CpuHandler::mainTimerCb(const ros::TimerEvent& event)
+bool CpuHandler::getTemperature(double& temp)
 {
-  // Get CPU temperature
-  ifstream file(kTemperatureFilePath);
-  if (!file)
+  ifstream temp_file(kTemperatureFilePath);
+  if (!temp_file)
   {
     rosError(name_, "Failed to open " << kTemperatureFilePath << ".");
-    return;
+    return false;
   }
-  file >> temp_millidegrees_;
+  temp_file >> temp_millidegrees_;
+  temp = static_cast<double>(temp_millidegrees_) * 1e-3;
+  return true;
+}
 
-  // Get CPU frequency
+bool CpuHandler::getFrequency(uint64_t& freq)
+{
   auto vcgencmd_out = tobas_std::exec_command("vcgencmd measure_clock arm");
   vcgencmd_out.pop_back();                                           // 改行コードを削除
   const auto freq_str = tobas_std::split(vcgencmd_out, '=').back();  // 数値部分のみ抜き出す
-  const auto freq = stoul(freq_str);                                 // str -> uint64
+  freq = stoul(freq_str);                                            // str -> uint64
+  return true;
+}
 
+bool CpuHandler::getLoad(double& load)
+{
+  // ファイルを読み込む
+  ifstream stat_file(kStatisticsFilePath);
+  if (!stat_file)
+  {
+    rosError(name_, "Failed to open " << kStatisticsFilePath << ".");
+    return false;
+  }
+
+  // ファイルの最初の行を読む
+  if (!getline(stat_file, cpu_line_))
+  {
+    rosError(name_, "Failed to read the first line of " << kStatisticsFilePath << ".");
+    return false;
+  }
+
+  // CPUの行を空白で区切る
+  std::istringstream iss(cpu_line_);
+
+  // CPU使用時間を取得 (http://my-web-site.iobb.net/~yuki/2017-10/raspberry-pi/cpustat/)
+  // 最初のトークン（"cpu"）を読み飛ばす
+  iss >> token_;
+
+  // (01) Time spent in user mode
+  if (!(iss >> token_))
+  {
+    rosError(name_, "Failed to read the CPU time spent in user mode.");
+    return false;
+  }
+  const auto new_user_time = std::stoul(token_);
+
+  // (02) Time spent in user mode with low priority (nice)
+  if (!(iss >> token_))
+  {
+    rosError(name_, "Failed to read the CPU time spent in user mode with low priority.");
+    return false;
+  }
+  const auto new_nice_time = std::stoul(token_);
+
+  // (03) Time spent in system mode
+  if (!(iss >> token_))
+  {
+    rosError(name_, "Failed to read the CPU time spent in system mode.");
+    return false;
+  }
+  const auto new_system_time = std::stoul(token_);
+
+  // (04) Time spent in the idle task
+  if (!(iss >> token_))
+  {
+    rosError(name_, "Failed to read the CPU time spent in the idle task.");
+    return false;
+  }
+  const auto new_idle_time = std::stoul(token_);
+
+  // 前回値との差分を計算
+  const auto user_time = new_user_time - prev_user_time_;
+  const auto nice_time = new_nice_time - prev_nice_time_;
+  const auto system_time = new_system_time - prev_system_time_;
+  const auto idle_time = new_idle_time - prev_idle_time_;
+
+  // 負荷を計算
+  const auto busy_time = user_time + nice_time + system_time;
+  load = static_cast<double>(busy_time) / static_cast<double>(idle_time);
+
+  // CPU使用時間を更新
+  prev_user_time_ = new_user_time;
+  prev_nice_time_ = new_nice_time;
+  prev_system_time_ = new_system_time;
+  prev_idle_time_ = new_idle_time;
+
+  return true;
+}
+
+void CpuHandler::mainTimerCb(const ros::TimerEvent& event)
+{
   // Create ROS message
   const auto cpu_msg = boost::make_shared<tobas_msgs::Cpu>();
   cpu_msg->header.stamp = event.current_real;
-  cpu_msg->temperature = static_cast<double>(temp_millidegrees_) * 1e-3;
-  cpu_msg->frequency = freq;
+
+  // Get CPU temperature
+  if (!getTemperature(cpu_msg->temperature))
+    return;
+
+  // Get CPU frequency
+  if (!getFrequency(cpu_msg->frequency))
+    return;
+
+  // Get CPU load
+  if (!getLoad(cpu_msg->load))
+    return;
 
   // Publish ROS message
   cpu_pub_.publish(cpu_msg);
