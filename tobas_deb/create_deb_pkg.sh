e27d2e83757b98a105215e9aaf4a0d00e5c95d60
Masa0u0 <masa0u0masa1215@gmail.com>
2024-05-04 08:09:15 +0900

    tobas_deb_ws -> ubuntu

--- tobas_deb/create_deb_pkg.sh
+++ tobas_deb/create_deb_pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
-TOBAS_DEB_WS=$(realpath ${TOBAS_DEB}/tobas_deb_ws)
+UBUNTU_WS=$(realpath ${TOBAS_DEB}/tobas_deb_ws)
 
 cd ${TOBAS_DEB}
-catkin build tobas -DCMAKE_INSTALL_PREFIX=${TOBAS_DEB_WS}/opt/tobas/
-fakeroot dpkg-deb --build ${TOBAS_DEB_WS} ${TOBAS_DEB}
+catkin build tobas -DCMAKE_INSTALL_PREFIX=${UBUNTU_WS}/opt/tobas/
+fakeroot dpkg-deb --build ${UBUNTU_WS} ${TOBAS_DEB}
