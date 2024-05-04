8e818c924cebd6d4ce835593fab727327bfd698c
Masa0u0 <masa0u0masa1215@gmail.com>
2024-05-04 10:50:27 +0900

    raspbian用のインストールスクリプト

--- tobas_deb/create_deb_pkg.sh
+++ tobas_deb/create_deb_pkg.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
-UBUNTU_WS=$(realpath ${TOBAS_DEB}/tobas_deb_ws)
-
 cd ${TOBAS_DEB}
+
+# Ubuntu
+UBUNTU_WS=$(realpath ${TOBAS_DEB}/ubuntu)
 catkin build tobas -DCMAKE_INSTALL_PREFIX=${UBUNTU_WS}/opt/tobas/
 fakeroot dpkg-deb --build ${UBUNTU_WS} ${TOBAS_DEB}
+
+# Raspbian
+RASPBIAN_WS=$(realpath ${TOBAS_DEB}/raspbian)
+fakeroot dpkg-deb --build ${RASPBIAN_WS} ${TOBAS_DEB}
---
+++ tobas_deb/raspbian/etc/tobas/kill_ros.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/killall -q rosmaster || true
+/usr/bin/killall -q rosout || true
+/usr/bin/killall -q roslaunch || true
