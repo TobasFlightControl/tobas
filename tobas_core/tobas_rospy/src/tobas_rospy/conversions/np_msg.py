d9933aff787d79a8f4a24f5be2498bbb2d45eaf1
dohi <masa0u0masa1215@gmail.com>
2024-05-13 18:26:04 +0900

    edit: np_msg.py

--- tobas_core/tobas_rospy/src/tobas_rospy/conversions/np_msg.py
+++ tobas_core/tobas_rospy/src/tobas_rospy/conversions/np_msg.py
@@ -1,5 +1,5 @@
 import numpy as np
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3, Quaternion
 
 
 def vector3_msg_to_np(m: Vector3) -> np.ndarray:
@@ -9,3 +9,12 @@ def vector3_msg_to_np(m: Vector3) -> np.ndarray:
 def vector3_np_to_msg(n: np.ndarray) -> Vector3:
     assert n.shape == (3,)
     return Vector3(x=n[0], y=n[1], z=n[2])
+
+
+def quaternion_msg_to_np(m: Quaternion) -> np.ndarray:
+    return np.array([m.x, m.y, m.z, m.w])
+
+
+def quaternion_np_to_msg(n: np.ndarray) -> Quaternion:
+    assert n.shape == (4,)
+    return Quaternion(x=n[0], y=n[1], z=n[2], w=n[3])
