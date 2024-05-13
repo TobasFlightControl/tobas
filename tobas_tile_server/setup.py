2a9867cd0e502d6fd0d8c02004659ad1f25cd55b
dohi <masa0u0masa1215@gmail.com>
2024-05-13 18:36:16 +0900

    black format

--- tobas_tile_server/setup.py
+++ tobas_tile_server/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-	packages=["tobas_tile_server"],
-	package_dir={"": "src"},
+    packages=["tobas_tile_server"],
+    package_dir={"": "src"},
 )
 
 setup(**setup_args)
