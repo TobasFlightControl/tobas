192ae02bb2ef68600f4c0318f6dc127c32be251c
Masa0u0 <masa0u0masa1215@gmail.com>
2024-06-01 12:32:28 +0900

    max_rot_speedの配置変更

--- tobas_setup_assistant/src/tobas_setup_assistant/setting_widgets/propulsion_system/motor.py
+++ tobas_setup_assistant/src/tobas_setup_assistant/setting_widgets/propulsion_system/motor.py
@@ -91,10 +91,6 @@ class MotorWidget(BaseSelectedLinkSettingWidget):
         for method in self._methods:
             method.load_settings(data[method.NAME])
 
-    def max_rot_speed(self) -> float:
-        """[rad/s]"""
-        return self._common_params.max_rot_speed.max_rot_speed()
-
     def direction(self) -> str:
         """CW or CCW"""
         return self._common_params.direction.get()
@@ -109,6 +105,10 @@ class MotorWidget(BaseSelectedLinkSettingWidget):
     def time_const_down(self) -> float:
         """[s]"""
         return self._common_params.time_const_down.get() * 1e-3
+
+    def max_rot_speed(self) -> float:
+        """[rad/s]"""
+        return self._common_params.max_rot_speed.max_rot_speed()
 
     def rot_speed_coefs(self) -> Tuple[float, float]:
         """V = a w + b w^2 (V[V], w[rad/s])"""
@@ -151,10 +151,6 @@ class MotorCommonParamsWidget(QWidget):
         self._rows = QVBoxLayout()
         self.setLayout(self._rows)
 
-        # Max Rotation Speed
-        self.max_rot_speed = MaxRotationSpeedWidget(main, link_name)
-        self._rows.addWidget(self.max_rot_speed)
-
         # Param Getter Widgets
         self._param_rows = QVBoxLayout()
         self._rows.addLayout(self._param_rows)
@@ -190,38 +186,42 @@ class MotorCommonParamsWidget(QWidget):
         )
         self._param_rows.addWidget(self.time_const_down)
 
-    def is_valid(self) -> bool:
-        if not self.max_rot_speed.is_valid():
-            return False
+        # Max Rotation Speed
+        self.max_rot_speed = MaxRotationSpeedWidget(main, link_name)
+        self._rows.addWidget(self.max_rot_speed)
 
+    def is_valid(self) -> bool:
         if self.num_poles.get() % 2 == 1:
             q_error_named(self, PROPULSION_SYSTEM, "The number of poles of a brushless motor must be even.")
+            return False
+
+        if not self.max_rot_speed.is_valid():
             return False
 
         return True
 
     def copy_from(self, src: MotorCommonParamsWidget) -> None:
-        self.max_rot_speed.copy_from(src.max_rot_speed)
-
         for i in range(self._param_rows.count()):
             param_des: ParamGetterWidget = self._param_rows.itemAt(i).widget()
             param_src: ParamGetterWidget = src._param_rows.itemAt(i).widget()
             param_des.set(param_src.get())
 
+        self.max_rot_speed.copy_from(src.max_rot_speed)
+
     def dump_settings(self) -> dict:
         res = dict()
-
-        res[self.MAX_ROT_SPEED_KEY] = self.max_rot_speed.dump_settings()
 
         for i in range(self._param_rows.count()):
             param: ParamGetterWidget = self._param_rows.itemAt(i).widget()
             res[param.name()] = param.get()
 
+        res[self.MAX_ROT_SPEED_KEY] = self.max_rot_speed.dump_settings()
+
         return res
 
     def load_settings(self, data: dict) -> None:
-        self.max_rot_speed.load_settings(data[self.MAX_ROT_SPEED_KEY])
-
         for i in range(self._param_rows.count()):
             param: ParamGetterWidget = self._param_rows.itemAt(i).widget()
             param.set(data[param.name()])
+
+        self.max_rot_speed.load_settings(data[self.MAX_ROT_SPEED_KEY])
