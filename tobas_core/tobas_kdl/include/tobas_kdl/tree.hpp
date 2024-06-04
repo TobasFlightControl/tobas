849bd4a52d6078068b3966171aedd4816779fe9b
dohi <masa0u0masa1215@gmail.com>
2024-06-04 18:17:41 +0900

    .

--- tobas_core/tobas_kdl/include/tobas_kdl/tree.hpp
+++ tobas_core/tobas_kdl/include/tobas_kdl/tree.hpp
@@ -15,20 +15,12 @@ public:
   SegmentMap::const_iterator parent;
   std::vector<SegmentMap::const_iterator> children;
 
-  inline explicit TreeElement(const Segment& _segment, const SegmentMap::const_iterator& _parent, const size_t& _q_nr)
-    : segment(_segment), q_nr(_q_nr), parent(_parent)
-  {
-  }
+  inline explicit TreeElement(const Segment& _segment, const SegmentMap::const_iterator& _parent, const size_t& _q_nr);
 
-  inline static TreeElement Root(const std::string& root_name)
-  {
-    return TreeElement(root_name);
-  }
+  inline static TreeElement Root(const std::string& root_name);
 
 private:
-  inline explicit TreeElement(const std::string& name) : segment(name), q_nr(0)
-  {
-  }
+  inline explicit TreeElement(const std::string& name);
 };
 
 /**
@@ -81,55 +73,6 @@ public:
   void addTree(const Tree& tree, const std::string& hook_name);
 
   /**
-   * Request the total number of joints in the tree.\n
-   * Important: It is not the same as the total number of segments
-   * since a segment does not need to have a joint.
-   *
-   * @return total nr of joints
-   */
-  inline const size_t& getNrOfJoints() const
-  {
-    return nj_;
-  };
-
-  /**
-   * Request the total number of segments in the tree.
-   * @return total number of segments
-   */
-  inline const size_t& getNrOfSegments() const
-  {
-    return ns_;
-  };
-
-  /**
-   * Request the segment of the tree with name segment_name.
-   *
-   * @param segment_name the name of the requested segment
-   *
-   * @return constant iterator pointing to the requested segment
-   */
-  inline SegmentMap::const_iterator getSegment(const std::string& segment_name) const
-  {
-    return segments_.find(segment_name);
-  };
-
-  /**
-   * Request the root segment of the tree
-   *
-   * @return constant iterator pointing to the root segment
-   */
-  inline SegmentMap::const_iterator getRootSegment() const
-  {
-    return segments_.find(root_name_);
-  };
-
-  /* Request the name of the root segment. */
-  inline const std::string& getRootName() const
-  {
-    return getRootSegment()->first;
-  }
-
-  /**
    * Request the chain of the tree between chain_root and chain_tip.  The chain_root
    * and chain_tip can be in different branches of the tree, the chain_root can be
    * an ancestor of chain_tip, and chain_tip can be an ancestor of chain_root.
@@ -150,10 +93,12 @@ public:
    */
   void getSubTree(const std::string& segment_name, Tree& tree, bool root_mass_ok = false) const;
 
-  inline const SegmentMap& getSegments() const
-  {
-    return segments_;
-  }
+  inline const size_t& getNrOfJoints() const;
+  inline const size_t& getNrOfSegments() const;
+  inline SegmentMap::const_iterator getSegment(const std::string& segment_name) const;
+  inline SegmentMap::const_iterator getRootSegment() const;
+  inline const std::string& getRootName() const;
+  inline const SegmentMap& getSegments() const;
 
   friend std::ostream& operator<<(std::ostream& os, const Tree& arg);
 
@@ -164,4 +109,48 @@ private:
 
   void addTreeRecursive(const SegmentMap::const_iterator& seg, const std::string& hook_name);
 };
+
+inline TreeElement::TreeElement(const Segment& _segment, const SegmentMap::const_iterator& _parent, const size_t& _q_nr)
+  : segment(_segment), q_nr(_q_nr), parent(_parent)
+{
+}
+
+inline TreeElement TreeElement::Root(const std::string& root_name)
+{
+  return TreeElement(root_name);
+}
+
+inline TreeElement::TreeElement(const std::string& name) : segment(name), q_nr(0)
+{
+}
+
+inline const size_t& Tree::getNrOfJoints() const
+{
+  return nj_;
+}
+
+inline const size_t& Tree::getNrOfSegments() const
+{
+  return ns_;
+}
+
+inline SegmentMap::const_iterator Tree::getSegment(const std::string& segment_name) const
+{
+  return segments_.find(segment_name);
+}
+
+inline SegmentMap::const_iterator Tree::getRootSegment() const
+{
+  return segments_.find(root_name_);
+}
+
+inline const std::string& Tree::getRootName() const
+{
+  return getRootSegment()->first;
+}
+
+inline const SegmentMap& Tree::getSegments() const
+{
+  return segments_;
+}
 }  // namespace tobas_kdl
