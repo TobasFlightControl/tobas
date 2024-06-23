ca1972c67281aa155996456186652267ebeb9f6e
Masa0u0 <masa0u0masa1215@gmail.com>
2024-06-23 09:51:51 +0900

    edit: mixer.cpp

--- tobas_controllers/tobas_np_pid/src/mixer.cpp
+++ tobas_controllers/tobas_np_pid/src/mixer.cpp
@@ -89,7 +89,8 @@ VectorXd Mixer::solve(
 
   // EoM行列等式の右辺
   // TODO: H-forceを考慮
-  const auto trans_right = mass * cur_rot.inverse(tar_acc_W - tobas::kWorldGravity);
+  const Vector grav_W(0, 0, -tobas::kGravity);
+  const auto trans_right = mass * cur_rot.inverse(tar_acc_W - grav_W);
   const auto rot_right = I_B * tar_dgyro_B + cur_gyro_B * (I_B * cur_gyro_B);
   h_.head<3>() = trans_right.data;
   h_.tail<3>() = rot_right.data;
