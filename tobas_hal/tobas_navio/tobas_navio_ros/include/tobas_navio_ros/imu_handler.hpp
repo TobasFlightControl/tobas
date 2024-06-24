e58b524036f4106e972f1255ea0e6b40173cbea2
Masa0u0 <masa0u0masa1215@gmail.com>
2024-06-24 15:59:40 +0900

    IMUのカットオフ周波数を変更

--- tobas_hal/tobas_navio/tobas_navio_ros/include/tobas_navio_ros/imu_handler.hpp
+++ tobas_hal/tobas_navio/tobas_navio_ros/include/tobas_navio_ros/imu_handler.hpp
@@ -15,7 +15,7 @@ class ImuHandler : public BaseSensorNode
 {
   // Constants
   static constexpr size_t kSamplingRate = 400;         // [Hz]
-  static constexpr double kHpfCutoff = 10.;            // [Hz] (G(1Hz) ~ 0.1, G(20Hz) ~ 0.9)
+  static constexpr double kHpfCutoff = 30.;            // [Hz] (G(3Hz) ~ 0.1, G(100Hz) ~ 0.95)
   static constexpr size_t kNoiseStatTimeWindow = 500;  // [ms]
   static constexpr size_t kWindowSize = kSamplingRate * kNoiseStatTimeWindow / 1000;
 
