0800c2ba3ee8dcfe6bb8bdf0fc86051c1e492b64
dohi <masa0u0masa1215@gmail.com>
2024-07-01 18:05:24 +0900

    add] ground_force_controller.xpp

---
+++ tobas_legged_robot/tobas_legged_tools/include/tobas_legged_tools/point_contacts/ground_force_controller.hpp
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <tobas_std_tools/range.hpp>
+#include <tobas_kdl/tree.hpp>
+#include <tobas_kdl/treefksolverpos.hpp>
+#include <tobas_kdl/treejnttoinertiasolver.hpp>
+#include <tobas_kdl/treeboundingboxsolver.hpp>
+#include <tobas_linear_control/mpc/linear_dense.hpp>
+#include <tobas_linear_control/c2d/rk4.hpp>
+#include <tobas_legged_tools/point_contacts/linear_dynamics.hpp>
+
+namespace lr_tools
+{
+struct GroundForceControllerConfig
+{
+  double friction_coef;  // [-]
+
+  double min_normal_force;  // [N]
+  double max_normal_force;  // [N]
+
+  double prediction_horizon;  // [s]
+  size_t prediction_steps;
+
+  double attitude_error_decay;  // [s]
+  double height_error_decay;    // [s]
+  double yawrate_error_decay;   // [s]
+  double velocity_error_decay;  // [s]
+
+  double attitude_weight;
+  double height_weight;
+  double gyro_weight;
+  double velocity_weight;
+  int force_weight_log10;
+  int force_rate_weight_log10;
+};
+
+class GroundForceController
+{
+  static constexpr size_t kCtrlSize = 9;
+
+public:
+  explicit GroundForceController(const kdl::Tree& tree, const std::vector<std::string>& foot_names);
+
+  void updateInternalDataStructures();
+
+  bool configure(const GroundForceControllerConfig& cfg);
+
+  void solve(
+    const double& cur_z,
+    const kdl::Vector& cur_vel,
+    const kdl::Vector& cur_gyro,
+    const double& tar_z,
+    const double& tar_yawrate,
+    const double& tar_vx,
+    const double& tar_vy,
+    const std::vector<double>& roll_pred,
+    const std::vector<double>& pitch_pred,
+    const std::vector<kdl::JntArray>& q_pred,
+    const std::vector<std::vector<bool>>& is_stand_pred);
+
+  inline const double& timeStep() const;
+
+  inline Eigen::Vector3d optimalReactionForce(size_t leg) const;
+  inline Eigen::Vector3d optimalDGyro() const;
+  inline Eigen::Vector3d optimalAccel() const;
+
+  inline double nextRoll() const;
+  inline double nextPitch() const;
+  inline double nextAltitude() const;
+  inline Eigen::Vector3d nextGyro() const;
+  inline Eigen::Vector3d nextVel() const;
+
+private:
+  const kdl::Tree& tree_;
+  const std::vector<std::string> foot_names_;
+  const size_t nc_;  // The number of contact points
+
+  // Config
+  double friction_coef_;
+  tobas_std::Range<double> normal_force_range_;
+
+  kdl::TreeJntToInertiaSolver inertia_solver_;
+  kdl::TreeBoundingBoxSolver bb_solver_;
+  PointContactsLinearDynamics cont_;
+  ctrl::LinearDenseMPC mpc_;
+  ctrl::C2D_RK4 c2d_;
+  Eigen::VectorXd x_rate_;
+  Eigen::VectorXd x_next_;
+
+  void initializeMPC();
+  double calcMass();
+  double calcSizeScale();
+  Eigen::MatrixXd makeCz();
+  ctrl::LinearEquation makeInputConstraint();
+};
+
+inline const double& GroundForceController::timeStep() const
+{
+  return mpc_.time_step;
+}
+
+inline Eigen::Vector3d GroundForceController::optimalReactionForce(size_t leg) const
+{
+  return mpc_.optimalControlInput().segment<3>(3 * leg);
+}
+
+inline Eigen::Vector3d GroundForceController::optimalDGyro() const
+{
+  return x_rate_.segment<3>(PointContactsLinearDynamics::kGyroXIdx);
+}
+
+inline Eigen::Vector3d GroundForceController::optimalAccel() const
+{
+  return x_rate_.segment<3>(PointContactsLinearDynamics::kVelXIdx);
+}
+
+inline double GroundForceController::nextRoll() const
+{
+  return x_next_(PointContactsLinearDynamics::kRollIdx);
+}
+
+inline double GroundForceController::nextPitch() const
+{
+  return x_next_(PointContactsLinearDynamics::kPitchIdx);
+}
+
+inline double GroundForceController::nextAltitude() const
+{
+  return x_next_(PointContactsLinearDynamics::kAltIdx);
+}
+
+inline Eigen::Vector3d GroundForceController::nextGyro() const
+{
+  return x_next_.segment<3>(PointContactsLinearDynamics::kGyroXIdx);
+}
+
+inline Eigen::Vector3d GroundForceController::nextVel() const
+{
+  return x_next_.segment<3>(PointContactsLinearDynamics::kVelXIdx);
+}
+}  // namespace lr_tools
---
+++ tobas_legged_robot/tobas_legged_tools/src/point_contacts/ground_force_controller.cpp
@@ -0,0 +1,173 @@
+#include <tobas_std_tools/universal_constants.hpp>
+#include <tobas_eigen_tools/core.hpp>
+#include <tobas_linear_control/util.hpp>
+
+#include "../include/tobas_legged_tools/point_contacts/ground_force_controller.hpp"
+
+using namespace std;
+using namespace Eigen;
+
+namespace lr_tools
+{
+GroundForceController::GroundForceController(const kdl::Tree& tree, const vector<string>& foot_names)
+  : tree_(tree),
+    foot_names_(foot_names),
+    nc_(foot_names.size()),
+    inertia_solver_(tree),
+    bb_solver_(tree),
+    cont_(tree, foot_names),
+    c2d_(cont_.stateSize(), cont_.inputSize())
+{
+  updateInternalDataStructures();
+}
+
+void GroundForceController::updateInternalDataStructures()
+{
+  inertia_solver_.updateInternalDataStructures();
+  bb_solver_.updateInternalDataStructures();
+
+  cont_.updateInternalDataStructures();
+
+  initializeMPC();
+}
+
+bool GroundForceController::configure(const GroundForceControllerConfig& cfg)
+{
+  // TODO: 有効な値かチェック
+
+  friction_coef_ = cfg.friction_coef;
+  normal_force_range_.lower = cfg.min_normal_force;
+  normal_force_range_.upper = cfg.max_normal_force;
+
+  mpc_.discrete_dynamics.resize(cfg.prediction_steps);
+  mpc_.prediction_steps = cfg.prediction_steps;
+  mpc_.input_steps = cfg.prediction_steps;
+  mpc_.time_step = cfg.prediction_horizon / cfg.prediction_steps;
+
+  mpc_.decay_time_consts.segment<2>(cont_.kRollIdx).fill(cfg.attitude_error_decay);
+  mpc_.decay_time_consts(cont_.kAltIdx) = cfg.height_error_decay;
+  mpc_.decay_time_consts.segment<2>(cont_.kGyroXIdx).setZero();
+  mpc_.decay_time_consts(cont_.kGyroZIdx) = cfg.yawrate_error_decay;
+  mpc_.decay_time_consts.segment<2>(cont_.kVelXIdx).fill(cfg.velocity_error_decay);
+  mpc_.decay_time_consts(cont_.kVelZIdx) = 0.;
+
+  mpc_.control_weight.segment<2>(cont_.kRollIdx).fill(cfg.attitude_weight);
+  mpc_.control_weight(cont_.kAltIdx) = cfg.height_weight;
+  mpc_.control_weight.segment<3>(cont_.kGyroXIdx).fill(cfg.gyro_weight);
+  mpc_.control_weight.segment<3>(cont_.kVelXIdx).fill(cfg.velocity_weight);
+
+  mpc_.input_weight.fill(exp10(cfg.force_weight_log10));
+  mpc_.input_rate_weight.fill(exp10(cfg.force_rate_weight_log10));
+
+  mpc_.input_rate_eqs.resize(cfg.prediction_steps, ctrl::LinearEquation(cont_.inputSize(), 0));
+  mpc_.input_eqs.resize(cfg.prediction_steps, ctrl::LinearEquation(cont_.inputSize(), 0));
+  mpc_.control_eqs.resize(cfg.prediction_steps, ctrl::LinearEquation(kCtrlSize, 0));
+
+  mpc_.input_rate_ineqs.resize(cfg.prediction_steps, ctrl::LinearEquation(cont_.inputSize(), 0));
+  mpc_.input_ineqs.resize(cfg.prediction_steps, makeInputConstraint());
+  mpc_.control_ineqs.resize(cfg.prediction_steps, ctrl::LinearEquation(kCtrlSize, 0));
+
+  return true;
+}
+
+void GroundForceController::solve(
+  const double& cur_z,
+  const kdl::Vector& cur_vel,
+  const kdl::Vector& cur_gyro,
+  const double& tar_z,
+  const double& tar_yawrate,
+  const double& tar_vx,
+  const double& tar_vy,
+  const vector<double>& roll_pred,
+  const vector<double>& pitch_pred,
+  const vector<kdl::JntArray>& q_pred,
+  const vector<vector<bool>>& is_stand_pred)
+{
+  assert(q_pred.size() == mpc_.prediction_steps);
+  assert(roll_pred.size() == mpc_.prediction_steps);
+  assert(pitch_pred.size() == mpc_.prediction_steps);
+  assert(is_stand_pred.size() == mpc_.prediction_steps);
+
+  // Update dynamics
+  // ステップ0の連続時間ダイナミクスを後の予測で使うため，未来から逆順で処理する．
+  for (Index k = mpc_.prediction_steps - 1; k >= 0; --k)
+  {
+    cont_.update(roll_pred[k], pitch_pred[k], q_pred[k], is_stand_pred[k]);
+    mpc_.discrete_dynamics[k] = c2d_.convert(cont_, mpc_.time_step);
+  }
+
+  // Update current state
+  mpc_.current_state << roll_pred[0], pitch_pred[0], cur_z, cur_gyro.x(), cur_gyro.y(), cur_gyro.z(), cur_vel.x(),
+    cur_vel.y(), cur_vel.z(), tobas_std::kGravity;
+
+  // Update set state
+  mpc_.set_state << 0, 0, tar_z, 0, 0, tar_yawrate, tar_vx, tar_vy, 0;
+
+  // Solve MPC
+  mpc_.solve();
+
+  // Prediction
+  const auto& u = mpc_.optimalControlInput();
+  x_rate_ = cont_.A * mpc_.current_state + cont_.B * u;
+  x_next_ = mpc_.discrete_dynamics[0].A * mpc_.current_state + mpc_.discrete_dynamics[0].B * u;
+}
+
+void GroundForceController::initializeMPC()
+{
+  mpc_.Cz = makeCz();
+  mpc_.decay_time_consts.resize(kCtrlSize);
+  mpc_.control_weight.resize(kCtrlSize);
+  mpc_.input_weight.resize(cont_.inputSize());
+  mpc_.input_rate_weight.resize(cont_.inputSize());
+
+  const double rpy_sc = M_PI;
+  const double size_sc = calcSizeScale();
+  const double gyro_sc = M_PI;
+  const double vel_sc = sqrt(tobas_std::kGravity * size_sc);  // フルード数の定義を元に決定
+  mpc_.state_scale.resize(cont_.stateSize());
+  mpc_.input_scale.resize(cont_.inputSize());
+  mpc_.control_scale.resize(kCtrlSize);
+  mpc_.state_scale << rpy_sc, rpy_sc, size_sc, gyro_sc, gyro_sc, gyro_sc, vel_sc, vel_sc, vel_sc, tobas_std::kGravity;
+  mpc_.input_scale.fill(calcMass() * tobas_std::kGravity);
+  mpc_.control_scale << rpy_sc, rpy_sc, size_sc, gyro_sc, gyro_sc, gyro_sc, vel_sc, vel_sc, vel_sc;
+
+  mpc_.current_state.resize(cont_.stateSize());
+  mpc_.set_state.resize(kCtrlSize);
+}
+
+double GroundForceController::calcMass()
+{
+  inertia_solver_.JntToCart(kdl::JntArray::Zero(tree_.getNrOfJoints()));
+  return inertia_solver_.getInertia().getMass();
+}
+
+double GroundForceController::calcSizeScale()
+{
+  bb_solver_.solve(kdl::JntArray::Zero(tree_.getNrOfJoints()));
+  return bb_solver_.diagonalLength();
+}
+
+MatrixXd GroundForceController::makeCz()
+{
+  MatrixXd res = MatrixXd::Zero(kCtrlSize, cont_.stateSize());
+  res.diagonal().head<kCtrlSize>().setOnes();
+  return res;
+}
+
+ctrl::LinearEquation GroundForceController::makeInputConstraint()
+{
+  constexpr size_t kNumConstraintsPerLeg = 6;  // 足1本あたりのハード制約の個数
+
+  // 足1本
+  MatrixXd F_(kNumConstraintsPerLeg, 3);
+  F_ << 0, 0, -1, 0, 0, 1, -1, 0, -friction_coef_, 1, 0, -friction_coef_, 0, -1, -friction_coef_, 0, 1, -friction_coef_;
+  VectorXd f_(kNumConstraintsPerLeg);
+  f_ << -normal_force_range_.lower, normal_force_range_.upper, 0, 0, 0, 0;
+
+  // 足4本
+  const auto F = eigen_tools::blockDiag(F_, nc_);
+  const auto f = eigen_tools::tile(f_, nc_, 0);
+
+  return ctrl::LinearEquation(F, f);
+}
+}  // namespace lr_tools
