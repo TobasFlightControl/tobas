3c6227a15a263e3d2e5bf72c4501f4d98ac2176b
dohi <masa0u0masa1215@gmail.com>
2024-07-02 12:23:51 +0900

    refactor: TreJntToInertiaSolver

--- tobas_core/tobas_kdl/src/treejnttoinertiasolver.cpp
+++ tobas_core/tobas_kdl/src/treejnttoinertiasolver.cpp
@@ -15,52 +15,47 @@ void TreeJntToInertiaSolver::updateInternalDataStructures()
 {
   super::updateInternalDataStructures();
 
-  const auto& segments = tree_.getSegments();
-  for (auto seg = segments.begin(); seg != segments.end(); ++seg)
+  X_.clear();
+  I_.clear();
+  for (const auto& [seg_name, _] : tree_.getSegments())
   {
-    const auto& seg_name = seg->first;
     X_[seg_name] = Frame::Identity();
     I_[seg_name] = RigidBodyInertia::Zero();
   }
 }
 
-int TreeJntToInertiaSolver::JntToCart(const JntArray& q_in)
+int TreeJntToInertiaSolver::JntToCart(const JntArray& q)
 {
   if (!isUpToDate())
     return setDefaultError(E_NOT_UP_TO_DATE);
-  if (q_in.rows() != nj_)
+  if (q.rows() != nj_)
     return setDefaultError(E_SIZE_MISMATCH);
 
-  const auto root = tree_.getRootSegment();
-  step(root, q_in);
+  const auto root_it = tree_.getRootSegment();
+  step(root_it, q);
 
   return setDefaultError(E_NOERROR);
 }
 
-void TreeJntToInertiaSolver::step(const SegmentMap::const_iterator& segment, const JntArray& q)
+void TreeJntToInertiaSolver::step(const SegmentMap::const_iterator& cur_it, const JntArray& q)
 {
-  const auto& seg = segment->second.segment;
-  const auto& seg_name = segment->first;
-  const auto& par_name = segment->second.parent->first;
+  const auto& cur_name = cur_it->first;
+  const auto& cur_ele = cur_it->second;
+  const auto& cur_seg = cur_ele.segment;
+  const auto& par_it = cur_ele.parent;
+  const auto& par_name = par_it->first;
 
   // Forward calculation
-  const auto& j = segment->second.q_nr;
-  const auto& qj = seg.getJoint().type == Joint::Fixed ? 0. : q(j);
-  X_.at(seg_name) = seg.pose(qj);
-  I_.at(seg_name) = seg.getInertia();
+  const auto& qj = cur_seg.getJoint().type == Joint::Fixed ? 0. : q(cur_ele.q_nr);
+  X_.at(cur_name) = cur_seg.pose(qj);
+  I_.at(cur_name) = cur_seg.getInertia();
 
   // Propagate calculations over each child segment
-  SegmentMap::const_iterator child;
-  for (size_t i = 0; i < segment->second.children.size(); ++i)
-  {
-    child = segment->second.children[i];
-    step(child, q);
-  }
-
-  // cout << seg_name << ": " << I_.at(seg_name).getMass() << " [kg]" << endl;
+  for (const auto& child_it : cur_ele.children)
+    step(child_it, q);
 
   // Backward calculation
-  if (segment != tree_.getRootSegment())
-    I_.at(par_name) += X_.at(seg_name) * I_.at(seg_name);
+  if (cur_it != tree_.getRootSegment())
+    I_.at(par_name) += X_.at(cur_name) * I_.at(cur_name);
 }
 }  // namespace kdl
