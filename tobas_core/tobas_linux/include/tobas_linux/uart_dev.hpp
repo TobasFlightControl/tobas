5a28bebc200afa7d15b3db5d0df6e53bc2779351
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-06 00:25:53 +0900

    edit: uart_dev.xpp

--- tobas_core/tobas_linux/include/tobas_linux/uart_dev.hpp
+++ tobas_core/tobas_linux/include/tobas_linux/uart_dev.hpp
@@ -2,21 +2,40 @@
 
 #include <cinttypes>
 #include <cstddef>
+#include <asm/termbits.h>
 
 namespace linux
 {
 class UARTdev
 {
+  static constexpr uint32_t kDefaultBaudRate = 9600;  // [bps]
+
 public:
+  enum parity_mode_t : tcflag_t
+  {
+    PARITY_ODD,
+    PARITY_EVEN,
+  };
+
   explicit UARTdev();
   ~UARTdev();
 
-  bool initialize(const char* uart_dev, uint32_t baudrate);
+  bool initialize(const char* uart_dev);
+
+  bool setBaudRate(uint32_t baud_rate);
+  bool setDataBits(uint8_t data_bits);
+  bool enableTwoStopBits();
+  bool enableParity(parity_mode_t mode);
+  bool enableHangupClose();
 
   bool send(const uint8_t* data, size_t length);
   bool receive(uint8_t* data, size_t length);
 
 private:
   int uart_fd_;
+  struct termios2 options_;
+
+  bool getConfig();
+  bool setConfig();
 };
 }  // namespace linux
--- tobas_core/tobas_linux/src/uart_dev.cpp
+++ tobas_core/tobas_linux/src/uart_dev.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
-#include <termios.h>
+#include <sys/ioctl.h>
 
 #include "../include/tobas_linux/uart_dev.hpp"
 
@@ -19,8 +19,9 @@ UARTdev::~UARTdev()
   close(uart_fd_);
 }
 
-bool UARTdev::initialize(const char* uart_dev, uint32_t baudrate)
+bool UARTdev::initialize(const char* uart_dev)
 {
+  // Open UART device
   uart_fd_ = open(uart_dev, O_RDWR | O_NOCTTY | O_NDELAY);
   if (uart_fd_ < 0)
   {
@@ -28,22 +29,99 @@ bool UARTdev::initialize(const char* uart_dev, uint32_t baudrate)
     return false;
   }
 
-  termios options;
-  tcgetattr(uart_fd_, &options);
-  options.c_cflag = baudrate | CS8 | CLOCAL | CREAD;
-  options.c_iflag = IGNPAR;
-  options.c_oflag = 0;
-  options.c_lflag = 0;
+  // Get the current configuration of the serial interface
+  if (!getConfig())
+    return false;
 
-  tcflush(uart_fd_, TCIFLUSH);
-  tcsetattr(uart_fd_, TCSANOW, &options);
+  // Set baud rate
+  options_.c_cflag &= ~CBAUD;            // Remove current baud rate
+  options_.c_cflag |= BOTHER;            // Allow custom baud rate using int input
+  options_.c_ispeed = kDefaultBaudRate;  // Set the input baud rate
+  options_.c_ospeed = kDefaultBaudRate;  // Set the output baud rate
+
+  // Configure the serial port
+  options_.c_cflag &= ~CSIZE;   // Clear data bit size
+  options_.c_cflag |= CS8;      // 8 bit size
+  options_.c_cflag &= ~CSTOPB;  // 1 stop bit
+  options_.c_cflag |= CREAD;    // Enable receiver
+  options_.c_cflag &= ~PARENB;  // No parity
+  options_.c_cflag |= CLOCAL;   // Set local mode
+
+  // Set the new configuration of the serial interface
+  if (!setConfig())
+    return false;
 
   return true;
 }
 
+bool UARTdev::setBaudRate(uint32_t baud_rate)
+{
+  options_.c_ispeed = baud_rate;
+  options_.c_ospeed = baud_rate;
+  return setConfig();
+}
+
+bool UARTdev::setDataBits(uint8_t data_bits)
+{
+  tcflag_t flag;
+  switch (data_bits)
+  {
+    case 5:
+      flag = CS5;
+      break;
+    case 6:
+      flag = CS6;
+      break;
+    case 7:
+      flag = CS7;
+      break;
+    case 8:
+      flag = CS8;
+      break;
+    default:
+      cerr << "Invalid data bit size." << endl;
+      return false;
+  }
+
+  options_.c_cflag &= ~CSIZE;
+  options_.c_cflag |= flag;
+  return setConfig();
+}
+
+bool UARTdev::enableTwoStopBits()
+{
+  options_.c_cflag |= CSTOPB;
+  return setConfig();
+}
+
+bool UARTdev::enableParity(parity_mode_t mode)
+{
+  switch (mode)
+  {
+    case PARITY_ODD:
+      options_.c_cflag |= PARODD;
+      break;
+    case PARITY_EVEN:
+      options_.c_cflag &= ~PARODD;
+      break;
+    default:
+      cerr << "Invalid parity mode." << endl;
+      return false;
+  }
+
+  options_.c_cflag |= PARENB;
+  return setConfig();
+}
+
+bool UARTdev::enableHangupClose()
+{
+  options_.c_cflag &= HUPCL;
+  return setConfig();
+}
+
 bool UARTdev::send(const uint8_t* data, size_t length)
 {
-  if (write(uart_fd_, data, length) != length)
+  if (write(uart_fd_, data, length) != static_cast<ssize_t>(length))
   {
     cerr << "UART TX error." << endl;
     return false;
@@ -53,9 +131,29 @@ bool UARTdev::send(const uint8_t* data, size_t length)
 
 bool UARTdev::receive(uint8_t* data, size_t length)
 {
-  if (read(uart_fd_, data, length) != length)
+  if (read(uart_fd_, data, length) != static_cast<ssize_t>(length))
   {
     cerr << "UART RX error." << endl;
+    return false;
+  }
+  return true;
+}
+
+bool UARTdev::getConfig()
+{
+  if (ioctl(uart_fd_, TCGETS2, &options_) < 0)
+  {
+    cerr << "Failed to get serial port settings." << endl;
+    return false;
+  }
+  return true;
+}
+
+bool UARTdev::setConfig()
+{
+  if (ioctl(uart_fd_, TCSETS2, &options_) < 0)
+  {
+    cerr << "Failed to set serial port settings." << endl;
     return false;
   }
   return true;
