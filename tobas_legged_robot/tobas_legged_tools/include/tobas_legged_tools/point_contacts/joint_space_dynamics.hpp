2d9aebbd6a7cff00b2f56db9de3f6fb0b14c1ea6
dohi <masa0u0masa1215@gmail.com>
2024-07-09 13:32:47 +0900

    add: joint_state_dynamics.xpp

---
+++ tobas_legged_robot/tobas_legged_tools/include/tobas_legged_tools/point_contacts/joint_space_dynamics.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <tobas_std_tools/range.hpp>
+#include <tobas_quadprog/dual_active_set.hpp>
+#include <tobas_kdl/euler.hpp>
+#include <tobas_kdl/jntspaceinertiamatrix.hpp>
+#include <tobas_kdl/treejnttojacsolver.hpp>
+#include <tobas_kdl/treeidsolver_rne.hpp>
+#include <tobas_kdl/treejntspaceinertiasolver.hpp>
+#include <tobas_kdl/treejnttoinertiasolver.hpp>
+#include <tobas_kdl/treeboundingboxsolver.hpp>
+
+namespace lr_tools
+{
+struct JointSpaceDynamicsConfig
+{
+  double friction_coef;  // [-]
+
+  double min_normal_force;  // [N]
+  double max_normal_force;  // [N]
+
+  double force_weight;  // 地面反力の参照値からのエラーに対するペナルティ
+  double base_weight;   // 浮遊リンクの加速度の参照値からのエラーに対するペナルティ
+};
+
+/* 浮遊リンクの加速度を実現するための地面反力を求める． */
+class JointSpaceDynamics
+{
+  static constexpr size_t kPosIdx = 0;
+  static constexpr size_t kYawIdx = 3;
+  static constexpr size_t kPitchIdx = 4;
+  static constexpr size_t kRollIdx = 5;
+
+  static constexpr size_t kBaseDoF = 6;         // 浮遊リンクの自由度
+  static constexpr size_t kIneqSizePerLeg = 6;  // 足1本あたりの不等式制約の個数
+
+  static constexpr double kYawAngle = 0.;  // フットプリント座標系で考えるため，ヨー角は常にゼロ．
+
+public:
+  explicit JointSpaceDynamics(const kdl::Tree& tree, const std::vector<std::string>& foot_names);
+
+  void updateInternalDataStructures();
+
+  bool configure(const JointSpaceDynamicsConfig& cfg);
+
+  void solve(
+    const double& roll,
+    const double& pitch,
+    const kdl::Vector& cur_vel,
+    const kdl::Vector& cur_gyro,
+    const kdl::JntArray& cur_q,
+    const kdl::JntArray& cur_qd,
+    const kdl::Vector& tar_acc,
+    const kdl::Euler& tar_rpydd,
+    const kdl::JntArray& tar_qdd,
+    const std::vector<kdl::Vector>& tar_force,
+    const std::vector<bool>& is_stand);
+
+  inline kdl::Vector getForce(size_t leg) const;
+  inline kdl::JntArray getEffort() const;
+
+private:
+  const kdl::Tree& tree_raw_;
+  const std::vector<std::string> foot_names_;
+  const size_t nc_, force_size_;
+
+  // Config
+  double friction_coef_;
+  tobas_std::Range<double> normal_force_range_;
+
+  Eigen::VectorXd f_out_;    // size = 3 * nc_
+  Eigen::VectorXd eff_out_;  // size = kBaseDoF + nj_raw_
+
+  kdl::Tree tree_;  // 浮遊リンク付きのツリー
+  size_t nj_raw_, nj_;
+  kdl::JntArray cur_q_, cur_qd_, tar_qdd_;
+  Eigen::MatrixXd J_;                                  // 足先位置のヤコビアンを並べたもの
+  Eigen::VectorXd f_ref_;                              // 地面反力の参照値
+  Eigen::Matrix<double, kIneqSizePerLeg, 3> CI_part_;  // (memo: 1-41)のA
+  Eigen::Matrix<double, kIneqSizePerLeg, 1> ci0_part_stand_, ci0_part_swing_;
+  quadprog::DualActiveSetSolver qp_;
+
+  kdl::TreeJntToJacSolver jac_solver_;
+  kdl::TreeIdSolver_RNE rne_;
+  kdl::TreeJntSpaceInertiaSolver mass_solver_;
+  kdl::TreeJntToInertiaSolver inertia_solver_;
+  kdl::TreeBoundingBoxSolver bb_solver_;
+
+  double calcMass();
+  double calcSizeScale();
+};
+
+inline kdl::Vector JointSpaceDynamics::getForce(size_t leg) const
+{
+  return kdl::Vector(f_out_.segment<3>(3 * leg));
+}
+
+inline kdl::JntArray JointSpaceDynamics::getEffort() const
+{
+  return kdl::JntArray(eff_out_.tail(nj_raw_));
+}
+}  // namespace lr_tools
---
+++ tobas_legged_robot/tobas_legged_tools/src/point_contacts/joint_space_dynamics.cpp
@@ -0,0 +1,182 @@
+#include <tobas_std_tools/universal_constants.hpp>
+#include <tobas_eigen_tools/core.hpp>
+#include <tobas_eigen_tools/geometry.hpp>
+
+#include "../../include/tobas_legged_tools/point_contacts/joint_space_dynamics.hpp"
+
+using namespace std;
+using namespace Eigen;
+namespace et = eigen_tools;
+
+namespace lr_tools
+{
+JointSpaceDynamics::JointSpaceDynamics(const kdl::Tree& tree, const vector<string>& foot_names)
+  : tree_raw_(tree),
+    foot_names_(foot_names),
+    nc_(foot_names.size()),
+    force_size_(3 * nc_),
+    f_ref_(force_size_),
+    jac_solver_(tree_),
+    rne_(tree_),
+    mass_solver_(tree_),
+    inertia_solver_(tree_),
+    bb_solver_(tree_)
+{
+  CI_part_.setZero();
+  CI_part_(0, 2) = -1;
+  CI_part_(1, 2) = 1;
+  CI_part_(2, 0) = -1;
+  CI_part_(3, 0) = 1;
+  CI_part_(4, 1) = -1;
+  CI_part_(5, 1) = 1;
+
+  ci0_part_stand_.setZero();
+  ci0_part_swing_.setZero();  // 遊脚の反力の等式制約は反力の範囲を0にすることで表現
+
+  qp_.resize(force_size_ + kBaseDoF, kBaseDoF, kIneqSizePerLeg * nc_);
+  qp_.setZero();
+
+  updateInternalDataStructures();
+}
+
+void JointSpaceDynamics::updateInternalDataStructures()
+{
+  tree_ = tree_raw_;  // TODO: 浮遊リンクを追加
+
+  // nj_raw_ = tree_raw_.getNrOfJoints();
+  nj_raw_ = 12;  // TODO
+  nj_ = tree_.getNrOfJoints();
+  J_.resize(force_size_, nj_);
+
+  jac_solver_.updateInternalDataStructures();
+  rne_.updateInternalDataStructures();
+  mass_solver_.updateInternalDataStructures();
+  inertia_solver_.updateInternalDataStructures();
+  bb_solver_.updateInternalDataStructures();
+
+  cur_q_.resize(nj_);
+  cur_qd_.resize(nj_);
+  tar_qdd_.resize(nj_);
+
+  qp_.x_scale.head(force_size_).fill(calcMass() * tobas_std::kGravity / nc_);
+  qp_.x_scale.segment<3>(force_size_).fill(sqrt(tobas_std::kGravity * calcSizeScale()));  // フルード数に基づく
+  qp_.x_scale.segment<3>(force_size_ + 3).fill(M_PI);
+}
+
+bool JointSpaceDynamics::configure(const JointSpaceDynamicsConfig& cfg)
+{
+  // TODO: 有効な値かチェック
+
+  CI_part_.block<4, 1>(2, 2).fill(-cfg.friction_coef);
+  const MatrixXd CI_left = et::blockDiag(CI_part_, nc_);
+  const MatrixXd CI_right = MatrixXd::Zero(kIneqSizePerLeg * nc_, kBaseDoF);
+  qp_.problem.A = et::concat(CI_left, CI_right, 1);
+
+  ci0_part_stand_(0) = -cfg.min_normal_force;
+  ci0_part_stand_(1) = cfg.max_normal_force;
+
+  qp_.problem.P.diagonal().head(force_size_).fill(cfg.force_weight);
+  qp_.problem.P.diagonal().tail<kBaseDoF>().fill(cfg.base_weight);
+
+  return true;
+}
+
+void JointSpaceDynamics::solve(
+  const double& roll,
+  const double& pitch,
+  const kdl::Vector& cur_vel,
+  const kdl::Vector& cur_gyro,
+  const kdl::JntArray& cur_q,
+  const kdl::JntArray& cur_qd,
+  const kdl::Vector& tar_acc,
+  const kdl::Euler& tar_rpydd,
+  const kdl::JntArray& tar_qdd,
+  const vector<kdl::Vector>& tar_force,
+  const vector<bool>& is_stand)
+{
+  assert(cur_q.size() == nj_raw_);
+  assert(cur_qd.size() == nj_raw_);
+  assert(tar_qdd.size() == nj_raw_);
+  assert(tar_force.size() == nc_);
+  assert(is_stand.size() == nc_);
+
+  // 浮遊リンクを含む関節状態の現在値を更新
+  cur_q_.data.segment<3>(kPosIdx).setZero();  // TODO: Z座標を入れなくても問題ない？
+  cur_q_(kRollIdx) = roll;
+  cur_q_(kPitchIdx) = pitch;
+  cur_q_(kYawIdx) = kYawAngle;
+  cur_qd_.data.segment<3>(kPosIdx) = cur_vel.data;
+  cur_qd_.data.segment<3>(kYawIdx) = et::eulerrateFromAngvelGlobal(cur_gyro.data, pitch, kYawAngle).reverse();
+  cur_q_.data.tail(nj_raw_) = cur_q.data;
+  cur_qd_.data.tail(nj_raw_) = cur_qd.data;
+
+  // ヤコビアンを更新
+  for (size_t l = 0; l < nc_; ++l)
+  {
+    if (jac_solver_.JntToJac(cur_q_, foot_names_[l]) < 0)
+      throw runtime_error("Jacobian solver failed: " + jac_solver_.errorMessage());
+    J_.block(3 * l, 0, 3, nj_) = jac_solver_.getJacobian().data.topRows<3>();  // 並進部分のみ取り出す
+  }
+
+  // 浮遊リンクを含む関節角速度の目標値を更新
+  tar_qdd_.data.segment<3>(kPosIdx) = tar_acc.data;
+  tar_qdd_.data(kRollIdx) = tar_rpydd.roll;
+  tar_qdd_.data(kPitchIdx) = tar_rpydd.pitch;
+  tar_qdd_.data(kYawIdx) = tar_rpydd.yaw;
+  tar_qdd_.data.tail(nj_raw_) = tar_qdd.data;
+
+  // 地面反力の参照値を更新
+  for (size_t l = 0; l < nc_; ++l)
+    f_ref_.segment<3>(3 * l) = tar_force[l].data;
+
+  // 外力項を除いたトルクを計算
+  if (rne_.CartToJnt(cur_q_, cur_qd_, tar_qdd_) < 0)
+    throw runtime_error("RNE failed: " + rne_.errorMessage());
+
+  // QPPを作成
+  const Matrix<double, kBaseDoF, Dynamic> Jt_base = J_.leftCols<kBaseDoF>().transpose();
+  if (mass_solver_.JntToMass(cur_q_) < 0)
+    throw runtime_error("Mass solver failed: " + mass_solver_.errorMessage());
+  const Matrix<double, kBaseDoF, kBaseDoF> Mb = mass_solver_.getMass().data.topLeftCorner<kBaseDoF, kBaseDoF>();
+
+  qp_.problem.G.leftCols(force_size_) = Jt_base;
+  qp_.problem.G.rightCols<kBaseDoF>() = -Mb;
+
+  qp_.problem.h = rne_.getEfforts().data.head<kBaseDoF>() - Jt_base * f_ref_;
+
+  for (size_t l = 0; l < nc_; ++l)
+  {
+    if (is_stand[l])
+      qp_.problem.b.segment<kIneqSizePerLeg>(kIneqSizePerLeg * l) =
+        ci0_part_stand_ - CI_part_ * f_ref_.segment<3>(3 * l);
+    else
+      qp_.problem.b.segment<kIneqSizePerLeg>(kIneqSizePerLeg * l) =
+        ci0_part_swing_ - CI_part_ * f_ref_.segment<3>(3 * l);
+  }
+
+  // QPPを解く
+  const VectorXd x = qp_.solve();
+
+  // 地面反力と浮遊リンクの加速度の残渣を取得
+  const VectorXd f_res = x.head(force_size_);
+  const VectorXd qdd_res = x.tail<kBaseDoF>();
+
+  // 修正された地面反力と関節トルクを計算
+  f_out_ = f_ref_ + x.head(force_size_);
+  eff_out_ = rne_.getEfforts().data - J_.transpose() * f_out_;
+  eff_out_.head<kBaseDoF>() += Mb * qdd_res;                                       // ベースの修正分
+  assert(et::isClose(eff_out_.head<kBaseDoF>().eval(), Vector6d::Zero().eval()));  // ベースのレンチは0になるはず
+}
+
+double JointSpaceDynamics::calcMass()
+{
+  inertia_solver_.JntToCart(kdl::JntArray::Zero(nj_));
+  return inertia_solver_.getInertia().getMass();
+}
+
+double JointSpaceDynamics::calcSizeScale()
+{
+  bb_solver_.solve(kdl::JntArray::Zero(nj_));
+  return bb_solver_.diagonalLength();
+}
+}  // namespace lr_tools
