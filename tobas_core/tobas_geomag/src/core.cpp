1514ae342a1411f142918c6ab8ef0661af94fe2c
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-16 00:27:38 +0900

    .

--- tobas_core/tobas_geomag/src/core.cpp
+++ tobas_core/tobas_geomag/src/core.cpp
@@ -48,7 +48,7 @@ Vector ecefFromGeodetic(double lat, double lon, double h)
 
 Vector magFieldFromECEF(double dyear, const Vector& position_itrs, const ConstModel& WMM)
 {
-  // mean radius of  ellipsoid in meters from section 1.2 of the WMM2015 Technical report
+  // Mean radius of  ellipsoid in meters from section 1.2 of the WMM2015 Technical report
   constexpr double EARTH_R = 6371200.0;
 
   const double x = position_itrs.x;
@@ -66,7 +66,7 @@ Vector magFieldFromECEF(double dyear, const Vector& position_itrs, const ConstMo
   const double f = z * temp;
   const double g = EARTH_R * temp;
 
-  // first m==0 row, just solve for the Vs
+  // First m==0 row, just solve for the Vs
   double Vtop = EARTH_R / sqrt(rsqrd);  // V0,0
   double Wtop = 0.0;                    // W0,0
   double Vprev = 0.0;
@@ -74,10 +74,10 @@ Vector magFieldFromECEF(double dyear, const Vector& position_itrs, const ConstMo
   double Vnm = Vtop;
   double Wnm = Wtop;
 
-  // iterate through all ms
+  // Iterate through all ms
   for (size_t m = 0; m <= NMAX + 1; ++m)
   {
-    // iterate through all ns
+    // Iterate through all ns
     for (size_t n = m; n <= NMAX + 1; ++n)
     {
       if (n == m)
