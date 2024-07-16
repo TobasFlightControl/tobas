cedc510ade6acbfa038a201f215a318c899526b6
dohi <masa0u0masa1215@gmail.com>
2024-07-16 11:10:04 +0900

    .

--- tobas_gcs/src/tobas_gcs/apps/hardware_setup/tabs/mag_calibration.py
+++ tobas_gcs/src/tobas_gcs/apps/hardware_setup/tabs/mag_calibration.py
@@ -285,7 +285,9 @@ class MagCalibrationWidget(BaseHardwareSetupWidget):
         return req
 
     def _reload_config(self) -> bool:
-        reload_config_sc = rospy.ServiceProxy(f"{self._drone.name}/magnetometer_handler/{Service.RELOAD_CONFIG}", Trigger)
+        reload_config_sc = rospy.ServiceProxy(
+            f"{self._drone.name}/magnetometer_handler/{Service.RELOAD_CONFIG}", Trigger
+        )
         try:
             reload_config_sc.wait_for_service(WAIT_FOR_SERVER)
         except rospy.ROSException:
