2607aea2f1d1d015cc4a709c594f2566bfe1013e
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-20 15:43:11 +0900

    .

--- tobas_core/tobas_linux/src/spi_dev.cpp
+++ tobas_core/tobas_linux/src/spi_dev.cpp
@@ -24,24 +24,23 @@ SPIdev::~SPIdev()
 
 bool SPIdev::initialize(const char* spi_dev, uint32_t speed_hz, size_t buf_size)
 {
-  buf_size_ = buf_size;
-  tx = (uint8_t*)malloc(buf_size * sizeof(uint8_t));
-  rx = (uint8_t*)malloc(buf_size * sizeof(uint8_t));
-
-  memset(&spi_transfer_, 0, sizeof(spi_ioc_transfer));
-
-  spi_transfer_.tx_buf = (uint64_t)tx;
-  spi_transfer_.rx_buf = (uint64_t)rx;
-  spi_transfer_.speed_hz = speed_hz;
-  spi_transfer_.bits_per_word = 8;
-  spi_transfer_.delay_usecs = 0;
-
   spi_fd_ = open(spi_dev, O_RDWR);
   if (spi_fd_ < 0)
   {
     cerr << "Failed to open SPI device: " << spi_dev << endl;
     return false;
   }
+
+  buf_size_ = buf_size;
+  tx = (uint8_t*)malloc(buf_size * sizeof(uint8_t));
+  rx = (uint8_t*)malloc(buf_size * sizeof(uint8_t));
+
+  memset(&spi_transfer_, 0, sizeof(spi_ioc_transfer));
+  spi_transfer_.tx_buf = (uint64_t)tx;
+  spi_transfer_.rx_buf = (uint64_t)rx;
+  spi_transfer_.speed_hz = speed_hz;
+  spi_transfer_.bits_per_word = 8;
+  spi_transfer_.delay_usecs = 0;
 
   return true;
 }
