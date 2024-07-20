b6beed2ac67133c81822b69a840a94ea3ea97504
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-20 22:57:55 +0900

    add: MagDriver

---
+++ tobas_hal/tobas_a1/tobas_a1_ros/include/tobas_a1_ros/mag_driver.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <tobas_kdl/vector.hpp>
+
+#include <tobas_hal_core/base_sensor_node.hpp>
+#include <tobas_a1_core/iis2mdc.hpp>
+
+namespace a1
+{
+class MagDriver : public hal::BaseSensorNode
+{
+  static constexpr size_t kSamplingRate = 100;  // [Hz] The maximum update rate of IIS2MDC
+
+  using self = MagDriver;
+  using super = hal::BaseSensorNode;
+
+public:
+  explicit MagDriver(ros::NodeHandle& nh, ros::NodeHandle& pnh, const std::string& name = ros::this_node::getName());
+
+private:
+  IIS2MDC mag_;
+  ros::Publisher mag_pub_;
+
+  void mainTimerCb(const ros::TimerEvent& event);
+};
+}  // namespace a1
