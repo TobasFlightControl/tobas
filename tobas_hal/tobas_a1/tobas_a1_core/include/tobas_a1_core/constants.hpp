b389796c68021e10a08666e755b0238bb3d4d899
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-25 23:57:49 +0900

    Define SPI devices

--- tobas_hal/tobas_a1/tobas_a1_core/include/tobas_a1_core/constants.hpp
+++ tobas_hal/tobas_a1/tobas_a1_core/include/tobas_a1_core/constants.hpp
@@ -13,11 +13,11 @@ static constexpr char kSbusDev[] = "/dev/serial0";
 
 namespace spi_device
 {
-static constexpr char kAdcDev[] = "/dev/spidev?.?";    // TODO
-static constexpr char kImuDev[] = "/dev/spidev?.?";    // TODO
-static constexpr char kGnssDev[] = "/dev/spidev?.?";   // TODO
-static constexpr char kDshotDev[] = "/dev/spidev?.?";  // TODO
-static constexpr char kPwmDev[] = "/dev/spidev?.?";    // TODO
+static constexpr char kImuDev[] = "/dev/spidev0.0";
+static constexpr char kAdcDev[] = "/dev/spidev0.1";
+static constexpr char kDshotDev[] = "/dev/spidev1.0";
+static constexpr char kPwmDev[] = "/dev/spidev1.1";
+static constexpr char kGnssDev[] = "/dev/spidev1.2";
 }  // namespace spi_device
 
 namespace i2c_address
