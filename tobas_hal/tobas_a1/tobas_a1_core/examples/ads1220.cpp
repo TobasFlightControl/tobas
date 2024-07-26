7eff347ed8c2835115ed319d9471fb064c19df36
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-26 12:50:24 +0900

    add: ads1220.cpp

---
+++ tobas_hal/tobas_a1/tobas_a1_core/examples/ads1220.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unistd.h>
+
+#include <tobas_a1_core/ads1220.hpp>
+
+using namespace std;
+
+int main()
+{
+  a1::ADS1220 adc;
+  double voltage;
+
+  if (!adc.initialize())
+  {
+    cerr << "Failed to initialize ADC driver." << endl;
+    return EXIT_FAILURE;
+  }
+
+  while (true)
+  {
+    if (!adc.readVoltage(voltage))
+    {
+      cerr << "Failed to read voltage channel." << endl;
+      return EXIT_FAILURE;
+    }
+
+    cout << "Voltage: " << voltage << endl;
+    sleep(1);
+  }
+
+  return EXIT_SUCCESS;
+}
