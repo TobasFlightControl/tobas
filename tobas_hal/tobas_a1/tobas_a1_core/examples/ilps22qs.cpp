8f5a3ce9b090d1325401c95268d2c3d9a77d3a74
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-26 14:07:23 +0900

    add: ilps22qs.cpp

---
+++ tobas_hal/tobas_a1/tobas_a1_core/examples/ilps22qs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unistd.h>
+
+#include <tobas_a1_core/ilps22qs.hpp>
+
+using namespace std;
+
+int main()
+{
+  a1::ILPS22QS baro;
+  double pres, temp;
+
+  if (!baro.initialize())
+  {
+    cerr << "Failed to initialize barometer." << endl;
+    return EXIT_FAILURE;
+  }
+
+  while (true)
+  {
+    if (!baro.readPressure(pres))
+    {
+      cerr << "Failed to read pressure." << endl;
+      return EXIT_FAILURE;
+    }
+
+    if (!baro.readTemperature(temp))
+    {
+      cerr << "Failed to read temperature." << endl;
+      return EXIT_FAILURE;
+    }
+
+    cout << "Pressure [Pa]: " << pres << endl;
+    cout << "Temperature [degC]: " << temp << endl;
+    sleep(1);
+  }
+
+  return EXIT_SUCCESS;
+}
