d7194e5852a1a1edf1d02f730a34b8c1abcc8b58
Masa0u0 <masa0u0masa1215@gmail.com>
2024-07-27 15:03:08 +0900

    edit: tether_station_force_plugin.xpp

--- tobas_gazebo/tobas_gazebo_plugins/plugins/tether_station_force_plugin.cpp
+++ tobas_gazebo/tobas_gazebo_plugins/plugins/tether_station_force_plugin.cpp
@@ -1,4 +1,5 @@
 #include "./tether_station_force_plugin.hpp"
+#include "../include/tobas_gazebo_plugins/common.hpp"
 #include "../include/tobas_gazebo_plugins/sdfparam.hpp"
 
 using namespace std;
@@ -16,29 +17,85 @@ void GazeboTetherStationForcePlugin::Load(physics::ModelPtr model, sdf::ElementP
 
   getSdfParams(sdf);
 
+  model_ = model;
+
   link_ = model->GetLink(link_name_);
   if (link_ == nullptr)
     gzthrow(kPluginName << ": Couldn't find specified link \"" << link_name_ << "\".");
+
+  contacts_sub_ = nh_.subscribe("/" + ns_ + "/" + kContactStatesTopic, 1, &self::contactStatesCb, this);
 
   update_connection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&self::onUpdate, this, _1));
 }
 
 void GazeboTetherStationForcePlugin::getSdfParams(sdf::ElementPtr sdf)
 {
+  getSdfParam(sdf, "robotNamespace", ns_);
   getSdfParam(sdf, "linkName", link_name_);
   getSdfParam(sdf, "worldEnd", W_Pos_WP_, Vector3d::Zero);
   getSdfParam(sdf, "droneEnd", B_Pos_BQ_, Vector3d::Zero);
   getSdfParam(sdf, "tension", tension_, kDefaultTension);
 }
 
-void GazeboTetherStationForcePlugin::onUpdate(const common::UpdateInfo&)
+void GazeboTetherStationForcePlugin::onUpdate(const common::UpdateInfo& info)
 {
+  if (contacts_ == nullptr)
+  {
+    GZ_WARN_THROTTLE(kWarnPeriod, kPluginName << ": /" << ns_ << "/" << kContactStatesTopic << " is not received yet.");
+    return;
+  }
+
+  // 最初に接地するまでは張力を加えない
+  // 初期位置がずれると，接触が不安定になり加速度に異常が生じる．
+  if (!first_contact_detected_)
+  {
+    if (isContactWithPlane())
+      first_contact_detected_ = true;
+    return;
+  }
+
+  // 接地時は張力を加えない
+  // 接地時に張力を加えると，接触が不安定になり加速度に異常が生じる．
+  if (isContactWithPlane())
+    return;
+
+  // ケーブルの端点間ベクトルを計算
   const auto& W_Pos_WB = link_->WorldPose().Pos();
   const auto& W_Rot_B = link_->WorldPose().Rot();
   const auto B_Pos_PQ = W_Rot_B.Inverse() * (W_Pos_WB - W_Pos_WP_) + B_Pos_BQ_;
+
+  // ケーブルの方向に張力を加える
   const auto axis = -B_Pos_PQ.Normalized();
   const auto force = tension_ * axis;  // TODO: 実際張力は一定ではない．より詳細な張力モデルを実装．
   link_->AddLinkForce(force, B_Pos_BQ_);
+}
+
+bool GazeboTetherStationForcePlugin::isThis(const std::string& name)
+{
+  return name == model_->GetName();
+}
+
+bool GazeboTetherStationForcePlugin::isPlane(const uint32_t& shape)
+{
+  return (shape & physics::Base::SHAPE & physics::Base::PLANE_SHAPE) > 0;
+}
+
+bool GazeboTetherStationForcePlugin::isContactWithPlane()
+{
+  for (const auto state : contacts_->states)
+  {
+    const auto& col1 = state.collision1;
+    const auto& col2 = state.collision2;
+    if ((isThis(col1.name) && isPlane(col2.shape_type)) || (isThis(col2.name) && isPlane(col1.shape_type)))
+      return true;
+  }
+
+  return false;
+}
+
+void GazeboTetherStationForcePlugin::contactStatesCb(const tobas_gazebo_msgs::ContactStatesConstPtr& contacts)
+{
+  contacts_ = contacts;
 }
 
 GZ_REGISTER_MODEL_PLUGIN(GazeboTetherStationForcePlugin);
--- tobas_gazebo/tobas_gazebo_plugins/plugins/tether_station_force_plugin.hpp
+++ tobas_gazebo/tobas_gazebo_plugins/plugins/tether_station_force_plugin.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <ros/ros.h>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/physics.hh>
+
+#include <tobas_gazebo_msgs/ContactStates.h>
 
 namespace gazebo
 {
@@ -23,16 +26,31 @@ protected:
   void Load(physics::ModelPtr model, sdf::ElementPtr sdf) override;
 
 private:
+  ros::NodeHandle nh_;
+
   // SDF parameters
+  std::string ns_;
   std::string link_name_;
   ignition::math::Vector3d W_Pos_WP_;
   ignition::math::Vector3d B_Pos_BQ_;
   double tension_;  // [N]
 
+  physics::ModelPtr model_;
   physics::LinkPtr link_;
   event::ConnectionPtr update_connection_;
 
+  bool first_contact_detected_ = false;
+  tobas_gazebo_msgs::ContactStatesConstPtr contacts_;
+
+  ros::Subscriber contacts_sub_;
+
   void getSdfParams(sdf::ElementPtr sdf);
   void onUpdate(const common::UpdateInfo& info);
+
+  bool isThis(const std::string& name);
+  bool isPlane(const uint32_t& shape);
+  bool isContactWithPlane();
+
+  void contactStatesCb(const tobas_gazebo_msgs::ContactStatesConstPtr& contacts);
 };
 }  // namespace gazebo
