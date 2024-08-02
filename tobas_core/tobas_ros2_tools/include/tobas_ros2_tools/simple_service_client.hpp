9cc018d87713a829bd7479ab1576a595b8422a6c
dohi <masa0u0masa1215@gmail.com>
2024-08-02 17:17:58 +0900

    add: simple_service_client.hpp

---
+++ tobas_core/tobas_ros2_tools/include/tobas_ros2_tools/simple_service_client.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace ros2
+{
+template <typename SrvType>
+class SimpleServiceClient
+{
+public:
+  inline explicit SimpleServiceClient(rclcpp::Node::SharedPtr node, const std::string& srv_name) : node_(node)
+  {
+    client_ = node_->create_client<SrvType>(srv_name);
+  }
+
+  bool call(const SrvType::Request::SharedPtr& req, const rclcpp::Duration& timeout = rclcpp::Duration(-1, 0))
+  {
+    if (!client_->wait_for_service(timeout.to_chrono<chrono::milliseconds>()))
+    {
+      RCLCPP_ERROR_STREAM(node_->get_logger(), "Failed to connect to " << srv_name << " server.");
+      return false;
+    }
+
+    auto id = client_->async_send_request(req);
+    if (rclcpp::spin_until_future_complete(node_, id) != rclcpp::FutureReturnCode::SUCCESS)
+    {
+      RCLCPP_ERROR_STREAM(node_->get_logger(), "Failed to call " << srv_name << " service.");
+      return false;
+    }
+
+    res_ = id.get();
+
+    return true;
+  }
+
+  inline const SrvType::Response::SharedPtr& getResult() const
+  {
+    return res_;
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Client<SrvType>::SharedPtr client_;
+  SrvType::Response::SharedPtr res_;
+};
+}  // namespace ros2
