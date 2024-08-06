eabf722474222442b188b219f2901e5317d47d72
dohi <masa0u0masa1215@gmail.com>
2024-08-06 19:21:27 +0900

    delete: tobas_tools_py

--- tobas_tools_py/src/tobas_tools_py/battery_config.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class BatteryConfig:
-    nominal_voltage: float = 0.0
-    max_voltage: float = 0.0
-    sag_voltage: float = 0.0
-    max_current: float = 0.0
--- tobas_tools_py/src/tobas_tools_py/util.py
+++
@@ -1,6 +0,0 @@
-from tobas_rclpy.utils import get_ros_ip
-
-
-def is_running_under_fc_master() -> bool:
-    """自身のノードがFC側のROSマスターに属する場合にTrueを返す．"""
-    return get_ros_ip().startswith("192.168.249.")
