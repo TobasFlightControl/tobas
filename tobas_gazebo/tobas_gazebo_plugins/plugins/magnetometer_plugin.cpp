70c95755b9250744337ea85da5a076850f1563de
dohi <masa0u0masa1215@gmail.com>
2024-08-13 13:36:45 +0900

    ros2: magnetometer_plugin

--- tobas_gazebo/tobas_gazebo_plugins/plugins/magnetometer_plugin.cpp
+++ tobas_gazebo/tobas_gazebo_plugins/plugins/magnetometer_plugin.cpp
@@ -1,23 +1,74 @@
 #include <tobas_math/core.hpp>
 #include <tobas_std_tools/geometry.hpp>
 #include <tobas_std_tools/time.hpp>
+#include <tobas_path_tools/join.hpp>
+#include <tobas_ros2_tools/time.hpp>
 #include <tobas_geomag/core.hpp>
 #include <tobas_constants/constants.hpp>
 #include <tobas_msgs/MagneticField.hpp>
 
-#include "./magnetometer_plugin.hpp"
+#include <tobas_gazebo_tools/math.hpp>
 
+#include "../include/tobas_gazebo_plugins/common/common.hpp"
+#include "../include/tobas_gazebo_plugins/conversions/conversions.hpp"
+#include "../include/tobas_gazebo_plugins/rate_manager.hpp"
+#include "../include/tobas_gazebo_plugins/random.hpp"
 #include "../include/tobas_gazebo_plugins/utils.hpp"
-#include "../include/tobas_gazebo_plugins/conversions/gazebo_ros.hpp"
-#include "../include/tobas_gazebo_plugins/conversions/gazebo_kdl.hpp"
 
 using namespace std;
 using namespace gz;
+using namespace gz::math;
 namespace cmp = sim::components;
 
 namespace gazebo
 {
-GazeboMagnetometerPlugin::GazeboMagnetometerPlugin() : super()
+class GazeboMagnetometerPlugin : public BaseNode,
+                                 public sim::System,
+                                 public sim::ISystemConfigure,
+                                 public sim::ISystemPostUpdate
+{
+  static constexpr size_t kDefaultUpdateRate = 100;  // [Hz]
+
+public:
+  explicit GazeboMagnetometerPlugin();
+
+  void Configure(
+    const sim::Entity& model,
+    const sdf::ElementConstPtr& sdf,
+    sim::EntityComponentManager& ecm,
+    sim::EventManager&) override;
+
+  void PostUpdate(const sim::UpdateInfo& info, const sim::EntityComponentManager& ecm) override;
+
+private:
+  // SDF parameters
+  string link_name_;
+  Vector3d offset_;                      // [m] B_Pos_BS
+  size_t update_rate_;                   // [Hz] Update rate
+  double lat_0_;                         // [deg] 原点の北緯
+  double lon_0_;                         // [deg] 原点の東経
+  double alt_0_;                         // [m] 原点の高度
+  Vector3d noise_normal_;                // [nT]
+  Vector3d noise_uniform_initial_bias_;  // [nT]
+
+  RateManager::SharedPtr rate_manager_;
+
+  const cmp::WorldPose* pose_W_;
+
+  Vector3d init_bias_;  // [nT] 世界座標系の地磁気に加わるバイアス
+  double lat_, lon_;    // [deg] 現在位置の経緯度
+
+  random_device rnd_dev_;
+  NormalDistribution3dPtr noise_;
+
+  PublisherPtr<tobas_msgs::MagneticField> mag_pub_;
+
+  void getSdfParams(const sdf::ElementConstPtr& sdf);
+
+  void publishMagMsg(const chrono::steady_clock::duration& time, const Vector3d& field) const;
+};
+
+GazeboMagnetometerPlugin::GazeboMagnetometerPlugin() : BaseNode("magnetometer_plugin")
 {
 }
 
@@ -27,54 +78,48 @@ void GazeboMagnetometerPlugin::Configure(
   sim::EntityComponentManager& ecm,
   sim::EventManager&)
 {
-
   initialize(sdf);
-
-  // Get SDF parameters
   getSdfParams(sdf);
 
-  // Get the world model
-  world_ = physics::get_world(sensor->WorldName());
+  rate_manager_ = make_shared<RateManager>(update_rate_);
 
-  // Get the pointer to the link
-  link_ = dynamic_pointer_cast<physics::Link>(world_->EntityByName(link_name_));
-  if (link_ == nullptr)
-    TOBAS_EXIT("Couldn't find specified link \"" << link_name_ << "\".");
+  const auto link = ecm.EntityByComponents(cmp::Link(), cmp::ParentEntity(model), cmp::Name(link_name_));
+  if (link == sim::kNullEntity)
+    TOBAS_EXIT("Failed to find specified link \"", link_name_, "\".");
 
-  // Create the normal noise distributions
-  noise_.reset(new NormalDistribution3d(rnd_dev_, math::Vector3d::Zero, noise_normal_));
+  pose_W_ = getComponent<cmp::WorldPose>(link, ecm);
 
-  // Create the initial bias
+  noise_.reset(new NormalDistribution3d(rnd_dev_, Vector3d::Zero, noise_normal_));
+
   UniformDistribution3d init_bias_dist(rnd_dev_, -noise_uniform_initial_bias_, noise_uniform_initial_bias_);
   init_bias_ = init_bias_dist.get();
 
-  // Advertise publisher
-  mag_pub_ = createPublisher<tobas_msgs::MagneticField>(path::join(ns(), tobas::kMagTopic);
-
-
-  update_connection_ = sensor->ConnectUpdated(std::bind(&GazeboMagnetometerPlugin::onUpdate, this));
+  mag_pub_ = createPublisher<tobas_msgs::MagneticField>(path::join(ns(), tobas::kMagTopic));
 }
 
 void GazeboMagnetometerPlugin::getSdfParams(const sdf::ElementConstPtr& sdf)
 {
-
   getSdfParam(sdf, "linkName", link_name_);
-  getSdfParam(sdf, "offset", offset_, math::Vector3d::Zero);
+  getSdfParam(sdf, "offset", offset_, Vector3d::Zero);
+  getSdfParam(sdf, "updateRate", update_rate_, kDefaultUpdateRate, NON_NEGATIVE);
 
   getSdfParam(sdf, "latitudeZero", lat_0_, kDefaultLatitudeZero);
   getSdfParam(sdf, "longitudeZero", lon_0_, kDefaultLongitudeZero);
   getSdfParam(sdf, "altitudeZero", alt_0_, kDefaultAltitudeZero);
 
-  getSdfParam(sdf, "noiseNormal", noise_normal_, math::Vector3d::Zero);
-  getSdfParam(sdf, "noiseUniformInitialBias", noise_uniform_initial_bias_, math::Vector3d::Zero);
+  getSdfParam(sdf, "noiseNormal", noise_normal_, Vector3d::Zero);
+  getSdfParam(sdf, "noiseUniformInitialBias", noise_uniform_initial_bias_, Vector3d::Zero);
   if (!allGreaterEqual(noise_normal_, 0.) || !allGreaterEqual(noise_uniform_initial_bias_, 0.))
     TOBAS_EXIT("Noise std. dev cannot be negative.");
 }
 
-void GazeboMagnetometerPlugin::PostUpdate(const sim::UpdateInfo& info, const sim::EntityComponentManager& ecm)
+void GazeboMagnetometerPlugin::PostUpdate(const sim::UpdateInfo& info, const sim::EntityComponentManager&)
 {
+  if (!rate_manager_->update(info.simTime))
+    return;
+
   // Get the sensor pose
-  const auto& T_W_B = link_->WorldPose();
+  const auto& T_W_B = pose_W_->Data();
   const auto& W_Pos_WB = T_W_B.Pos();
   const auto& W_Rot_B = T_W_B.Rot();
   const auto W_Pos_WS = W_Pos_WB + W_Rot_B * offset_;
@@ -94,24 +139,28 @@ void GazeboMagnetometerPlugin::PostUpdate(const sim::UpdateInfo& info, const sim
   field_B += noise_->get();
 
   // Publish message
-  publishMagMsg(field_B);
+  publishMagMsg(info.simTime, field_B);
 }
 
-void GazeboMagnetometerPlugin::publishMagMsg(const gz::math::Vector3d& field) const
+void GazeboMagnetometerPlugin::publishMagMsg(const chrono::steady_clock::duration& time, const Vector3d& field) const
 {
-  const auto mag_msg =std::make_unique<tobas_msgs::MagneticField>();
+  auto mag_msg = make_unique<tobas_msgs::MagneticField>();
 
-  ros2::timeChronoToMsg(info.simTime, mag_msg->header.stamp);
+  ros2::timeChronoToMsg(time, mag_msg->header.stamp);
   mag_msg->header.frame_id = link_name_;
 
   vectorGazeboToKDL(field, mag_msg->magnetic_field);
 
   mag_msg->covariance.setZero();
   for (size_t i = 0; i < 3; ++i)
-    mag_msg->covariance.diagonal()(i) = math::sqr(noise_normal_[i]);
+    mag_msg->covariance.diagonal()(i) = ::math::sqr(noise_normal_[i]);
 
-  mag_pub_->publish(mag_msg);
+  mag_pub_->publish(move(mag_msg));
 }
-
-GZ_REGISTER_SENSOR_PLUGIN(GazeboMagnetometerPlugin);
 }  // namespace gazebo
+
+GZ_ADD_PLUGIN(
+  gazebo::GazeboMagnetometerPlugin,
+  sim::System,
+  gazebo::GazeboMagnetometerPlugin::ISystemConfigure,
+  gazebo::GazeboMagnetometerPlugin::ISystemPostUpdate)
