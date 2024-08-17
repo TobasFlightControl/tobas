e3adb10801a089332b55b42da77f78b87d8212bf
dohi <masa0u0masa1215@gmail.com>
2024-08-17 15:12:26 +0900

    add: hardware_interfaces.launch.py

---
+++ tobas_hal/tobas_a1/tobas_a1_ros/launch/hardware_interfaces.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    pkg_name = "tobas_a1_ros"
+    extra_arguments = [{"use_intra_process_comms": True}]
+
+    load_composable_nodes_high = LoadComposableNodes(
+        target_container="component_manager_high",
+        composable_node_descriptions=[
+            ComposableNode(package=pkg_name, plugin="IMUDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="SBUSDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="DShotDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="PWMDriverNode", extra_arguments=extra_arguments),
+        ],
+    )
+
+    load_composable_nodes_medium = LoadComposableNodes(
+        target_container="component_manager_medium",
+        composable_node_descriptions=[
+            ComposableNode(package=pkg_name, plugin="MagDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="BaroDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="GNSSDriverNode", extra_arguments=extra_arguments),
+            ComposableNode(package=pkg_name, plugin="ADCDriverNode", extra_arguments=extra_arguments),
+        ],
+    )
+
+    return LaunchDescription([load_composable_nodes_high, load_composable_nodes_medium])
