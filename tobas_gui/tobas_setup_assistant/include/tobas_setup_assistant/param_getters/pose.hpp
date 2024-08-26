a491c4c641fb1051b2695d00a7039b280fd8fcdf
dohi <masa0u0masa1215@gmail.com>
2024-08-26 17:31:42 +0900

    wip: tobas_setup_assistant

---
+++ tobas_gui/tobas_setup_assistant/include/tobas_setup_assistant/param_getters/pose.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <eigen3/Eigen/Core>
+
+#include "./base.hpp"
+#include "./scalar_getter.hpp"
+
+namespace gui
+{
+namespace setup_assistant
+{
+class ParamGetterWidget_Pose : public ParamGetterWidget<std::pair<Eigen::Vector3d, Eigen::Vector3d>>
+{
+  Q_OBJECT
+
+  using super = ParamGetterWidget<std::pair<Eigen::Vector3d, Eigen::Vector3d>>;
+
+  static constexpr double kXYZSingleStep = 0.1;
+  static constexpr double kRPYSingleStep = 0.1;
+
+Q_SIGNALS:
+  void valueChanged(std::pair<Eigen::Vector3d, Eigen::Vector3d> value);
+
+public:
+  explicit ParamGetterWidget_Pose(
+    const QString& param_name,
+    const QString& description_text = "",
+    int decimals = 3,
+    double xyz_min = std::numeric_limits<double>::lowest(),
+    double xyz_max = std::numeric_limits<double>::max(),
+    const Eigen::Vector3d& xyz_default = Eigen::Vector3d::Zero(),
+    const QString& xyz_suffix = " m",
+    double rpy_min = std::numeric_limits<double>::lowest(),
+    double rpy_max = std::numeric_limits<double>::max(),
+    const Eigen::Vector3d& rpy_default = Eigen::Vector3d::Zero(),
+    const QString& rpy_suffix = " rad");
+
+  std::pair<Eigen::Vector3d, Eigen::Vector3d> get() const override;
+  bool set(const std::pair<Eigen::Vector3d, Eigen::Vector3d>& src) override;
+
+  double x() const;
+  double y() const;
+  double z() const;
+  double roll() const;
+  double pitch() const;
+  double yaw() const;
+
+private Q_SLOTS:
+  void onValueChanged(double value);
+
+private:
+  DoubleGetter* x_;
+  DoubleGetter* y_;
+  DoubleGetter* z_;
+  DoubleGetter* roll_;
+  DoubleGetter* pitch_;
+  DoubleGetter* yaw_;
+};
+}  // namespace setup_assistant
+}  // namespace gui
---
+++ tobas_gui/tobas_setup_assistant/src/param_getters/pose.cpp
@@ -0,0 +1,111 @@
+#include "tobas_setup_assistant/param_getters/pose.hpp"
+
+namespace gui
+{
+namespace setup_assistant
+{
+ParamGetterWidget_Pose::ParamGetterWidget_Pose(
+  const QString& param_name,
+  const QString& description_text,
+  int decimals,
+  double xyz_min,
+  double xyz_max,
+  const Eigen::Vector3d& xyz_default,
+  const QString& xyz_suffix,
+  double rpy_min,
+  double rpy_max,
+  const Eigen::Vector3d& rpy_default,
+  const QString& rpy_suffix)
+  : super(param_name, description_text)
+{
+  // XYZ
+  auto cols_xyz = new QHBoxLayout();
+  rows_->addLayout(cols_xyz);
+
+  x_ = new DoubleGetter("x", decimals, xyz_min, xyz_max, kXYZSingleStep, xyz_default.x(), xyz_suffix);
+  cols_xyz->addWidget(x_);
+
+  y_ = new DoubleGetter("y", decimals, xyz_min, xyz_max, kXYZSingleStep, xyz_default.y(), xyz_suffix);
+  cols_xyz->addWidget(y_);
+
+  z_ = new DoubleGetter("z", decimals, xyz_min, xyz_max, kXYZSingleStep, xyz_default.z(), xyz_suffix);
+  cols_xyz->addWidget(z_);
+
+  // RPY
+  auto cols_rpy = new QHBoxLayout();
+  rows_->addLayout(cols_rpy);
+
+  roll_ = new DoubleGetter("roll", decimals, rpy_min, rpy_max, kRPYSingleStep, rpy_default.x(), rpy_suffix);
+  cols_rpy->addWidget(roll_);
+
+  pitch_ = new DoubleGetter("pitch", decimals, rpy_min, rpy_max, kRPYSingleStep, rpy_default.y(), rpy_suffix);
+  cols_rpy->addWidget(pitch_);
+
+  yaw_ = new DoubleGetter("yaw", decimals, rpy_min, rpy_max, kRPYSingleStep, rpy_default.z(), rpy_suffix);
+  cols_rpy->addWidget(yaw_);
+
+  // Connections
+  connect(x_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+  connect(y_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+  connect(z_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+  connect(roll_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+  connect(pitch_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+  connect(yaw_, SIGNAL(IntGetter::valueChanged(double)), this, SLOT(onValueChanged(double)));
+}
+
+std::pair<Eigen::Vector3d, Eigen::Vector3d> ParamGetterWidget_Pose::get() const
+{
+  return { { x(), y(), z() }, { roll(), pitch(), yaw() } };
+}
+
+bool ParamGetterWidget_Pose::set(const std::pair<Eigen::Vector3d, Eigen::Vector3d>& src)
+{
+  const auto& xyz = src.first;
+  const auto& rpy = src.second;
+
+  x_->set(xyz.x());
+  y_->set(xyz.y());
+  z_->set(xyz.z());
+  roll_->set(rpy.x());
+  pitch_->set(rpy.y());
+  yaw_->set(rpy.z());
+
+  return true;
+}
+
+double ParamGetterWidget_Pose::x() const
+{
+  return x_->get();
+}
+
+double ParamGetterWidget_Pose::y() const
+{
+  return y_->get();
+}
+
+double ParamGetterWidget_Pose::z() const
+{
+  return z_->get();
+}
+
+double ParamGetterWidget_Pose::roll() const
+{
+  return roll_->get();
+}
+
+double ParamGetterWidget_Pose::pitch() const
+{
+  return pitch_->get();
+}
+
+double ParamGetterWidget_Pose::yaw() const
+{
+  return yaw_->get();
+}
+
+void ParamGetterWidget_Pose::onValueChanged(double)
+{
+  Q_EMIT valueChanged({ { x(), y(), z() }, { roll(), pitch(), yaw() } });
+}
+}  // namespace setup_assistant
+}  // namespace gui
