1632cc0975a22e477f17ae7a23bc9b644c71db9e
dohi <masa0u0masa1215@gmail.com>
2024-09-06 11:39:11 +0900

    .

--- tobas_gui/tobas_setup_assistant/launch/setup_assistant.launch.py
+++ tobas_gui/tobas_setup_assistant/launch/setup_assistant.launch.py
@@ -1,18 +1,21 @@
 from launch import LaunchDescription
-from moveit_configs_utils.launch_utils import (
-    add_debuggable_node,
-    DeclareBooleanLaunchArg,
-)
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
-    add_debuggable_node(
-        ld,
-        package="moveit_setup_assistant",
-        executable="moveit_setup_assistant",
+    # Launch setup assistant
+    ld.add_action(Node(package="tobas_setup_assistant", executable="main"))
+
+    # Launch robot state publisher with minimul URDF
+    minimul_urdf = '<robot name="empty"><link name="root"/></robot>'
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{"robot_description": minimul_urdf}],
+        )
     )
 
     return ld
