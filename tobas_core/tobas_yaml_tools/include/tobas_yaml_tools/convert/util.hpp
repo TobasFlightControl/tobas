2a96447b6331a9f8e53bda3e3bcf7fb349003fda
dohi <masa0u0masa1215@gmail.com>
2024-09-14 09:21:33 +0900

    std::format -> std::snprintf

--- tobas_core/tobas_yaml_tools/include/tobas_yaml_tools/convert/util.hpp
+++ tobas_core/tobas_yaml_tools/include/tobas_yaml_tools/convert/util.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <format>
+#include <cstdio>
+#include <string>
 
 namespace YAML
 {
@@ -9,8 +10,11 @@ namespace util
 /* 少数が自動的に整数に丸められるのを防ぐため，明示的に指数表示でエンコードする． */
 inline std::string format(double value)
 {
-  // std::to_stringだと微小値が丸められてしまうためstd::formatを採用
-  return std::format("{:e}", value);
+  // std::to_stringだと微小値が丸められてしまう
+  // std::formatはGCC-13以上でないと使えない
+  char buffer[15];  // snprintfは最大長さ15の文字列を返す
+  std::snprintf(buffer, sizeof(buffer), "%e", value);
+  return std::string(buffer);  // char*のままだとメモリ開放されてしまうためstd::stringに変換
 }
 }  // namespace util
 }  // namespace YAML
