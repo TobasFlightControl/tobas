24aa9a733e50dccd5387eefa84c9cdac0648472c
dohi <masa0u0masa1215@gmail.com>
2024-09-20 10:17:31 +0900

    edit: tobas_ssh_server

--- tobas_ssh/tobas_ssh_server/tobas_ssh_server/ssh_server_node.py
+++ tobas_ssh/tobas_ssh_server/tobas_ssh_server/ssh_server_node.py
@@ -2,10 +2,11 @@ import rclpy
 from rclpy.node import Node
 
 from tobas_ssh_msgs.srv import Execute, SCPPut, SFTPRead, SFTPWrite
-from tobas_ssh_server.ssh_client import SSHClientWrapper
+from .ssh_client import SSHClientWrapper
 
 
 class SSHServerNode(Node):
+    NO_CONNECTION_ERROR = "No connection with SSH server."
 
     def __init__(self) -> None:
         super().__init__("ssh_server")
@@ -16,13 +17,21 @@ class SSHServerNode(Node):
         passwd = self.declare_parameter("passwd", "").get_parameter_value().string_value
 
         self._ssh_client = SSHClientWrapper(host, port, user, passwd)
+        self._is_connected = False
 
         self._execute_ss = self.create_service(Execute, "ssh/execute", self._execute_cb)
         self._scp_put_ss = self.create_service(SCPPut, "ssh/scp_put", self._scp_put_cb)
         self._sftp_read_ss = self.create_service(SFTPRead, "ssh/sftp_read", self._sftp_read_cb)
         self._sftp_write_ss = self.create_service(SFTPWrite, "ssh/sftp_write", self._sftp_write_cb)
 
+        self._connect_timer = self.create_timer(1.0, self._connect_timer_cb)
+
     def _execute_cb(self, req: Execute.Request, res: Execute.Response) -> Execute.Response:
+        if not self._is_connected:
+            res.success = False
+            res.error_output = self.NO_CONNECTION_ERROR
+            return res
+
         if req.superuser:
             if req.background:
                 self._ssh_client.exec_command_bg_super(req.command)
@@ -43,6 +52,11 @@ class SSHServerNode(Node):
         return res
 
     def _scp_put_cb(self, req: SCPPut.Request, res: SCPPut.Response) -> SCPPut.Response:
+        if not self._is_connected:
+            res.success = False
+            res.message = self.NO_CONNECTION_ERROR
+            return res
+
         if req.superuser:
             try:
                 self._ssh_client.scp_put_dir_super(req.local_dir, req.remote_dir, req.exclude_dirs)
@@ -61,6 +75,11 @@ class SSHServerNode(Node):
         return res
 
     def _sftp_read_cb(self, req: SFTPRead.Request, res: SFTPRead.Response) -> SFTPRead.Response:
+        if not self._is_connected:
+            res.success = False
+            res.message = self.NO_CONNECTION_ERROR
+            return res
+
         try:
             res.text = self._ssh_client.sftp_read(req.remote_path)
             res.success = True
@@ -71,6 +90,11 @@ class SSHServerNode(Node):
         return res
 
     def _sftp_write_cb(self, req: SFTPWrite.Request, res: SFTPWrite.Response) -> SFTPWrite.Response:
+        if not self._is_connected:
+            res.success = False
+            res.message = self.NO_CONNECTION_ERROR
+            return res
+
         if req.superuser:
             try:
                 self._ssh_client.sftp_write_super(req.remote_path, req.text)
@@ -87,6 +111,14 @@ class SSHServerNode(Node):
                 res.message = e
 
         return res
+
+    def _connect_timer_cb(self) -> None:
+        try:
+            self._ssh_client.connect()
+            self._is_connected = True
+        except Exception as e:
+            self.get_logger().warn(f"{e}")
+            self._is_connected = False
 
 
 def main(args=None) -> None:
