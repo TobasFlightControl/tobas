3f68d05e97303ce720d0ffac17d7e6f2f8cc3f13
pi <TODO>
2024-09-30 12:16:33 +0100

    .

--- tobas_hal/tobas_aso/tobas_aso_core/include/tobas_aso_core/dshot.hpp
+++ tobas_hal/tobas_aso/tobas_aso_core/include/tobas_aso_core/dshot.hpp
@@ -22,7 +22,7 @@ private:
   static constexpr size_t kChannelBytes = 2;                           // 1チャネルあたりのバイト数
   static constexpr size_t kSpiBufSize = kChannelSize * kChannelBytes;  // SPIバッファのサイズ
   // static constexpr uint32_t kSpiClockFreq = 50'000'000;                // [Hz] F722のSPI1の最大値
-  static constexpr uint32_t kSpiClockFreq = 1'000'000;  // TODO: 配線を改善するなどしてクロック周波数を上げる
+  static constexpr uint32_t kSpiClockFreq = 2'000'000;  // TODO: 配線を改善するなどしてクロック周波数を上げる
   static constexpr uint16_t kThrottleMask = (1 << 11) - 1;
   static constexpr uint16_t kDShotDisableCommand = 41;  // Not yet assigned
 
