abbf4292c169db794c41cc8e7fa27a29e98104aa
dohi <masa0u0masa1215@gmail.com>
2024-10-02 00:35:05 +0900

    real.launch.xml -> real.launch.py

--- tobas_gui/tobas_setup_assistant/src/package_generator.cpp
+++ tobas_gui/tobas_setup_assistant/src/package_generator.cpp
@@ -239,7 +239,7 @@ bool PackageGenerator::generateConfigPackage(const inja::json& tpl_data)
   cfg_env_->generate(tpl_data, "CMakeLists.txt.tplcmake", config_pkg_path);
   cfg_env_->generate(tpl_data, "package.xml.tplxml", config_pkg_path);
   cfg_env_->generate(tpl_data, "gazebo.launch.xml.tplxml", launch_dir);
-  cfg_env_->generate(tpl_data, "real.launch.xml.tplxml", launch_dir);
+  cfg_env_->generate(tpl_data, "real.launch.py.tplpy", launch_dir);
   cfg_env_->generate(tpl_data, "core_component.launch.py.tplpy", launch_dir);
   cfg_env_->generate(tpl_data, "core_standalone.launch.py.tplpy", launch_dir);
   cfg_env_->generate(tpl_data, "hil.launch.xml.tplxml", launch_dir);
