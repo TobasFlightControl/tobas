d9e5c13175717c619d2c02c14a7cb385fc0d671a
dohi <masa0u0masa1215@gmail.com>
2024-10-02 16:06:29 +0900

    fix

--- tobas_hal/tobas_aso/tobas_aso_ros/launch/hardware_interfaces.launch.py
+++ tobas_hal/tobas_aso/tobas_aso_ros/launch/hardware_interfaces.launch.py
@@ -1,30 +1,83 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
+
+NAMESPACE = "namespace"
 
 
 def generate_launch_description():
     pkg_name = "tobas_aso_ros"
     extra_arguments = [{"use_intra_process_comms": True}]
 
-    load_composable_nodes_high = LoadComposableNodes(
-        target_container="component_manager_high",
-        composable_node_descriptions=[
-            ComposableNode(package=pkg_name, plugin="IMUDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="SBUSDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="DShotDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="PWMDriverNode", extra_arguments=extra_arguments),
-        ],
+    ld = LaunchDescription()
+
+    ld.add_action(DeclareLaunchArgument(NAMESPACE))
+    ns = LaunchConfiguration(NAMESPACE)
+
+    ld.add_action(
+        LoadComposableNodes(
+            target_container=PathJoinSubstitution([ns, "component_manager_high"]),  # target_containerはフルパスのみ
+            composable_node_descriptions=[
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="IMUDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="SBUSDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="DShotDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="PWMDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+            ],
+        )
     )
 
-    load_composable_nodes_medium = LoadComposableNodes(
-        target_container="component_manager_medium",
-        composable_node_descriptions=[
-            ComposableNode(package=pkg_name, plugin="MagDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="BaroDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="GNSSDriverNode", extra_arguments=extra_arguments),
-            ComposableNode(package=pkg_name, plugin="ADCDriverNode", extra_arguments=extra_arguments),
-        ],
+    ld.add_action(
+        LoadComposableNodes(
+            target_container=PathJoinSubstitution([ns, "component_manager_medium"]),
+            composable_node_descriptions=[
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="MagDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="BaroDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="GNSSDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+                ComposableNode(
+                    package=pkg_name,
+                    plugin="ADCDriverNode",
+                    namespace=ns,
+                    extra_arguments=extra_arguments,
+                ),
+            ],
+        )
     )
 
-    return LaunchDescription([load_composable_nodes_high, load_composable_nodes_medium])
+    return ld
