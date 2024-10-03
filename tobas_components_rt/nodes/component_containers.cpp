a46ab0b8e9ab7e19c7d1e60e199777dba66849e4
dohi <masa0u0masa1215@gmail.com>
2024-10-03 22:59:06 +0900

    メモリロック

--- tobas_components_rt/nodes/component_containers.cpp
+++ tobas_components_rt/nodes/component_containers.cpp
@@ -1,7 +1,14 @@
+#include <tobas_linux/memory_lock.hpp>
+
 #include "tobas_components_rt/multi_component_managers.hpp"
+
+#define LOCK_MEMORY_MB 1000
 
 int main(int argc, char* argv[])
 {
+  if (!linux::lockAndPrefaultDynamic(LOCK_MEMORY_MB * (1 << 20)))
+    throw std::runtime_error("Failed to lock memory.");
+
   rclcpp::init(argc, argv);
 
   ros2::MultiComponentManagers managers(SCHED_FIFO, 4, 0);
