f5303f3b4a96ba4d18f91a8639cc1bcbc7b7e473
dohi <masa0u0masa1215@gmail.com>
2024-10-03 16:44:26 +0900

    時間差で起動

--- tobas_hal/tobas_aso/tobas_aso_ros/launch/hardware_interfaces.launch.py
+++ tobas_hal/tobas_aso/tobas_aso_ros/launch/hardware_interfaces.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, TimerAction
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
@@ -18,83 +18,77 @@ def generate_launch_description():
 
     # Launch 1st priority nodes (Twist Control)
     ld.add_action(
-        LoadComposableNodes(
-            target_container=PathJoinSubstitution([ns, "component_manager_1"]),
-            composable_node_descriptions=[
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="IMUDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="ADCDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="DShotDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="PWMDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
+        TimerAction(
+            period=0.0,
+            actions=[
+                LoadComposableNodes(
+                    target_container=PathJoinSubstitution([ns, "component_manager_1"]),
+                    composable_node_descriptions=[
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="IMUDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="ADCDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="DShotDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="PWMDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                    ],
+                )
             ],
         )
     )
 
     # Launch 2nd priority nodes (Pose Control & Manipulation)
     ld.add_action(
-        LoadComposableNodes(
-            target_container=PathJoinSubstitution([ns, "component_manager_2"]),
-            composable_node_descriptions=[
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="MagDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="BaroDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="GNSSDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
-                ComposableNode(
-                    package=pkg_name,
-                    plugin="SBUSDriverNode",
-                    namespace=ns,
-                    extra_arguments=extra_arguments,
-                ),
+        TimerAction(
+            period=0.1,
+            actions=[
+                LoadComposableNodes(
+                    target_container=PathJoinSubstitution([ns, "component_manager_2"]),
+                    composable_node_descriptions=[
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="MagDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="BaroDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="GNSSDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                        ComposableNode(
+                            package=pkg_name,
+                            plugin="SBUSDriverNode",
+                            namespace=ns,
+                            extra_arguments=extra_arguments,
+                        ),
+                    ],
+                )
             ],
-        )
-    )
-
-    # Launch 3rd priority nodes (Navigation)
-    ld.add_action(
-        LoadComposableNodes(
-            target_container=PathJoinSubstitution([ns, "component_manager_3"]),
-            composable_node_descriptions=[],
-        )
-    )
-
-    # Launch 4th priority nodes (Others)
-    ld.add_action(
-        LoadComposableNodes(
-            target_container=PathJoinSubstitution([ns, "component_manager_4"]),
-            composable_node_descriptions=[],
         )
     )
 
