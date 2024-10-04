5b6bb068dc2c447ebc4a85d7da898138deadbfce
dohi <masa0u0masa1215@gmail.com>
2024-10-04 10:39:47 +0900

    delete: latency_throttle

--- tobas_topic_throttle/nodes/latency_throttle_node.cpp
+++
@@ -1,7 +0,0 @@
-#include <tobas_msgs/msg/latency.hpp>
-
-#include "./base.hpp"
-
-using LatencyThrottleNode = TopicThrottleNode<tobas_msgs::msg::Latency, tobas::kLatencyTopic>;
-
-RCLCPP_COMPONENTS_REGISTER_NODE(LatencyThrottleNode)
