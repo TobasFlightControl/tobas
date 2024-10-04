6112b0206a9af705ca3e069d728639e664d7876b
pi <TODO>
2024-10-04 03:32:59 +0100

    disable cpu affinity

--- tobas_components_rt/nodes/component_containers.cpp
+++ tobas_components_rt/nodes/component_containers.cpp
@@ -18,24 +18,26 @@ int main(int argc, char* argv[])
 
   ros2::MultiComponentManagers managers(NUM_MANAGERS);
 
+  // FIXME: CPU Affinityを設定するとsystemdから起動したときにCPU0,1が使用されなくなる
+
   managers.setPolicy(0, SCHED_FIFO);
   managers.setPriority(0, 90);
-  managers.setCPUAffinity(0, (1 << 0) | (1 << 1));
+  // managers.setCPUAffinity(0, (1 << 0) | (1 << 1));
   managers.setNumThreads(0, 0);
 
   managers.setPolicy(1, SCHED_FIFO);
   managers.setPriority(1, 50);
-  managers.setCPUAffinity(1, (1 << 2));
+  // managers.setCPUAffinity(1, (1 << 2));
   managers.setNumThreads(1, 0);
 
   managers.setPolicy(2, SCHED_FIFO);
   managers.setPriority(2, 10);
-  managers.setCPUAffinity(2, (1 << 3));
+  // managers.setCPUAffinity(2, (1 << 3));
   managers.setNumThreads(2, 0);
 
   managers.setPolicy(3, SCHED_FIFO);
   managers.setPriority(3, 0);
-  managers.setCPUAffinity(3, (1 << 3));
+  // managers.setCPUAffinity(3, (1 << 3));
   managers.setNumThreads(3, 0);
 
   managers.spin();
