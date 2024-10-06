be4172527d4f8a2e76ca74ecc794d69bae77e1a8
dohi <masa0u0masa1215@gmail.com>
2024-10-07 01:32:38 +0900

    .

--- tobas_core/tobas_linux/include/tobas_linux/termios2.hpp
+++ tobas_core/tobas_linux/include/tobas_linux/termios2.hpp
@@ -4,5 +4,11 @@
 
 namespace linux
 {
+/**
+ * @brief 標準ではないボーレートを設定する．
+ * cf. pySerial: https://github.com/pyserial/pyserial
+ *
+ * @note pySerialのように，termios.h内のtermiosに加え，asm/termibits.h内のtermios2の両方を設定する必要がある．
+ */
 bool setNonStandardBaudRate(int fd, uint32_t baud_rate);
-}
+}  // namespace linux
