6cfa46897611d62d4053faced642906e7db56a2a
pi <TODO>
2024-10-13 09:37:49 +0100

    change realtime policy and cpu affinity and number of threads

--- tobas_components_rt/nodes/component_containers.cpp
+++ tobas_components_rt/nodes/component_containers.cpp
@@ -17,21 +17,23 @@ int main(int argc, char* argv[])
   // MultiThreadedExecutorはCPU不可が高く，SingleThreadedExecutorよりもレイテンシが大きくなる．
   // しかし，シングルスレッドだとサービスコールやDDS再接続でブロッキングが発生する．
   // そのため，リアルタイム性が重視されるグループには最低でも2コア2スレッド与える必要がある．
+  // SCHED_FIFOよりもSCHED_RRの方がROSと相性が良いらしい．
+  // なぜだか分からないが，最悪時間はSCHED_RRの方が小さいように思える．
 
-  managers.setPolicy(0, SCHED_FIFO);
+  managers.setPolicy(0, SCHED_RR);
   managers.setPriority(0, 90);
   managers.setCPUAffinity(0, 0b0011);
   managers.setNumThreads(0, 2);
 
-  managers.setPolicy(1, SCHED_FIFO);
+  managers.setPolicy(1, SCHED_RR);
   managers.setPriority(1, 50);
-  managers.setCPUAffinity(1, 0b1100);
-  managers.setNumThreads(1, 2);
+  managers.setCPUAffinity(1, 0b0100);
+  managers.setNumThreads(1, 1);
 
-  managers.setPolicy(2, SCHED_FIFO);
+  managers.setPolicy(2, SCHED_RR);
   managers.setPriority(2, 10);
-  managers.setCPUAffinity(2, 0b1100);
-  managers.setNumThreads(2, 2);
+  managers.setCPUAffinity(2, 0b1000);
+  managers.setNumThreads(2, 1);
 
   managers.spin();
 
