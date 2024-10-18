04c5280d873de3de12c921e1765d254ee6c9bd5b
dohi <masa0u0masa1215@gmail.com>
2024-10-18 23:42:56 +0900

    設定が反映されたかどうかを確認

--- tobas_core/tobas_linux/src/termios2.cpp
+++ tobas_core/tobas_linux/src/termios2.cpp
@@ -11,21 +11,33 @@ namespace linux
 {
 bool setNonStandardBaudRate(int fd, uint32_t baud_rate)
 {
-  struct termios2 buf;
+  struct termios2 buf1, buf2;
 
-  if (ioctl(fd, TCGETS2, buf) != 0)
+  if (ioctl(fd, TCGETS2, buf1) != 0)
   {
     cerr << "Failed to get termios2 struct (TCGETS2): " << strError() << endl;
     return false;
   }
 
-  buf.c_cflag &= ~CBAUD;
-  buf.c_cflag |= CBAUDEX;
-  buf.c_ispeed = buf.c_ospeed = baud_rate;
+  buf1.c_cflag &= ~CBAUD;
+  buf1.c_cflag |= CBAUDEX;
+  buf1.c_ispeed = buf1.c_ospeed = baud_rate;
 
-  if (ioctl(fd, TCSETS2, buf) != 0)
+  if (ioctl(fd, TCSETS2, buf1) != 0)
   {
     cerr << "Failed to set termios2 struct (TCSETS2): " << strError() << endl;
+    return false;
+  }
+
+  // 設定が反映されたかどうか確認
+  if (ioctl(fd, TCGETS2, buf2) != 0)
+  {
+    cerr << "Failed to get termios2 struct (TCGETS2): " << strError() << endl;
+    return false;
+  }
+  if (buf1.c_cflag != buf2.c_cflag || buf1.c_ispeed != buf2.c_ispeed || buf1.c_ospeed != buf2.c_ospeed)
+  {
+    cerr << "Configurations are not reflected." << endl;
     return false;
   }
 
