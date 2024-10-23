55e83064ad8ca6877d8ddff741b1001c46c7ced4
dohi <masa0u0masa1215@gmail.com>
2024-10-23 15:17:54 +0900

    fix

--- tobas_deb/create_deb_ubuntu.sh
+++ tobas_deb/create_deb_ubuntu.sh
@@ -2,6 +2,18 @@
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
 UBUNTU_WS=${TOBAS_DEB}/ubuntu
+
+# ワークスペースに移動
 cd ${ROS2_WORKSPACE}
-colcon build --packages-up-to tobas --parallel-workers $(nproc) --install-base ${UBUNTU_WS}/opt/tobas --build-base ${ROS2_WORKSPACE}/build --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+# 過去のビルドディレクトリを削除
+# そうしないと自動的にシンボリックリンク生成されることがある
+colcon clean workspace -y
+rm -rf ${UBUNTU_WS}/opt/tobas
+
+# ビルド
+# シンボリックリンクを作らないように--merge-installオプションをつける
+colcon build --merge-install --packages-up-to tobas --parallel-workers $(nproc) --install-base ${UBUNTU_WS}/opt/tobas --build-base ${ROS2_WORKSPACE}/build --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+# debパッケージを作成
 fakeroot dpkg-deb --build ${UBUNTU_WS} ${TOBAS_DEB}
