18fca4163d1ce3c3290c38fe651e4dcf8cab70fe
dohi <masa0u0masa1215@gmail.com>
2024-11-09 16:41:35 +0900

    .

--- tobas_gui/tobas_hardware_setup/src/rotor_test/rotor_test.cpp
+++ tobas_gui/tobas_hardware_setup/src/rotor_test/rotor_test.cpp
@@ -21,13 +21,13 @@ RotorTestWidget::RotorTestWidget(rclcpp::Node::SharedPtr node, const tobas::Dron
 
   const auto instruction = new qt::DescriptionWidget(
     "1. Connect the ESCs to the FC in the correct order.\n\n"
-    "2. Press \"Start\" button.\n\n"
+    "2. Press \"Start\" button to enable motors.\n\n"
     "3. For all motors, confirm the followings:\n"
     "   - The motor rotates in the correct direction. If not, swap any two of the three ESC-motor connections.\n"
-    "   - The motor does not rotate when the command RPM is 0.\n"
-    "   - The sound of rotation gradually increases as the command RPM approaches its maximum value.\n"
-    "   - Two motors of the same model produce roughly the same sound level at the same command RPM.\n\n"
-    "4. Press \"Stop\" button.\n\n",
+    "   - The motor does not rotate when the command RPM is 0.\n\n"
+    "4. Tune the control gain of each motor to the maximum value at which no vibrations or abnormal noise occur.\n\n"
+    "5. Press \"Save\" button to save the control gains.\n\n"
+    "6. Press \"Stop\" button to disable motors.\n\n",
     kBodyPSize);
   rows_->addWidget(instruction);
 
