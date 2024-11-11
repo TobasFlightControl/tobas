08e502eb78beb65c64752fe8669f0ab84653058a
dohi <masa0u0masa1215@gmail.com>
2024-11-11 10:00:03 +0900

    add: echo.cpp

---
+++ tobas_core/tobas_keyboard/examples/echo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+#include <tobas_keyboard/keyboard_reader.hpp>
+
+#define HEX_STREAM(c) "0x" << setw(2) << setfill('0') << hex << uppercase << (int)c
+
+using namespace std;
+
+int main()
+{
+  keyboard::KeyboardReader keyboard;
+
+  while (true)
+  {
+    const auto c = keyboard.readKey();
+    if (c < 0)
+      cout << "Failed to read keyboard." << endl;
+    else if (c == 0)
+      continue;
+    else if (c < 0x21)
+      cout << "Special Command : " << HEX_STREAM(c) << endl;
+    else
+      cout << "Normal Character: " << c << endl;
+  }
+}
--- tobas_core/tobas_keyboard/src/keyboard_reader.cpp
+++ tobas_core/tobas_keyboard/src/keyboard_reader.cpp
@@ -21,7 +21,7 @@ KeyboardReader::KeyboardReader()
 
   // 入力受付のタイムリミットを設定
   // https://stackoverflow.com/questions/2917881/how-to-implement-a-timeout-in-read-function-call
-  changed_.c_cc[VMIN] = 0;  // 最低文字数を0に設定．つまりデータがなくてもすぐ返す．
+  changed_.c_cc[VMIN] = 0;  // 最低文字数を0に設定．つまり入力がなくてもすぐ返す．
   changed_.c_cc[VTIME] = 0;
 
   tcsetattr(STD_INPUT_FD, TCSANOW, &changed_);
