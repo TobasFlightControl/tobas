44e95e073bef36e0bd458a0e34ba31f6b39d41c0
dohi <masa0u0masa1215@gmail.com>
2024-11-11 22:49:38 +0900

    "Random"ボタンを追加

--- tobas_gui/tobas_setup_assistant/src/joint_state_publisher.cpp
+++ tobas_gui/tobas_setup_assistant/src/joint_state_publisher.cpp
@@ -1,4 +1,5 @@
 #include <QPushButton>
+#include <QHBoxLayout>
 
 #include <tobas_std_tools/vector.hpp>
 #include <tobas_ros2_tools/register.hpp>
@@ -15,31 +16,51 @@ namespace gui
 namespace setup_assistant
 {
 JointStatePublisherWidget::JointStatePublisherWidget(rclcpp::Node::SharedPtr node, const RobotInfo& robot)
-  : node_(node), robot_(robot)
+  : node_(node), robot_(robot), rnd_gen_(rnd_dev_())
 {
-  const auto rows = new QVBoxLayout();
-  setLayout(rows);
+  slider_rows_ = new QVBoxLayout();
 
   const auto scroll_area = new qt::ScrollArea();
   scroll_area->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
-  rows->addWidget(scroll_area);
-
-  slider_rows_ = new QVBoxLayout();
   scroll_area->setLayout(slider_rows_);
 
-  const auto button_rows = new QVBoxLayout();
-  rows->addLayout(button_rows);
-
   const auto center_button = new QPushButton("Center");
-  button_rows->addWidget(center_button);
+  const auto random_button = new QPushButton("Random");
 
-  // Register publishers
-  js_pub_ = ros2::createPublisher<sensor_msgs::msg::JointState>(node_, tobas::kJointStatesTopic);
-  drs_pub_ = ros2::createPublisher<moveit_msgs::msg::DisplayRobotState>(node_, "display_robot_state", false, true);
+  center_button->setFixedHeight(kButtonHeight);
+  random_button->setFixedHeight(kButtonHeight);
+
+  // Layout
+  const auto button_cols = new QHBoxLayout();
+  button_cols->addWidget(center_button);
+  button_cols->addWidget(random_button);
+
+  const auto rows = new QVBoxLayout();
+  rows->addWidget(scroll_area);
+  rows->addLayout(button_cols);
+
+  setLayout(rows);
 
   // Connections
   connect(&robot, &RobotInfo::loaded, this, &self::onRobotLoaded);
   connect(center_button, &QPushButton::clicked, this, &self::onCenterButtonClicked);
+  connect(random_button, &QPushButton::clicked, this, &self::onRandomButtonClicked);
+
+  // Register publishers
+  js_pub_ = ros2::createPublisher<sensor_msgs::msg::JointState>(node_, tobas::kJointStatesTopic);
+  drs_pub_ = ros2::createPublisher<moveit_msgs::msg::DisplayRobotState>(node_, "display_robot_state", false, true);
+}
+
+void JointStatePublisherWidget::publish()
+{
+  js_.header.stamp = node_->get_clock()->now();
+
+  auto js = make_unique<sensor_msgs::msg::JointState>(js_);
+  js_pub_->publish(std::move(js));
+
+  auto drs = make_unique<moveit_msgs::msg::DisplayRobotState>();
+  drs->state.joint_state = js_;
+  drs_pub_->publish(std::move(drs));
 }
 
 void JointStatePublisherWidget::onRobotLoaded()
@@ -104,19 +125,20 @@ void JointStatePublisherWidget::onValueChanged(double value, const string& jnt_n
 void JointStatePublisherWidget::onCenterButtonClicked()
 {
   for (auto& slider : sliders_)
-    slider->setCenterValue();
+  {
+    const auto value = (slider->getMinimum() + slider->getMaximum()) / 2;
+    slider->setValue(value);
+  }
 }
 
-void JointStatePublisherWidget::publish()
+void JointStatePublisherWidget::onRandomButtonClicked()
 {
-  js_.header.stamp = node_->get_clock()->now();
-
-  auto js = make_unique<sensor_msgs::msg::JointState>(js_);
-  js_pub_->publish(std::move(js));
-
-  auto drs = make_unique<moveit_msgs::msg::DisplayRobotState>();
-  drs->state.joint_state = js_;
-  drs_pub_->publish(std::move(drs));
+  for (auto& slider : sliders_)
+  {
+    uniform_real_distribution<double> uniform(slider->getMinimum(), slider->getMaximum());
+    const auto value = uniform(rnd_gen_);
+    slider->setValue(value);
+  }
 }
 }  // namespace setup_assistant
 }  // namespace gui
