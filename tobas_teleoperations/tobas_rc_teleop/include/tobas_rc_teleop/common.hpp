0582b57ba71d9d44ed93bec08bfd17377f6069ee
dohi <masa0u0masa1215@gmail.com>
2024-12-25 11:32:07 +0900

    change default max yawrate

--- tobas_teleoperations/tobas_rc_teleop/include/tobas_rc_teleop/common.hpp
+++ tobas_teleoperations/tobas_rc_teleop/include/tobas_rc_teleop/common.hpp
@@ -15,5 +15,5 @@ static constexpr double kDefaultMaxVerVel = 3.;          // [m/s]
 static constexpr double kDefaultMaxHorAcc = 5.;          // [m/s^2]
 static constexpr double kDefaultMaxVerAcc = 4.;          // [m/s^2]
 static constexpr double kDefaultMaxAttitude = M_PI / 6;  // [rad]
-static constexpr double kDefaultMaxYawrate = M_PI;       // [rad/s]
+static constexpr double kDefaultMaxYawrate = M_PI_2;     // [rad/s]
 }  // namespace tobas_rc_teleop
