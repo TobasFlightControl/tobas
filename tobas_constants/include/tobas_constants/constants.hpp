27ed87ac1d60e5be1146141c6d7ed5e25028cb80
dohi <masa0u0masa1215@gmail.com>
2025-01-03 20:34:41 +0900

    impl: tobas_disturbance_observer

--- tobas_constants/include/tobas_constants/constants.hpp
+++ tobas_constants/include/tobas_constants/constants.hpp
@@ -52,6 +52,7 @@ static constexpr char kEventTopic[] = "event";
 static constexpr char kLatencyTopic[] = "latency";
 static constexpr char kArmingTopic[] = "arming";
 static constexpr char kPreArmCheckTopic[] = "prearm_check";
+static constexpr char kDisturbanceForceTopic[] = "disturbance_force";
 static constexpr char kThrottledTopicNS[] = "throttled";
 static constexpr char kRemoteIfaceTopicNS[] = "remote_interface";
 // Command
