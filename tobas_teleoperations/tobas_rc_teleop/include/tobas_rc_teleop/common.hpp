7e05d1fb0199d3edeb7d8a4d6596b42b6e6603f6
dohi <masa0u0masa1215@gmail.com>
2025-01-18 17:12:27 +0900

    edit: kDefaultMaxAttitude: pi/6 -> pi/2

--- tobas_teleoperations/tobas_rc_teleop/include/tobas_rc_teleop/common.hpp
+++ tobas_teleoperations/tobas_rc_teleop/include/tobas_rc_teleop/common.hpp
@@ -10,10 +10,10 @@ static constexpr double kMaxPositionError = 5.;  // [m]
 static constexpr double kMaxYawError = M_PI;     // [rad]
 
 // Default parameters
-static constexpr double kDefaultMaxHorVel = 3.;          // [m/s]
-static constexpr double kDefaultMaxVerVel = 3.;          // [m/s]
-static constexpr double kDefaultMaxHorAcc = 5.;          // [m/s^2]
-static constexpr double kDefaultMaxVerAcc = 4.;          // [m/s^2]
-static constexpr double kDefaultMaxAttitude = M_PI / 6;  // [rad]
-static constexpr double kDefaultMaxYawrate = M_PI_2;     // [rad/s]
+static constexpr double kDefaultMaxHorVel = 3.;        // [m/s]
+static constexpr double kDefaultMaxVerVel = 3.;        // [m/s]
+static constexpr double kDefaultMaxHorAcc = 5.;        // [m/s^2]
+static constexpr double kDefaultMaxVerAcc = 4.;        // [m/s^2]
+static constexpr double kDefaultMaxAttitude = M_PI_2;  // [rad]
+static constexpr double kDefaultMaxYawrate = M_PI_2;   // [rad/s]
 }  // namespace tobas_rc_teleop
