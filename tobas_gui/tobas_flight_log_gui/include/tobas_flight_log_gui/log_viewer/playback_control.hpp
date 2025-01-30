3ccda21bdb5559885eca0cc0cec04ec06c0c9dc6
dohi <masa0u0masa1215@gmail.com>
2025-01-30 21:59:15 +0900

    wip: tobas_flight_log_gui

--- tobas_gui/tobas_flight_log_gui/include/tobas_flight_log_gui/log_viewer/playback_control.hpp
+++ tobas_gui/tobas_flight_log_gui/include/tobas_flight_log_gui/log_viewer/playback_control.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <QWidget>
+#include <QPushButton>
+#include <QSlider>
+#include <QLabel>
 
 namespace gui
 {
@@ -13,10 +15,35 @@ class PlaybackControlWidget : public QWidget
   using self = PlaybackControlWidget;
   using super = QWidget;
 
+Q_SIGNALS:
+  void timeChanged(int msec);
+
 public:
   explicit PlaybackControlWidget();
 
+  void reset();
+
+  int getDuration() const;
+  int getCurrentTime() const;
+
+  void setDuration(int msec);
+  void setCurrentTime(int msec);
+
 private:
+  QPushButton* play_button_;
+  QSlider* slider_;
+  QLabel* current_time_;
+  QLabel* remaining_time_;
+
+  QString formatTime(int msec);
+
+  void updateCurrentTimeLabel(int cur_time);
+  void updateRemainingTimeLabel(int cur_time);
+  void updateTimeLabels(int cur_time);
+
+private Q_SLOTS:
+  void onPlayButtonToggled(bool checked);
+  void onSliderValueChanged(int value);
 };
 }  // namespace log
 }  // namespace gui
--- tobas_gui/tobas_flight_log_gui/src/log_viewer/playback_control.cpp
+++ tobas_gui/tobas_flight_log_gui/src/log_viewer/playback_control.cpp
@@ -1,3 +1,6 @@
+#include <QStyle>
+#include <QHBoxLayout>
+
 #include "tobas_flight_log_gui/log_viewer/playback_control.hpp"
 
 namespace gui
@@ -6,7 +9,100 @@ namespace log
 {
 PlaybackControlWidget::PlaybackControlWidget()
 {
-  // TODO
+  play_button_ = new QPushButton();
+  play_button_->setCheckable(true);
+
+  slider_ = new QSlider(Qt::Horizontal);
+  slider_->setMinimum(0);
+
+  current_time_ = new QLabel();
+  remaining_time_ = new QLabel();
+
+  // Layout
+  const auto cols = new QHBoxLayout();
+  cols->addWidget(play_button_);
+  cols->addWidget(current_time_);
+  cols->addWidget(slider_);
+  cols->addWidget(remaining_time_);
+  setLayout(cols);
+
+  // Connection
+  connect(play_button_, &QPushButton::toggled, this, &self::onPlayButtonToggled);
+  connect(slider_, &QSlider::valueChanged, this, &self::onSliderValueChanged);
+
+  reset();
+}
+
+void PlaybackControlWidget::reset()
+{
+  play_button_->setChecked(false);
+  play_button_->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+
+  slider_->setMaximum(0);
+
+  current_time_->setText("00:00");
+  remaining_time_->setText("00:00");
+}
+
+int PlaybackControlWidget::getDuration() const
+{
+  return slider_->maximum();
+}
+
+int PlaybackControlWidget::getCurrentTime() const
+{
+  return slider_->value();
+}
+
+void PlaybackControlWidget::setDuration(int msec)
+{
+  slider_->setMaximum(msec);
+  updateTimeLabels(getCurrentTime());
+}
+
+void PlaybackControlWidget::setCurrentTime(int msec)
+{
+  slider_->setValue(msec);
+}
+
+void PlaybackControlWidget::updateCurrentTimeLabel(int cur_time)
+{
+  current_time_->setText(formatTime(cur_time));
+}
+
+void PlaybackControlWidget::updateRemainingTimeLabel(int cur_time)
+{
+  remaining_time_->setText(formatTime(getDuration() - cur_time));
+}
+
+void PlaybackControlWidget::updateTimeLabels(int cur_time)
+{
+  updateCurrentTimeLabel(cur_time);
+  updateRemainingTimeLabel(cur_time);
+}
+
+QString PlaybackControlWidget::formatTime(int msec)
+{
+  auto seconds = msec / 1000;
+  const auto minutes = seconds / 60;
+  seconds %= 60;
+
+  return QString("%1:%2").arg(minutes, 2, 10, QLatin1Char('0')).arg(seconds, 2, 10, QLatin1Char('0'));
+}
+
+void PlaybackControlWidget::onPlayButtonToggled(bool checked)
+{
+  if (checked)
+    play_button_->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+  else
+    play_button_->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+
+  Q_EMIT timeChanged(getCurrentTime());
+}
+
+void PlaybackControlWidget::onSliderValueChanged(int value)
+{
+  Q_EMIT timeChanged(value);
 }
 }  // namespace log
 }  // namespace gui
