d8166a3fa6b830078ef364c40b9edf11130fd0a7
dohi <masa0u0masa1215@gmail.com>
2025-03-08 14:22:28 +0900

    並進の外乱補償はデフォルトでオン

--- tobas_gui/tobas_setup_assistant/src/setting_widgets/controller/multirotor_pid.cpp
+++ tobas_gui/tobas_setup_assistant/src/setting_widgets/controller/multirotor_pid.cpp
@@ -19,7 +19,7 @@ MultirotorPIDWidget::MultirotorPIDWidget(
   setLayout(rows);
 
   do_dist_comp_trans_ = new QCheckBox("Do Disturbance Compensation (Translation)");
-  do_dist_comp_trans_->setChecked(false);
+  do_dist_comp_trans_->setChecked(true);
   rows->addWidget(do_dist_comp_trans_);
 
   do_dist_comp_rot_ = new QCheckBox("Do Disturbance Compensation (Rotation)");
