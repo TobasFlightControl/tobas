70475b975bedd601e28622f118227721ead394f1
dohi <masa0u0masa1215@gmail.com>
2025-03-10 17:17:49 +0900

    impl: tobas_landing_detection

--- tobas_constants/include/tobas_constants/constants.hpp
+++ tobas_constants/include/tobas_constants/constants.hpp
@@ -49,6 +49,7 @@ static constexpr char kArmingTopic[] = "arming";
 static constexpr char kPreArmCheckTopic[] = "prearm_check";
 static constexpr char kPostArmCheckTopic[] = "postarm_check";
 static constexpr char kDisturbanceForceTopic[] = "disturbance_force";
+static constexpr char kLandedTopic[] = "landed";
 static constexpr char kRosbagStateTopic[] = "rosbag_state";
 static constexpr char kThrottledTopicNS[] = "throttled";
 static constexpr char kRemoteIfaceTopicNS[] = "remote_interface";
