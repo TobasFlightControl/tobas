735369d4a9a443cc04632febdd41bfbb5eef80b4
dohi <masa0u0masa1215@gmail.com>
2025-03-10 17:55:25 +0900

    デフォルト値を変更

--- tobas_gui/tobas_control_system/src/mission_planner/fields/acceptance_radius.cpp
+++ tobas_gui/tobas_control_system/src/mission_planner/fields/acceptance_radius.cpp
@@ -11,9 +11,9 @@ namespace field
 AcceptanceRadiusWidget::AcceptanceRadiusWidget()
 {
   spinbox_ = new qt::DoubleSpinBox();
-  spinbox_->setDecimals(3);
-  spinbox_->setMinimum(1e-3);
-  spinbox_->setValue(1.0);
+  spinbox_->setDecimals(2);
+  spinbox_->setMinimum(1e-2);
+  spinbox_->setValue(1.);
   spinbox_->setSuffix(" m");
 
   const auto cols = new QHBoxLayout();
