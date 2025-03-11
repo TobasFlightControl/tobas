35686dc6a5d6a8cac4a1edbb678b0e1620c8558a
dohi <masa0u0masa1215@gmail.com>
2025-03-11 22:19:55 +0900

    rclcpp::Timeの引き算を回避

--- tobas_tools/src/control_latency_publisher.cpp
+++ tobas_tools/src/control_latency_publisher.cpp
@@ -1,3 +1,4 @@
+#include <tobas_ros2_tools/time.hpp>
 #include <tobas_constants/constants.hpp>
 
 #include "../include/tobas_tools/control_latency_publisher.hpp"
@@ -16,7 +17,7 @@ void ControlLatencyPublisher::initialize(rclcpp::Node::SharedPtr node)
 
 void ControlLatencyPublisher::publish(const builtin_interfaces::msg::Time& start_time)
 {
-  const auto cur_time = node_->get_clock()->now();
+  const builtin_interfaces::msg::Time cur_time(node_->get_clock()->now());  // クロックタイプの削除して例外を回避
 
   auto msg = std::make_unique<tobas_msgs::msg::Latency>();
   msg->header.stamp = cur_time;
