2a059f6e88e26522e33498f50fd4dc565b7831fc
dohi <masa0u0masa1215@gmail.com>
2025-03-17 11:05:22 +0900

    .

--- tobas_deb/create_deb_t1.sh
+++ tobas_deb/create_deb_t1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
-ASO_WS=$(realpath ${TOBAS_DEB}/t1)
-fakeroot dpkg-deb --build ${ASO_WS} ${TOBAS_DEB}
+WORKSPACE=$(realpath ${TOBAS_DEB}/t1)
+fakeroot dpkg-deb --build ${WORKSPACE} ${TOBAS_DEB}
--- tobas_deb/create_deb_ubuntu.sh
+++ tobas_deb/create_deb_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
-UBUNTU_WS=${TOBAS_DEB}/ubuntu
+WORKSPACE=${TOBAS_DEB}/ubuntu
 
 # ワークスペースに移動
 cd ${ROS2_WORKSPACE}
@@ -9,11 +9,11 @@ cd ${ROS2_WORKSPACE}
 # 過去のビルドディレクトリを削除
 # そうしないと自動的にシンボリックリンク生成されることがある
 colcon clean workspace -y
-rm -rf ${UBUNTU_WS}/opt/tobas
+rm -rf ${WORKSPACE}/opt/tobas
 
 # ビルド
 # シンボリックリンクを作らないように--merge-installオプションをつける
-colcon build --merge-install --packages-up-to tobas --parallel-workers $(nproc) --install-base ${UBUNTU_WS}/opt/tobas --build-base ${ROS2_WORKSPACE}/build --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --merge-install --packages-up-to tobas --parallel-workers $(nproc) --install-base ${WORKSPACE}/opt/tobas --build-base ${ROS2_WORKSPACE}/build --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 # debパッケージを作成
-fakeroot dpkg-deb --build ${UBUNTU_WS} ${TOBAS_DEB}
+fakeroot dpkg-deb --build ${WORKSPACE} ${TOBAS_DEB}
