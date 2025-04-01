90689f98b075bee8089be6bd65543a66ee404c59
dohi <masa0u0masa1215@gmail.com>
2025-04-01 23:07:50 +0900

    add: tobas_f550_np

---
+++ tobas_example_packages/tobas_f550_np.TBS/tobas_f550_np_config/launch/robot_state_publisher.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+# Template parameters
+DRONE_NAME = "f550_np"
+CONFIG_PKG_NAME = "tobas_f550_np_config"
+
+# Arguments
+LOG_LEVEL = "log_level"
+OUTPUT = "output"
+USE_SIM_TIME = "use_sim_time"
+USER_DEBUG = "user_debug"
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Declare arguments
+    ld.add_action(DeclareLaunchArgument(LOG_LEVEL, default_value="info"))
+    ld.add_action(DeclareLaunchArgument(OUTPUT, default_value="screen"))
+    ld.add_action(DeclareLaunchArgument(USE_SIM_TIME, default_value="false"))
+    ld.add_action(DeclareLaunchArgument(USER_DEBUG, default_value="false"))
+
+    # Get arguments
+    log_level = LaunchConfiguration(LOG_LEVEL)
+    output = LaunchConfiguration(OUTPUT)
+    use_sim_time = LaunchConfiguration(USE_SIM_TIME)
+    user_debug = LaunchConfiguration(USER_DEBUG)
+
+    urdf_content = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            PathJoinSubstitution([FindPackageShare(CONFIG_PKG_NAME), "urdf", "drone.xacro"]),
+            f" DEBUG:=",
+            user_debug,
+        ]
+    )
+
+    ld.add_action(
+        Node(
+            namespace=DRONE_NAME,
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[{"robot_description": urdf_content, "use_sim_time": use_sim_time}],
+            ros_arguments=["--log-level", log_level],
+            output=output,
+        )
+    )
+
+    return ld
