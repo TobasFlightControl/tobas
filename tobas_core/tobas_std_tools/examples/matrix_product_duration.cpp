3d57ea89290c3ac1ba9c694623bd0b7dde7ffc2f
dohi <masa0u0masa1215@gmail.com>
2025-04-02 00:17:34 +0900

    add: matrix_product_duration.cpp

---
+++ tobas_core/tobas_std_tools/examples/matrix_product_duration.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <eigen3/Eigen/Core>
+
+#include <tobas_std_tools/stopwatch.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    cerr << "Usage: " << argv[0] << " <Size> <Trials>" << endl;
+    return EXIT_FAILURE;
+  }
+
+  const auto size = stoul(argv[1]);
+  const auto trials = stoul(argv[2]);
+
+  const auto A = Eigen::MatrixXd::Random(size, size);
+  const auto B = Eigen::MatrixXd::Random(size, size);
+
+  tobas_std::Stopwatch stopwatch(trials);
+
+  for (size_t _ = 0; _ < trials; ++_)
+  {
+    stopwatch.start();
+    const auto C = (A * B).eval();
+    stopwatch.stop();
+  }
+
+  return EXIT_SUCCESS;
+}
