c4fc21b7362da13ba0d113d2598ccec8c1fc6ef8
dohi <masa0u0masa1215@gmail.com>
2025-04-02 11:43:42 +0900

    edit: matrix_product_duration.cpp

--- tobas_core/tobas_std_tools/examples/matrix_product_duration.cpp
+++ tobas_core/tobas_std_tools/examples/matrix_product_duration.cpp
@@ -4,28 +4,55 @@
 #include <tobas_std_tools/stopwatch.hpp>
 
 using namespace std;
+using namespace std::chrono;
+using namespace Eigen;
 
-int main(int argc, char** argv)
+template <typename T>
+void process(int n, int iter)
 {
-  if (argc != 3)
+  const auto A = Matrix<T, Dynamic, Dynamic>::Random(n, n);
+  const auto B = Matrix<T, Dynamic, Dynamic>::Random(n, n);
+
+  tobas_std::Stopwatch stopwatch(iter);
+
+  for (int i = 0; i < iter; ++i)
   {
-    cerr << "Usage: " << argv[0] << " <Size> <Trials>" << endl;
-    return EXIT_FAILURE;
-  }
+    cout << "Iter " << i << endl;
 
-  const auto size = stoul(argv[1]);
-  const auto trials = stoul(argv[2]);
-
-  const auto A = Eigen::MatrixXf::Random(size, size);
-  const auto B = Eigen::MatrixXf::Random(size, size);
-
-  tobas_std::Stopwatch stopwatch(trials);
-
-  for (size_t _ = 0; _ < trials; ++_)
-  {
     stopwatch.start();
     const auto C = (A * B).eval();
     stopwatch.stop();
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc < 4)
+  {
+    cerr << "Usage: " << argv[0] << " <Scalar> <Matrix Size> <Iterations>" << endl;
+    return EXIT_FAILURE;
+  }
+
+  const auto scalar = argv[1];
+  const auto n = atoi(argv[2]);
+  const auto iter = atoi(argv[3]);
+
+  if (strcmp(scalar, "char") == 0)
+    process<char>(n, iter);
+  else if (strcmp(scalar, "short") == 0)
+    process<short>(n, iter);
+  else if (strcmp(scalar, "int") == 0)
+    process<int>(n, iter);
+  else if (strcmp(scalar, "long") == 0)
+    process<long>(n, iter);
+  else if (strcmp(scalar, "float") == 0)
+    process<float>(n, iter);
+  else if (strcmp(scalar, "double") == 0)
+    process<double>(n, iter);
+  else
+  {
+    cerr << "Invalid scalar type: " << scalar << endl;
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
