8a0e31336fed11fa82b0b23b917c7fab2960534b
dohi <masa0u0masa1215@gmail.com>
2025-04-21 17:29:59 +0900

    .

--- tobas_gui/tobas_control_system/include/tobas_control_system/cpu_viewer.hpp
+++ tobas_gui/tobas_control_system/include/tobas_control_system/cpu_viewer.hpp
@@ -19,7 +19,7 @@ class CPUViewerWidget : public QWidget
   static constexpr int kBarHeight = 40;
 
   static constexpr double kMinTemp = 0.;    // [degC]
-  static constexpr double kMaxTemp = 90.;   // [degC]
+  static constexpr double kMaxTemp = 100.;  // [degC]
   static constexpr double kMinLoad = 0.;    // [%]
   static constexpr double kMaxLoad = 100.;  // [%]
 
