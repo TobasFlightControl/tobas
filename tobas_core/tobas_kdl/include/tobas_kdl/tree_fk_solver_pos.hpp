2983d0101e61e462d11a7bb726044598464e88ef
dohi <masa0u0masa1215@gmail.com>
2025-05-05 19:33:22 +0900

    .

--- tobas_core/tobas_kdl/include/tobas_kdl/tree_fk_solver_pos.hpp
+++ tobas_core/tobas_kdl/include/tobas_kdl/tree_fk_solver_pos.hpp
@@ -15,14 +15,16 @@ public:
 
   int JntToCart(const JntArray& q, const std::string& seg_name);
 
-  const Frame& getFrame() const
-  {
-    return p_out_;
-  }
+  inline const Frame& getFrame() const;
 
 private:
   Frame p_out_;
 
   Frame recursiveFk(const JntArray& q, const SegmentMap::const_iterator& seg_it);
 };
+
+inline const Frame& TreeFkSolverPos::getFrame() const
+{
+  return p_out_;
+}
 }  // namespace kdl
