c8da0efab72e8247dc9a1dea3784566820440c61
dohi <masa0u0masa1215@gmail.com>
2025-05-06 10:40:30 +0900

    .

--- tobas_core/tobas_linux/include/tobas_linux/command_executor.hpp
+++ tobas_core/tobas_linux/include/tobas_linux/command_executor.hpp
@@ -12,13 +12,15 @@ public:
 
   bool execute(const std::string& command);
 
-  inline const std::string& getOutput() const
-  {
-    return output_;
-  }
+  inline const std::string& getOutput() const;
 
 private:
   std::array<char, 128> buffer_;
   std::string output_;
 };
+
+inline const std::string& CommandExecutor::getOutput() const
+{
+  return output_;
+}
 }  // namespace linux
