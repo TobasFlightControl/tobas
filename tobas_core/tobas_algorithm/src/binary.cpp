eea4159701b566ec0c38cb314cad5ab5dfd163d2
dohi <masa0u0masa1215@gmail.com>
2025-05-18 09:41:23 +0900

    .

--- tobas_core/tobas_algorithm/src/binary.cpp
+++ tobas_core/tobas_algorithm/src/binary.cpp
@@ -2,17 +2,15 @@
 
 #include <cmath>
 
-using namespace std;
-
 namespace algo
 {
 float decodeR32(uint32_t bin)
 {
   // exponentは符号付き整数型でなければならない
   // exponentが符号なしだと続く(exponent - 150)がオーバーフローしてしまう
-  const int sign = bin >> 31 ? -1 : 1;
-  const int exponent = (bin >> 23) & 0xFF;
-  const int mantissa = (exponent == 0) ? (bin & 0x7FFFFF) << 1 : (bin & 0x7FFFFF) | 0x800000;
-  return sign * mantissa * pow(2, exponent - 150);
+  const int32_t sign = bin >> 31 ? -1 : 1;
+  const int32_t exponent = (bin >> 23) & 0xFF;
+  const int32_t mantissa = (exponent == 0) ? (bin & 0x7FFFFF) << 1 : (bin & 0x7FFFFF) | 0x800000;
+  return sign * mantissa * std::pow(2, exponent - 150);
 }
 }  // namespace algo
