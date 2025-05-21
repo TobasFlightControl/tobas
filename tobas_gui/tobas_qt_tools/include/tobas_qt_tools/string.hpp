efa66d31af01a8cdf27f3e66a3a2686425c5c371
dohi <masa0u0masa1215@gmail.com>
2025-05-21 18:32:05 +0900

    .

--- tobas_gui/tobas_qt_tools/include/tobas_qt_tools/string.hpp
+++ tobas_gui/tobas_qt_tools/include/tobas_qt_tools/string.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include <QStringList>
 
 namespace qt
 {
-QStringList stringListFromStdToQt(const std::vector<std::string>& list);
+QStringList stringListFromStdToQt(const std::vector<std::string>& src);
+std::vector<std::string> stringListFromQtToStd(const QStringList& src);
 }  // namespace qt
