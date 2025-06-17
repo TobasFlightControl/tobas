822685bab45542dc22bbffee8ef951e2399f0cb4
Masayoshi Dohi <masa0u0masa1215@gmail.com>
2025-06-17 12:15:45 +0900

    .

--- tobas_core/tobas_time_tools/src/frequency_measure.cpp
+++ tobas_core/tobas_time_tools/src/frequency_measure.cpp
@@ -13,15 +13,17 @@ FrequencyMeasure::FrequencyMeasure(const std::chrono::nanoseconds& period) : per
 
 void FrequencyMeasure::count()
 {
+  // Initialize
   if (count_ == 0) {
     start_time_ = steady_clock::now();
   }
 
+  // Increase count
   ++count_;
 
+  // Output
   const auto end_time = steady_clock::now();
   const auto dur = end_time - start_time_;
-
   if (dur > period_) {
     const auto freq = count_ / duration<double>(dur).count();
     cout << freq << " Hz" << endl;
