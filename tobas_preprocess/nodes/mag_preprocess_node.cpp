2d9aaebfe847e4a6d24b634e11f5040c86f3cfe1
Masayoshi Dohi <masa0u0masa1215@gmail.com>
2025-06-21 15:24:09 +0900

    change: 地磁気の共分散を動的に計算

--- tobas_preprocess/nodes/mag_preprocess_node.cpp
+++ tobas_preprocess/nodes/mag_preprocess_node.cpp
@@ -1,61 +1,67 @@
 #include <tobas_constants/constants.hpp>
-#include <tobas_math/core.hpp>
-#include <tobas_node/node.hpp>
-
+#include <tobas_dsp/low_pass_filter.hpp>
+#include <tobas_dsp/noise_variance_filter.hpp>
 #include <tobas_msgs_adapter/magnetic_field_stamped.hpp>
 #include <tobas_msgs_adapter/magnetic_field_with_covariance_stamped.hpp>
+#include <tobas_node/node.hpp>
+#include <tobas_ros2_tools/time.hpp>
+
+using namespace std;
 
 class MagPreprocessNode : public tobas::BaseNode
 {
+  static constexpr double kNoiseFiltrerHpfCutoff = 10.;  // [Hz] (G(1Hz) ~ 0.1, G(20Hz) ~ 0.9)
+  static constexpr size_t kNoiseFilterWindowSize = 100;
+
   using self = MagPreprocessNode;
   using super = tobas::BaseNode;
-
-  static constexpr double kMagScale = 5e+4;  // TODO: 経緯高度による変化を反映
 
 public:
   explicit MagPreprocessNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
-  Eigen::Matrix3d mag_noise_cov_;  // [-]
+  tobas_msgs::MagneticFieldStamped::ConstSharedPtr mag_raw_;
+  dsp::NoiseVarianceFilter<double, 3, kNoiseFilterWindowSize> mag_noise_;
 
   ros2::PublisherPtr<tobas_msgs::MagneticFieldWithCovarianceStamped> mag_pub_;
   ros2::SubscriberPtr<tobas_msgs::MagneticFieldStamped> mag_raw_sub_;
-
-  bool magNoiseStddevCb(const long& p);
 
   void magRawCb(const tobas_msgs::MagneticFieldStamped::ConstSharedPtr& mag_raw);
 };
 
 MagPreprocessNode::MagPreprocessNode(const rclcpp::NodeOptions& options) : super("mag_preprocess", options)
 {
-  // 地磁気はサンプリング周波数が小さくオンライン分散推定は困難なため，固定値を用いる．
-  // IIS2MDCのデータシートによると，LPF付きでノイズのRMS (= 標準偏差) の最大値が4.6mG (= 460nT)．
-  // ナイキスト周波数を考慮したLPFを通さない場合はその√2倍で650nTほどだと思われる．
-  // 電源やモータ等の環境ノイズも考えて，デフォルト値はそれよりさらに大きい値に設定．
-  addDynamicIntParam("mag_noise_stddev", &self::magNoiseStddevCb, this, 1000, 1, 5000, " nT");
-
   mag_pub_ = createPublisher<tobas_msgs::MagneticFieldWithCovarianceStamped>(tobas::kMagTopic);
   mag_raw_sub_ = createSubscriber(tobas::kMagRawTopic, &self::magRawCb, this);
 }
 
-bool MagPreprocessNode::magNoiseStddevCb(const long& p)
-{
-  const auto noise_stddev = static_cast<double>(p) / kMagScale;  // [-]
-  const auto noise_var = math::sqr(noise_stddev);
-  mag_noise_cov_ = Eigen::Vector3d::Constant(noise_var).asDiagonal();
-  return true;
-}
-
 void MagPreprocessNode::magRawCb(const tobas_msgs::MagneticFieldStamped::ConstSharedPtr& mag_raw)
 {
+  // Initialize
+  if (!mag_raw_) {
+    if (!mag_noise_.initialize(kNoiseFiltrerHpfCutoff, mag_raw->mag.data)) {
+      TOBAS_ERROR("Failed to initialize magnetic field noise variance filter.");
+      return;
+    }
+    mag_raw_ = mag_raw;
+    return;
+  }
+
+  // Compute time difference
+  const auto dt = (mag_raw->header.stamp - mag_raw_->header.stamp).seconds();
+  mag_raw_ = mag_raw;
+
+  // Update noise filter
+  mag_noise_.update(mag_raw->mag.data, dt);
+
   // Create message
   auto mag_out = std::make_unique<tobas_msgs::MagneticFieldWithCovarianceStamped>();
   mag_out->header = mag_raw->header;
   mag_out->mag.mag = mag_raw->mag;
-  mag_out->mag.covariance = mag_noise_cov_;
+  mag_out->mag.covariance = mag_noise_.noiseVariance();
 
   // Publish message
-  mag_pub_->publish(std::move(mag_out));
+  mag_pub_->publish(move(mag_out));
 }
 
 RCLCPP_COMPONENTS_REGISTER_NODE(MagPreprocessNode)
