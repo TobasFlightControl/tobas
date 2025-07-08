0fc67f2a66dc5700872f0b1bd56525b40785b03f
Masayoshi Dohi <m.dohi@tobas.jp>
2025-07-09 00:43:06 +0900

    add: Add UADF parse methods

--- tobas_uadf/src/parser.cpp
+++ tobas_uadf/src/parser.cpp
@@ -2,27 +2,25 @@
 
 #include <iostream>
 
-#include <tinyxml2.h>
 #include <urdf_parser/urdf_parser.h>
 
 #include <tobas_ros2_tools/xacro.hpp>
+#include <tobas_string_tools/stream.hpp>
+#include <tobas_xml_tools/core.hpp>
 
 namespace uadf
 {
-bool parseFromText(const std::string& uadf_text, Model& uadf_model)
+bool parseFromXml(const tinyxml2::XMLDocument* uadf_doc, Model& uadf_model)
 {
   // モデルを初期化
   uadf_model.clear();
 
-  // XMLを読み込む
-  tinyxml2::XMLDocument doc;
-  if (doc.Parse(uadf_text.c_str()) != tinyxml2::XML_SUCCESS) {
-    std::cerr << "Failed to parse XML document." << std::endl;
-    return false;
-  }
+  // 編集用にXMLドキュメントをコピー
+  tinyxml2::XMLDocument uadf_doc_cp;
+  uadf_doc->DeepCopy(&uadf_doc_cp);
 
   // ルート要素を取得
-  const auto robot = doc.RootElement();
+  const auto robot = uadf_doc_cp.RootElement();
   if (strcmp(robot->Name(), "robot") != 0) {
     std::cerr << "The root name of UADF must be \"robot\"." << std::endl;
     return false;
@@ -35,8 +33,9 @@ bool parseFromText(const std::string& uadf_text, Model& uadf_model)
       const auto joint_type = child->Attribute("type");
 
       if (strcmp(joint_type, "thrust") == 0) {
-        Thrust thrust;
+        child->SetAttribute("type", "continuous");
 
+        Thrust thrust;
         bool direction_found = false;
 
         for (auto gchild = child->FirstChildElement(); gchild; gchild = gchild->NextSiblingElement()) {
@@ -62,31 +61,30 @@ bool parseFromText(const std::string& uadf_text, Model& uadf_model)
         }
 
         uadf_model.thrusts[joint_name] = thrust;
-        child->SetAttribute("type", "continuous");
       }
       else if (strcmp(joint_type, "tilt") == 0) {
+        child->SetAttribute("type", "revolute");
+
         TiltJoint tilt;
 
         for (auto gchild = child->FirstChildElement(); gchild; gchild = gchild->NextSiblingElement()) {}
 
         uadf_model.tilts[joint_name] = tilt;
-        child->SetAttribute("type", "revolute");
       }
       else if (strcmp(joint_type, "cs") == 0) {
+        child->SetAttribute("type", "revolute");
+
         ControlSurface cs;
 
         for (auto gchild = child->FirstChildElement(); gchild; gchild = gchild->NextSiblingElement()) {}
 
         uadf_model.control_surfaces[joint_name] = cs;
-        child->SetAttribute("type", "revolute");
       }
     }
   }
 
   // XACROを出力
-  tinyxml2::XMLPrinter printer;
-  doc.Print(&printer);
-  const auto xacro_text = printer.CStr();
+  const auto xacro_text = xml::xmlDocumentToString(&uadf_doc_cp);
 
   // XACROを解析
   std::string urdf_text;
@@ -98,5 +96,26 @@ bool parseFromText(const std::string& uadf_text, Model& uadf_model)
   uadf_model.urdf = urdf::parseURDF(urdf_text);
 
   return true;
+}
+
+bool parseFromText(const std::string& uadf_text, Model& uadf_model)
+{
+  tinyxml2::XMLDocument uadf_doc;
+  if (uadf_doc.Parse(uadf_text.c_str()) != tinyxml2::XML_SUCCESS) {
+    std::cerr << "Failed to parse UADF document." << std::endl;
+    return false;
+  }
+
+  return parseFromXml(&uadf_doc, uadf_model);
+}
+
+bool parseFromPath(const std::string& uadf_path, Model& uadf_model)
+{
+  std::string uadf_text;
+  if (!str::readText(uadf_path, uadf_text)) {
+    return false;
+  }
+
+  return parseFromText(uadf_text, uadf_model);
 }
 }  // namespace uadf
