245b308dcaa5ddd662dd74ab320141648d2d4ca3
Masayoshi Dohi <m.dohi@tobas.jp>
2025-07-13 23:58:31 +0900

    modify: rate_manager.cpp

--- tobas_gazebo/tobas_gazebo_system_plugins/src/rate_manager.cpp
+++ tobas_gazebo/tobas_gazebo_system_plugins/src/rate_manager.cpp
@@ -6,21 +6,27 @@ using namespace std;
 
 namespace gazebo
 {
-RateManager::RateManager(const size_t& update_rate) : update_rate_(update_rate), t_next_(0ns)
+RateManager::RateManager(const size_t& update_rate) : update_rate_(update_rate), next_time_(0ns)
 {
 }
 
-bool RateManager::update(const chrono::steady_clock::duration& time)
+bool RateManager::update(const chrono::steady_clock::duration& cur_time)
 {
   if (update_rate_ == 0) {
     return true;
   }
 
-  if (time < t_next_) {
+  if (cur_time < next_time_) {
     return false;
   }
   else {
-    t_next_ += chrono::nanoseconds(1'000'000'000 / update_rate_);
+    const chrono::nanoseconds period(1'000'000'000 / update_rate_);
+    if (cur_time - next_time_ < period) {
+      next_time_ += period;  // 次回時刻基準で更新することで周波数を守る
+    }
+    else {
+      next_time_ = cur_time + period;  // 2周期以上空いているなら現在時刻基準でリセット
+    }
     return true;
   }
 }
