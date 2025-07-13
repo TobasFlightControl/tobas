8bbd96c4b5e558c31a515968f065d22e087e0bec
Masayoshi Dohi <m.dohi@tobas.jp>
2025-07-14 00:37:56 +0900

    .

--- tobas_gazebo/tobas_gazebo_system_plugins/include/tobas_gazebo_system_plugins/common/constants.hpp
+++ tobas_gazebo/tobas_gazebo_system_plugins/include/tobas_gazebo_system_plugins/common/constants.hpp
@@ -4,7 +4,7 @@
 
 namespace gazebo
 {
-static constexpr std::chrono::steady_clock::duration kCheckTopicWarnStartTime = std::chrono::seconds(3);
+static constexpr auto kCheckTopicWarnStartTime = std::chrono::seconds(3);
 static constexpr double kWarnPeriod = 3.;              // [s]
 static constexpr double kErrorPeriod = 1.;             // [s]
 static constexpr double kRotorSpeedSlowdownSim = 10.;  // [-]
