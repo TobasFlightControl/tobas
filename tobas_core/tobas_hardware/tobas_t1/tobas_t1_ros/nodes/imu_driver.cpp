9a076eebd91eef3677051bda64cdcdc6c5009452
Masayoshi Dohi <m.dohi@tobas.jp>
2025-09-04 14:06:18 +0900

    .

--- tobas_core/tobas_hardware/tobas_t1/tobas_t1_ros/nodes/imu_driver.cpp
+++ tobas_core/tobas_hardware/tobas_t1/tobas_t1_ros/nodes/imu_driver.cpp
@@ -78,6 +78,8 @@ bool ImuDriverNode::initializeImuDriver()
     return false;
   }
 
+  // 加速度がサチるのを防ぐためにスケールを大きめに設定．
+  // 量子化誤差よりもセンサの固有ノイズの方が大きいため，分解能は問題にならない．
   if (!imu_.setAccelFullScale(stm::ISM330DLC::fs_xl_t::FS_XL_8G)) {
     TOBAS_ERROR("Failed to set accelerometer full scale.");
     return false;
