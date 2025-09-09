d71729d06ac3b1471930e77f082e63b8a65a9ac3
Masayoshi Dohi <m.dohi@tobas.jp>
2025-09-09 12:56:27 +0900

    modify: dummy_nodeの名前をランダムに

--- tobas_core/tobas_dummy_pkg/nodes/dummy_node.cpp
+++ tobas_core/tobas_dummy_pkg/nodes/dummy_node.cpp
@@ -1,5 +1,27 @@
+#include <random>
+#include <string_view>
+
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
+
+namespace tobas
+{
+namespace
+{
+std::string randomAlNum(size_t n)
+{
+  static constexpr std::string_view alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+  static thread_local std::mt19937 rng(std::random_device{}());
+  std::uniform_int_distribution<size_t> dist(0, alphabet.size() - 1);
+
+  std::string s;
+  s.reserve(n);
+  for (size_t i = 0; i < n; ++i) {
+    s.push_back(alphabet[dist(rng)]);
+  }
+  return s;
+}
+}  // namespace
 
 class DummyNode : public rclcpp::Node
 {
@@ -7,9 +29,9 @@ public:
   explicit DummyNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 };
 
-DummyNode::DummyNode(const rclcpp::NodeOptions& options) : rclcpp::Node("dummy_node", options)
+DummyNode::DummyNode(const rclcpp::NodeOptions& options) : rclcpp::Node("dummy_node_" + randomAlNum(10), options)
 {
-  RCLCPP_WARN_STREAM(get_logger(), "Dummy \"" << get_name() << "\" has started.");
 }
+}  // namespace tobas
 
-RCLCPP_COMPONENTS_REGISTER_NODE(DummyNode)
+RCLCPP_COMPONENTS_REGISTER_NODE(tobas::DummyNode)
