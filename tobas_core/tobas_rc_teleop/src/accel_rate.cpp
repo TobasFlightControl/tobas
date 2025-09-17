4f333b0486876be468ad174cbda1048828e94ae0
Masayoshi Dohi <m.dohi@tobas.jp>
2025-09-17 17:54:58 +0900

    change: パラメータ変更

--- tobas_core/tobas_rc_teleop/src/accel_rate.cpp
+++ tobas_core/tobas_rc_teleop/src/accel_rate.cpp
@@ -31,9 +31,9 @@ bool AccelRateController::requireAngularVelocity()
 void AccelRateController::initialize(tobas::BaseNode* node, tobas::FlightMode mode)
 {
   node->addDynamicDoubleParam(
-    addMode("max_horizontal_accel", mode), &self::maxHorizontalAccelCb, this, 0.5, 10, 1, 20, " m/s^2");
+    addMode("max_horizontal_accel", mode), &self::maxHorizontalAccelCb, this, 0.5, 18, 4, 30, " m/s^2");
   node->addDynamicDoubleParam(
-    addMode("max_vertical_accel", mode), &self::maxVerticalAccelCb, this, 0.5, 8, 1, 20, " m/s^2");
+    addMode("max_vertical_accel", mode), &self::maxVerticalAccelCb, this, 0.5, 18, 4, 30, " m/s^2");
   node->addDynamicIntParam(addMode("max_attitude_rate", mode), &self::maxAttitudeRateCb, this, 180, 0, 360, " dps");
   node->addDynamicIntParam(addMode("max_heading_rate", mode), &self::maxHeadingRateCb, this, 90, 0, 360, " dps");
   node->addDynamicIntParam(
