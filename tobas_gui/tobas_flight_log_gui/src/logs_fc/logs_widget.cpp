3a59a3e9b48f86157b4e8748438943c9e018959a
Masayoshi Dohi <m.dohi@tobas.jp>
2025-09-28 17:05:25 +0900

    modify: ログビューアのレイアウトを変更

--- tobas_gui/tobas_flight_log_gui/src/logs_fc/logs_widget.cpp
+++ tobas_gui/tobas_flight_log_gui/src/logs_fc/logs_widget.cpp
@@ -6,7 +6,6 @@
 #include <tobas_constants/constants.hpp>
 #include <tobas_qt_tools/cast.hpp>
 #include <tobas_qt_tools/message.hpp>
-#include <tobas_qt_tools/util.hpp>
 #include <tobas_qt_tools/widgets/label.hpp>
 #include <tobas_ros2_tools/util.hpp>
 #include <tobas_std_tools/check.hpp>
@@ -41,12 +40,12 @@ FlightLogsWidgetFC::FlightLogsWidgetFC(rclcpp::Node::SharedPtr node)
 
   // Layout
   const auto cols = new QHBoxLayout();
+  cols->addWidget(new qt::Label("Flight Controller", kPSize1, QFont::Bold));
+  cols->addStretch();
   cols->addWidget(read_button_);
   cols->addWidget(clean_button_);
-  cols->addStretch();
 
   const auto rows = new QVBoxLayout();
-  qt::addWidgetCenter(new qt::Label("Flight Controller", kPSize1, QFont::Bold), rows);
   rows->addLayout(cols);
   rows->addWidget(log_list_);
 
--- tobas_gui/tobas_flight_log_gui/src/logs_gcs/logs_widget.cpp
+++ tobas_gui/tobas_flight_log_gui/src/logs_gcs/logs_widget.cpp
@@ -6,7 +6,6 @@
 #include <tobas_constants/constants.hpp>
 #include <tobas_qt_tools/cast.hpp>
 #include <tobas_qt_tools/message.hpp>
-#include <tobas_qt_tools/util.hpp>
 #include <tobas_qt_tools/widgets/label.hpp>
 #include <tobas_ros2_tools/util.hpp>
 #include <tobas_std_tools/check.hpp>
@@ -36,12 +35,12 @@ FlightLogsWidgetGCS::FlightLogsWidgetGCS()
 
   // Layout
   const auto cols = new QHBoxLayout();
+  cols->addWidget(new qt::Label("Ground Control Station", kPSize1, QFont::Bold));
+  cols->addStretch();
   cols->addWidget(read_button_);
   cols->addWidget(clean_button_);
-  cols->addStretch();
 
   const auto rows = new QVBoxLayout();
-  qt::addWidgetCenter(new qt::Label("Ground Control Station", kPSize1, QFont::Bold), rows);
   rows->addLayout(cols);
   rows->addWidget(log_list_);
 
