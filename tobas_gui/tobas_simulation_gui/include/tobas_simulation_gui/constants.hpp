34ad2928f35606b10804d748068de6fdbadd5a35
Masayoshi Dohi <masa0u0masa1215@gmail.com>
2025-10-04 01:10:54 +0900

    gazebo起動時のサービスの待ち時間を最大に

--- tobas_gui/tobas_simulation_gui/include/tobas_simulation_gui/constants.hpp
+++ tobas_gui/tobas_simulation_gui/include/tobas_simulation_gui/constants.hpp
@@ -8,7 +8,7 @@ namespace sim
 {
 static constexpr char kPackageName[] = "tobas_simulation_gui";
 
-static constexpr auto kWaitForService = std::chrono::seconds(10);
+static constexpr auto kWaitForService = std::chrono::seconds::max();  // ワールドのダウンロードに時間がかかる
 static constexpr auto kServiceCallTimeout = std::chrono::seconds(1);
 }  // namespace sim
 }  // namespace gui
