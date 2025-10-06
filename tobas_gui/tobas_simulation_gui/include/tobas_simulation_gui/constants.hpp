d11cc18d597a812fac4fb3f8c04e716d9c4815ec
Masayoshi Dohi <m.dohi@tobas.jp>
2025-10-06 10:11:54 +0900

    fix: kWaitForServiceを5分に

--- tobas_gui/tobas_simulation_gui/include/tobas_simulation_gui/constants.hpp
+++ tobas_gui/tobas_simulation_gui/include/tobas_simulation_gui/constants.hpp
@@ -8,7 +8,7 @@ namespace sim
 {
 static constexpr char kPackageName[] = "tobas_simulation_gui";
 
-static constexpr auto kWaitForService = std::chrono::seconds::max();  // ワールドのダウンロードに時間がかかる
+static constexpr auto kWaitForService = std::chrono::minutes(5);  // ワールドのダウンロードに時間がかかる
 static constexpr auto kServiceCallTimeout = std::chrono::seconds(1);
 }  // namespace sim
 }  // namespace gui
