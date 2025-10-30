0db9873abb6fc4504f86e7a9f1c1a051ea2ffe70
Masayoshi Dohi <m.dohi@tobas.jp>
2025-10-30 23:25:33 +0900

    加速度キャリブレーションの閾値を緩めた

--- tobas_gui/tobas_sensor_calibration/include/tobas_sensor_calibration/accel_calibration/thread.hpp
+++ tobas_gui/tobas_sensor_calibration/include/tobas_sensor_calibration/accel_calibration/thread.hpp
@@ -17,8 +17,8 @@ class AccelCalibrationThread : public QThread
   using super = QThread;
 
   static constexpr size_t kDataCount = 200;
-  static constexpr double kCollectDataTimeout = 10.;   // [s]
-  static constexpr double kAccelBiasNormThresh = 0.3;  // [m/s^2]
+  static constexpr double kCollectDataTimeout = 10.;  // [s]
+  static constexpr double kAccelBiasNormThresh = 1.;  // [m/s^2] ISM330DLCで最大 85mg (= 0.83m/s^2) のオフセット
 
 Q_SIGNALS:
   void finished(bool success, const QString& message);
