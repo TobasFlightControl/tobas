fc46a2497442dbc8724b6d247338b8f72ccc1c58
Masayoshi Dohi <m.dohi@tobas.jp>
2025-11-06 12:05:00 +0900

    .

--- tobas_core/tobas_common/tobas_std_tools/src/gnss.cpp
+++ tobas_core/tobas_common/tobas_std_tools/src/gnss.cpp
@@ -7,15 +7,15 @@
 
 #include "tobas_std_tools/unit_conversions.hpp"
 
-using namespace std;
+namespace ch = std::chrono;
 
 namespace tobas_std
 {
 long computeGPSDelayFromToW(uint32_t gps_tow_ms)
 {
   // 現在のUTC時刻を取得
-  const auto now = chrono::system_clock::now();
-  const auto now_c = chrono::system_clock::to_time_t(now);
+  const auto now = ch::system_clock::now();
+  const auto now_c = ch::system_clock::to_time_t(now);
   const auto utc_time = gmtime(&now_c);
 
   // その週の日曜日0時0分0秒を計算
@@ -23,11 +23,11 @@ long computeGPSDelayFromToW(uint32_t gps_tow_ms)
   utc_time->tm_min = 0;
   utc_time->tm_hour = 0;
   utc_time->tm_mday -= utc_time->tm_wday;  // 現在の曜日から日曜日に戻る
-  const auto start_of_week = chrono::system_clock::from_time_t(mktime(utc_time));
+  const auto start_of_week = ch::system_clock::from_time_t(mktime(utc_time));
 
   // 週のはじめから現在までの経過時間を計算
   const auto duration = now - start_of_week;
-  const auto cur_tow_ms = chrono::duration_cast<chrono::milliseconds>(duration).count();
+  const auto cur_tow_ms = ch::duration_cast<ch::milliseconds>(duration).count();
 
   // GPS TOWと現在のTOWの差を計算
   const auto diff_ms = cur_tow_ms - gps_tow_ms;
