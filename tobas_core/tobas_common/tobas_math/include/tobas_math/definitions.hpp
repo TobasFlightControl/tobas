3fb9f3683bc88952ec89a5b1f8780772c475a4f7
Masayoshi Dohi <m.dohi@tobas.jp>
2025-11-16 22:13:09 +0900

    .

--- tobas_core/tobas_common/tobas_math/include/tobas_math/definitions.hpp
+++ tobas_core/tobas_common/tobas_math/include/tobas_math/definitions.hpp
@@ -4,3 +4,9 @@
 
 #define M_PI_3 (M_PI / 3)
 #define M_2PI (2 * M_PI)
+
+namespace math
+{
+static constexpr double kDeg2Rad = M_PI / 180;  // degree -> radian
+static constexpr double kRpm2Rps = M_PI / 30;   // rpm -> rad/s
+}  // namespace math
