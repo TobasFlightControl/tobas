4df99d497f3f19927a663b01b78de858cd8c3be1
Masayoshi Dohi <m.dohi@tobas.jp>
2025-11-20 12:13:10 +0900

    change: チルト機もデフォルトでI制御を入れる

--- tobas_core/tobas_controllers/tobas_random_axis_tilt_multi_controller/nodes/controller_node.cpp
+++ tobas_core/tobas_controllers/tobas_random_axis_tilt_multi_controller/nodes/controller_node.cpp
@@ -145,6 +145,12 @@ ControllerNode::ControllerNode(const rclcpp::NodeOptions& options)
   do_dist_comp_trans_ = getBoolParam("do_disturbance_compensation_translation");
   do_dist_comp_rot_ = getBoolParam("do_disturbance_compensation_rotation");
 
+  // Iゲインは1~2秒で補正が感じられるくらいに設定するのが良いらしい (GPT o1)
+  const long default_horizontal_i_gain = do_dist_comp_trans_ ? 0 : 10;
+  const long default_vertical_i_gain = do_dist_comp_trans_ ? 0 : 10;
+  const long default_attitude_i_gain = do_dist_comp_rot_ ? 0 : 10;
+  const long default_heading_i_gain = do_dist_comp_rot_ ? 0 : 10;
+
   // Register dynamic parameters
   // I制御は姿勢変化が大きいと逆効果なため，デフォルトではオフにする．
   addDynamicDoubleParam(
@@ -156,10 +162,10 @@ ControllerNode::ControllerNode(const rclcpp::NodeOptions& options)
   addDynamicDoubleParam("vertical_damping_ratio", &self::verticalDampingRatioCb, this, 0.1, 10, 1, 30);
   addDynamicDoubleParam("attitude_damping_ratio", &self::attitudeDampingRatioCb, this, 0.1, 10, 1, 30);
   addDynamicDoubleParam("heading_damping_ratio", &self::headingDampingRatioCb, this, 0.1, 10, 1, 30);
-  addDynamicDoubleParam("horizontal_i_gain", &self::horizontalIGainCb, this, 0.01, 0, 0, 30);
-  addDynamicDoubleParam("vertical_i_gain", &self::verticalIGainCb, this, 0.01, 0, 0, 30);
-  addDynamicDoubleParam("attitude_i_gain", &self::attitudeIGainCb, this, 0.1, 0, 0, 30);
-  addDynamicDoubleParam("heading_i_gain", &self::headingIGainCb, this, 0.01, 0, 0, 30);
+  addDynamicDoubleParam("horizontal_i_gain", &self::horizontalIGainCb, this, 0.01, default_horizontal_i_gain, 0, 30);
+  addDynamicDoubleParam("vertical_i_gain", &self::verticalIGainCb, this, 0.01, default_vertical_i_gain, 0, 30);
+  addDynamicDoubleParam("attitude_i_gain", &self::attitudeIGainCb, this, 0.1, default_attitude_i_gain, 0, 30);
+  addDynamicDoubleParam("heading_i_gain", &self::headingIGainCb, this, 0.01, default_heading_i_gain, 0, 30);
   addDynamicDoubleParam("max_horizontal_accel", &self::maxHorizontalAccelCb, this, 0.5, 16, 2, 40, " m/s^2");
   addDynamicDoubleParam("max_vertical_accel", &self::maxVerticalAccelCb, this, 0.5, 8, 2, 20, " m/s^2");
   addDynamicIntParam(
