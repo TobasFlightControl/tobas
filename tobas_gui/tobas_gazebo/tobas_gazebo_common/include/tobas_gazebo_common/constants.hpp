9ab4f26f2c4327dbc72ee68ffedf0cf2cecb9a15
Masayoshi Dohi <m.dohi@tobas.jp>
2026-01-11 20:28:14 +0900

    wip: plugin

--- tobas_gui/tobas_gazebo/tobas_gazebo_common/include/tobas_gazebo_common/constants.hpp
+++ tobas_gui/tobas_gazebo/tobas_gazebo_common/include/tobas_gazebo_common/constants.hpp
@@ -26,5 +26,7 @@ static constexpr char kGetWindParamsSrv[] = "gazebo/get_wind_parameters";
 static constexpr char kSetWindParamsSrv[] = "gazebo/set_wind_parameters";
 static constexpr char kGetTetherParamsSrv[] = "gazebo/get_tether_parameters";
 static constexpr char kSetTetherParamsSrv[] = "gazebo/set_tether_parameters";
+static constexpr char kAttachSuspenedLoadSrv[] = "gazebo/attach_suspended_load";
+static constexpr char kDetachSuspenedLoadSrv[] = "gazebo/detach_suspended_load";
 static constexpr char kBreakRotorSrvNS[] = "gazebo/break_rotor";
 }  // namespace gazebo
--- tobas_gui/tobas_gazebo/tobas_gazebo_system_plugins/plugins/suspended_load_plugin.cpp
+++ tobas_gui/tobas_gazebo/tobas_gazebo_system_plugins/plugins/suspended_load_plugin.cpp
@@ -8,7 +8,6 @@
 #include <tobas_gazebo_conversions/gazebo_msg.hpp>
 #include <tobas_gazebo_tools/model_mass_holder.hpp>
 #include <tobas_gazebo_tools/utils.hpp>
-#include <tobas_std_tools/check.hpp>
 
 #include <tobas_gazebo_msgs/srv/attach_suspended_load.hpp>
 #include <tobas_gazebo_msgs/srv/detach_suspended_load.hpp>
@@ -30,6 +29,7 @@ class GazeboSuspendedLoadPlugin : public BaseNode,
 {
   // Constants
   static constexpr char kPluginName[] = "gazebo_suspended_load_plugin";
+  static constexpr char kLoadNamePrefix[] = "load_";
   static constexpr int kUpdateMarkerRate = 60;  // [Hz]
 
   // Default parameters
@@ -45,7 +45,7 @@ public:
   explicit GazeboSuspendedLoadPlugin();
 
   void Configure(
-    const gz::sim::Entity& model_entity,
+    const gz::sim::Entity& model,
     const sdf::ElementConstPtr& sdf,
     gz::sim::EntityComponentManager& ecm,
     gz::sim::EventManager&) override;
@@ -60,17 +60,24 @@ private:
   double csa_;    // [mm^2] 断面積 (Cross-Sectional Area)
   uint64_t line_id_;
 
-  // Load parameters
-  bool load_exist_ = false;
+  // Aircraft
+  std::shared_ptr<gz::sim::Link> base_link_;
+  const cmp::WorldPose* W_Pose_B_ = nullptr;
+  const cmp::WorldLinearVelocity* W_Vel_WB_ = nullptr;
+  const cmp::WorldAngularVelocity* W_Gyro_WB_ = nullptr;
+
+  // Load
+  gz::math::Vector3d L_Pos_LQ_;
+  double load_mass_;
   double cable_length_;
+  bool load_exist_ = false;
+  int load_index_ = 0;
+  std::shared_ptr<gz::sim::Link> load_link_;
+  const cmp::WorldPose* W_Pose_L_ = nullptr;
+  const cmp::WorldLinearVelocity* W_Vel_WL_ = nullptr;
+  const cmp::WorldAngularVelocity* W_Gyro_WL_ = nullptr;
 
   std::string world_name_;
-  std::shared_ptr<gz::sim::Link> link_;
-
-  const cmp::WorldPose* pose_W_;
-  const cmp::WorldLinearVelocity* linvel_W_;
-  const cmp::WorldAngularVelocity* angvel_W_;
-
   ModelMassHolder mass_holder_;
   RateManager rate_manager_;
 
@@ -83,6 +90,7 @@ private:
   gz::msgs::Vector3d* line_p1_;
 
   void getSdfParams(const sdf::ElementConstPtr& sdf);
+  std::string loadName() const;
 
   void attachLoadCb(const AttachSrv::Request::ConstSharedPtr& req, const AttachSrv::Response::SharedPtr& res);
   void detachLoadCb(const DetachSrv::Request::ConstSharedPtr& req, const DetachSrv::Response::SharedPtr& res);
@@ -108,23 +116,28 @@ void GazeboSuspendedLoadPlugin::Configure(
   world_name_ = world_name.value();
 
   const auto link_entity = ecm.EntityByComponents(cmp::Link(), cmp::ParentEntity(model_entity), cmp::Name(link_name_));
-  link_ = std::make_shared<gz::sim::Link>(link_entity);
-  if (!link_->Valid(ecm)) {
+  base_link_ = std::make_shared<gz::sim::Link>(link_entity);
+  if (!base_link_->Valid(ecm)) {
     TOBAS_EXIT("Failed to find the specified link \"", link_name_, "\".");
   }
 
-  TOBAS_CHECK(pose_W_ = getComponent<cmp::WorldPose>(link_entity, ecm));
-  TOBAS_CHECK(linvel_W_ = getComponent<cmp::WorldLinearVelocity>(link_entity, ecm));
-  TOBAS_CHECK(angvel_W_ = getComponent<cmp::WorldAngularVelocity>(link_entity, ecm));
+  if (!(W_Pose_B_ = getComponent<cmp::WorldPose>(link_entity, ecm))) {
+    TOBAS_EXIT("Failed to get the world pose of \"", link_name_, "\".");
+  }
+  if (!(W_Vel_WB_ = getComponent<cmp::WorldLinearVelocity>(link_entity, ecm))) {
+    TOBAS_EXIT("Failed to get the world linear velocity of \"", link_name_, "\".");
+  }
+  if (!(W_Gyro_WB_ = getComponent<cmp::WorldAngularVelocity>(link_entity, ecm))) {
+    TOBAS_EXIT("Failed to get the world angular velocity of \"", link_name_, "\".");
+  }
 
   if (!mass_holder_.initialize(model_entity, ecm)) {
     TOBAS_EXIT("Failed to initialize model mass holder.");
   }
 
-  attach_load_ss_ = createService<AttachSrv>(kGetTetherParamsSrv, &self::attachLoadCb, this);
-  detach_load_ss_ = createService<DetachSrv>(kSetTetherParamsSrv, &self::detachLoadCb, this);
+  attach_load_ss_ = createService<AttachSrv>(kAttachSuspenedLoadSrv, &self::attachLoadCb, this);
+  detach_load_ss_ = createService<DetachSrv>(kDetachSuspenedLoadSrv, &self::detachLoadCb, this);
 
-  marker_.set_action(gz::msgs::Marker::ADD_MODIFY);
   marker_.set_ns(kPluginName);
   marker_.set_id(line_id_);
   marker_.set_type(gz::msgs::Marker::LINE_LIST);
@@ -138,19 +151,63 @@ void GazeboSuspendedLoadPlugin::PreUpdate(const gz::sim::UpdateInfo& info, gz::s
     return;
   }
 
+  // 荷重の状態にアクセスできるようにする
+  if (!load_link_) {
+    const auto model_entity = ecm.EntityByComponents(cmp::Model(), cmp::Name(loadName()));
+    if (model_entity == gz::sim::kNullEntity) {
+      return;
+    }
+    TOBAS_INFO("Load entity is found: ", model_entity);
+
+    const gz::sim::Model load_model(model_entity);
+    if (!load_model.Valid(ecm)) {
+      TOBAS_ERROR("Failed to find the load model.");
+      load_exist_ = false;
+      return;
+    }
+
+    const auto link_entity = load_model.CanonicalLink(ecm);
+    load_link_ = std::make_shared<gz::sim::Link>(link_entity);
+    if (!load_link_->Valid(ecm)) {
+      TOBAS_EXIT("Failed to find the canonical link of the load.");
+    }
+
+    if (!(W_Pose_L_ = getComponent<cmp::WorldPose>(link_entity, ecm))) {
+      TOBAS_ERROR("Failed to get the world pose of the load.");
+      load_exist_ = false;
+      return;
+    }
+    if (!(W_Vel_WL_ = getComponent<cmp::WorldLinearVelocity>(link_entity, ecm))) {
+      TOBAS_ERROR("Failed to get the world linear velocity of the load.");
+      load_exist_ = false;
+      return;
+    }
+    if (!(W_Gyro_WL_ = getComponent<cmp::WorldAngularVelocity>(link_entity, ecm))) {
+      TOBAS_ERROR("Failed to get the world angular velocity of the load.");
+      load_exist_ = false;
+      return;
+    }
+  }
+
   // 機体の状態を取得
-  const auto& T_W_B = pose_W_->Data();
-  const auto& W_Pos_WB = T_W_B.Pos();
-  const auto& R_W_B = T_W_B.Rot();
-  const auto& linvel_W = linvel_W_->Data();
-  const auto& angvel_W = angvel_W_->Data();
+  const auto& W_Pose_B = W_Pose_B_->Data();
+  const auto& W_Pos_WB = W_Pose_B.Pos();
+  const auto& W_Rot_B = W_Pose_B.Rot();
+  const auto& W_Vel_WB = W_Vel_WB_->Data();
+  const auto& W_Gyro_WB = W_Gyro_WB_->Data();
 
   // 荷重の状態を取得
-  gz::math::Vector3d W_Pos_WQ;  // TODO
+  const auto& W_Pose_L = W_Pose_L_->Data();
+  const auto& W_Pos_WL = W_Pose_L.Pos();
+  const auto& W_Rot_L = W_Pose_L.Rot();
+  const auto& W_Vel_WL = W_Vel_WL_->Data();
+  const auto& W_Gyro_WL = W_Gyro_WL_->Data();
 
   // ケーブルの端点間ベクトルを計算
-  const auto W_Pos_BP = R_W_B.RotateVector(B_Pos_BP_);
+  const auto W_Pos_BP = W_Rot_B.RotateVector(B_Pos_BP_);
   const auto W_Pos_WP = W_Pos_WB + W_Pos_BP;
+  const auto W_Pos_LQ = W_Rot_L.RotateVector(L_Pos_LQ_);
+  const auto W_Pos_WQ = W_Pos_WL + W_Pos_LQ;
   const auto W_Pos_PQ = W_Pos_WQ - W_Pos_WP;
   const auto length = W_Pos_PQ.Length();  // [m]
 
@@ -160,11 +217,15 @@ void GazeboSuspendedLoadPlugin::PreUpdate(const gz::sim::UpdateInfo& info, gz::s
     const auto x = length - cable_length_;  // [m]
 
     // ケーブル長の変化率を計算
-    const auto W_Vel_PQ = linvel_W + angvel_W.Cross(W_Pos_BP);  // [m]
-    const auto xd = W_Vel_PQ.Dot(W_Pos_PQ) / length;            // [m/s]
+    const auto W_Vel_WP = W_Vel_WB + W_Gyro_WB.Cross(W_Pos_BP);
+    const auto W_Vel_WQ = W_Vel_WL + W_Gyro_WL.Cross(W_Pos_LQ);
+    const auto W_Vel_PQ = W_Vel_WQ - W_Vel_WP;
+    const auto xd = W_Vel_PQ.Dot(W_Pos_PQ) / length;  // 相対速度をケーブル方向に射影したものがケーブル長の変化率
 
     // マスバネダンパ系の係数
-    const auto m = mass_holder_.getMass();         // [kg]
+    const auto& m1 = mass_holder_.getMass();
+    const auto& m2 = load_mass_;
+    const auto m = m1 * m2 / (m1 + m2);            // [kg] 相対運動の等価質量 (memo: 3-47)
     const auto k = young_ * csa_ * cable_length_;  // [N/m]
     const auto d = 2 * sqrt(m * k);                // [Ns/m] 臨海減衰する粘性係数
 
@@ -172,16 +233,19 @@ void GazeboSuspendedLoadPlugin::PreUpdate(const gz::sim::UpdateInfo& info, gz::s
     const auto T = k * x + d * xd;  // [N]
 
     // ケーブルの方向に張力を加える
-    const auto axis_W = -W_Pos_PQ.Normalized();
-    const auto force_W = T * axis_W;
-    link_->AddWorldForce(ecm, force_W, B_Pos_BP_);
+    const auto W_Force_PQ = T * W_Pos_PQ.Normalized();
+    base_link_->AddWorldForce(ecm, W_Force_PQ, B_Pos_BP_);
+    load_link_->AddWorldForce(ecm, -W_Force_PQ, L_Pos_LQ_);
   }
 
   // 描画用のラインマーカを更新
   if (rate_manager_.update(info.simTime)) {
     vector3dGzToMsg(W_Pos_WP, *line_p0_);
     vector3dGzToMsg(W_Pos_WQ, *line_p1_);
-    node_.Request(kGzMarkerSrv, marker_);
+    marker_.set_action(gz::msgs::Marker::ADD_MODIFY);
+    if (!node_.Request(kGzMarkerSrv, marker_)) {
+      TOBAS_ERROR("Failed to update the line marker.");
+    }
   }
 }
 
@@ -192,6 +256,11 @@ void GazeboSuspendedLoadPlugin::getSdfParams(const sdf::ElementConstPtr& sdf)
   getSdfParam(sdf, "youngModulus", young_, kDefaultYoungModulus, kPositive);
   getSdfParam(sdf, "crossSectionArea", csa_, kDefaultCrossSectionArea, kPositive);
   getSdfParam(sdf, "lineId", line_id_, kDefaultLineId, kPositive);  // 0だとIDがランダムに割り当てられて無限に増える
+}
+
+std::string GazeboSuspendedLoadPlugin::loadName() const
+{
+  return kLoadNamePrefix + std::to_string(load_index_);
 }
 
 void GazeboSuspendedLoadPlugin::attachLoadCb(
@@ -214,8 +283,11 @@ void GazeboSuspendedLoadPlugin::attachLoadCb(
     return;
   }
 
+  ++load_index_;
+
+  // TODO: 出現位置
   gz::msgs::EntityFactory gzreq;
-  gzreq.set_sdf(makeBoxSdf("load", req->load_size, req->load_size, req->load_size, req->load_mass, 0., 0., 0.));
+  gzreq.set_sdf(makeBoxSdf(loadName(), req->load_size, req->load_size, req->load_size, req->load_mass, 0., 0., 0.));
   gzreq.set_allow_renaming(true);
 
   gz::msgs::Boolean gzrep;
@@ -238,7 +310,10 @@ void GazeboSuspendedLoadPlugin::attachLoadCb(
     return;
   }
 
+  L_Pos_LQ_.Set(0., 0., req->load_size / 2.);  // 直方体の上面の中央にケーブルを取り付ける想定
+  load_mass_ = req->load_mass;
   cable_length_ = req->cable_length;
+
   load_exist_ = true;
 
   res->success = true;
@@ -249,7 +324,18 @@ void GazeboSuspendedLoadPlugin::detachLoadCb(
   const DetachSrv::Request::ConstSharedPtr&,
   const DetachSrv::Response::SharedPtr& res)
 {
+  marker_.set_action(gz::msgs::Marker::DELETE_MARKER);
+  if (!node_.Request(kGzMarkerSrv, marker_)) {
+    res->success = false;
+    res->message = "Failed to delete the line marker.";
+    return;
+  }
+
   load_exist_ = false;
+  load_link_.reset();
+  W_Pose_L_ = nullptr;
+  W_Vel_WL_ = nullptr;
+  W_Gyro_WL_ = nullptr;
 
   res->success = true;
   res->message.clear();
