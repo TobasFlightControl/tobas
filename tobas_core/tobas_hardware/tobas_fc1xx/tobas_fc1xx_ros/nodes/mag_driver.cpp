c20a72cf7eacb964bb8e99f57332f14ecc723442
Masayoshi Dohi <m.dohi@tobas.jp>
2026-01-31 11:41:02 +0900

    コンパスの更新レートを変更: 50Hz -> 100Hz

--- tobas_core/tobas_hardware/tobas_fc1xx/tobas_fc1xx_ros/nodes/mag_driver.cpp
+++ tobas_core/tobas_hardware/tobas_fc1xx/tobas_fc1xx_ros/nodes/mag_driver.cpp
@@ -10,7 +10,7 @@ using namespace std::chrono_literals;
 
 class MagDriverNode : public hardware::BaseSensorNode
 {
-  static constexpr auto kSamplingPeriod = 20ms;
+  static constexpr auto kSamplingPeriod = 10ms;
 
   using self = MagDriverNode;
   using super = hardware::BaseSensorNode;
