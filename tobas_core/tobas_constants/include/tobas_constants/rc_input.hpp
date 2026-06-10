4ca5486d96249d3b78a9befc4627243c165881b5
Masayoshi Dohi <m.dohi@tobas.jp>
2026-06-10 14:58:37 +0900

    .

--- tobas_core/tobas_constants/include/tobas_constants/rc_input.hpp
+++ tobas_core/tobas_constants/include/tobas_constants/rc_input.hpp
@@ -22,6 +22,7 @@ static constexpr size_t kMaxSbusChannels = 16;
 static constexpr size_t kMaxNumOfGpsw = kMaxSbusChannels - kMinSbusChannels;
 
 static constexpr double kRcInputMin = -1.;
-static constexpr double kRCInputMid = 0.;
 static constexpr double kRcInputMax = 1.;
+static constexpr double kRCInputMid = (kRcInputMax + kRcInputMin) / 2;
+static constexpr double kRcInputRange = kRcInputMax - kRcInputMin;
 }  // namespace tobas
