d641cfaef199412494b7b4f3303c3f43f2432665
Masayoshi Dohi <m.dohi@tobas.jp>
2026-06-18 00:22:11 +0900

    離陸前にフェイルセーフが発動したらディスアーム

--- tobas_core/tobas_failsafe/nodes/failsafe_executor.cpp
+++ tobas_core/tobas_failsafe/nodes/failsafe_executor.cpp
@@ -10,6 +10,7 @@
 
 #include <tobas_mission_msgs/action/execute_mission.hpp>
 #include <tobas_msgs/msg/arming.hpp>
+#include <tobas_msgs/msg/landed_state.hpp>
 #include <tobas_msgs/msg/vehicle_health.hpp>
 #include <tobas_msgs/srv/set_arm.hpp>
 #include <tobas_msgs_adapter/rc_input.hpp>
@@ -38,10 +39,12 @@ private:
 
   tobas_msgs::msg::Arming::ConstSharedPtr arming_;
   tobas_msgs::RCInput::ConstSharedPtr rcin_;
+  tobas_msgs::msg::LandedState::ConstSharedPtr landed_;
 
   ros2::SubscriberPtr<tobas_msgs::msg::VehicleHealth> health_sub_;
   ros2::SubscriberPtr<tobas_msgs::msg::Arming> arming_sub_;
   ros2::SubscriberPtr<tobas_msgs::RCInput> rcin_sub_;
+  ros2::SubscriberPtr<tobas_msgs::msg::LandedState> landed_sub_;
 
   ros2::ServiceClientPtr<tobas_msgs::srv::SetArm> set_arm_sc_;
   ros2::ActionClientPtr<Action> mission_ac_;
@@ -54,6 +57,7 @@ private:
   void vehicleHealthCb(const tobas_msgs::msg::VehicleHealth::ConstSharedPtr& health);
   void armingCb(const tobas_msgs::msg::Arming::ConstSharedPtr& arming);
   void rcInputCb(const tobas_msgs::RCInput::ConstSharedPtr& rcin);
+  void landedCb(const tobas_msgs::msg::LandedState::ConstSharedPtr& landed);
 };
 
 FailsafeExecutorNode::FailsafeExecutorNode(const rclcpp::NodeOptions& options)
@@ -62,6 +66,7 @@ FailsafeExecutorNode::FailsafeExecutorNode(const rclcpp::NodeOptions& options)
   health_sub_ = createSubscriber(topic::kVehicleHealth, &self::vehicleHealthCb, this);
   arming_sub_ = createSubscriber(topic::kArming, &self::armingCb, this);
   rcin_sub_ = createSubscriber(topic::kRcInput, &self::rcInputCb, this);
+  landed_sub_ = createSubscriber(topic::kLanded, &self::landedCb, this);
 
   set_arm_sc_ = create_client<tobas_msgs::srv::SetArm>(service::kSetArm);
   mission_ac_ = rclcpp_action::create_client<Action>(this, action::kExecuteMission);
@@ -178,69 +183,67 @@ void FailsafeExecutorNode::vehicleHealthCb(const tobas_msgs::msg::VehicleHealth:
 {
   using VH = tobas_msgs::msg::VehicleHealth;
 
-  const auto voltage_too_low = (health->battery_voltage == VH::FAILED);
-  const auto radio_link_lost = (health->radio_link == VH::FAILED);
-  const auto posvel_accurate = (health->position_accuracy == VH::PASSED) && (health->velocity_accuracy == VH::PASSED);
-
   // アームしていなければフェイルセーフは発動しない
   if (!arming_ || !arming_->data) {
     return;
   }
 
-  const auto is_manual_ctrl_enabled = (!radio_link_lost && rcin_ && rcin_->ok && rcin_->enable);
+  const auto voltage_too_low = (health->battery_voltage == VH::FAILED);
+  const auto radio_link_lost = (health->radio_link == VH::FAILED);
+  const auto posvel_accurate = (health->position_accuracy == VH::PASSED) && (health->velocity_accuracy == VH::PASSED);
+
+  const auto landed = (landed_ && landed_->landed);
+  const auto manual_ctrl_enabled = (!radio_link_lost && rcin_ && rcin_->ok && rcin_->enable);
+  const auto failsafe_mission_ready = (!manual_ctrl_enabled && posvel_accurate);
 
   switch (state_) {
     case kNoFailSafe: {
-      // 手動操縦中はフェイルセーフを発動しない
-      if (is_manual_ctrl_enabled) {
-        break;
-      }
-
-      // 位置と速度の推定ができてないならフェイルセーフを発動しない
-      if (!posvel_accurate) {
-        break;
-      }
-
       // フェイルセーフを更新
       if (voltage_too_low) {
-        TOBAS_WARN("Battery fail-safe is activated.");
-        startLand();
-        break;
+        if (landed) {
+          TOBAS_WARN("Battery fail-safe is activated.");
+          disarm();
+        }
+        else if (failsafe_mission_ready) {
+          TOBAS_WARN("Battery fail-safe is activated.");
+          startLand();
+        }
       }
-      if (radio_link_lost) {
-        TOBAS_WARN("Radio fail-safe is activated.");
-        startRTL();
-        break;
+      else if (radio_link_lost) {
+        if (landed) {
+          TOBAS_WARN("Radio fail-safe is activated.");
+          disarm();
+        }
+        else if (failsafe_mission_ready) {
+          TOBAS_WARN("Radio fail-safe is activated.");
+          startRTL();
+        }
       }
 
       break;
     }
     case kReturnToLaunch: {
       // 手動操縦が有効になったらフェイルセーフをキャンセル
-      if (is_manual_ctrl_enabled) {
+      if (manual_ctrl_enabled) {
         TOBAS_INFO("Fail-safe was canceled because the manual control was enabled.");
         mission_ac_->async_cancel_all_goals();
         state_ = kNoFailSafe;
         break;
       }
 
-      // 位置と速度の推定ができてないならフェイルセーフを発動しない
-      if (!posvel_accurate) {
-        break;
-      }
-
       // フェイルセーフを更新
       if (voltage_too_low) {
-        TOBAS_WARN("Battery fail-safe is activated.");
-        startLand();
-        break;
+        if (failsafe_mission_ready) {
+          TOBAS_WARN("Battery fail-safe is activated.");
+          startLand();
+        }
       }
 
       break;
     }
     case kLand: {
       // 手動操縦が有効になったらフェイルセーフをキャンセル
-      if (is_manual_ctrl_enabled) {
+      if (manual_ctrl_enabled) {
         TOBAS_INFO("Fail-safe was canceled because the manual control was enabled.");
         mission_ac_->async_cancel_all_goals();
         state_ = kNoFailSafe;
@@ -284,6 +287,11 @@ void FailsafeExecutorNode::armingCb(const tobas_msgs::msg::Arming::ConstSharedPt
 void FailsafeExecutorNode::rcInputCb(const tobas_msgs::RCInput::ConstSharedPtr& rcin)
 {
   rcin_ = rcin;
+}
+
+void FailsafeExecutorNode::landedCb(const tobas_msgs::msg::LandedState::ConstSharedPtr& landed)
+{
+  landed_ = landed;
 }
 }  // namespace tobas
 
