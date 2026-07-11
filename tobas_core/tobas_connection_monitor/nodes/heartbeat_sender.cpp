2b4db82e51c12d8faf512e7bebf079ddf718058d
Masayoshi Dohi <m.dohi@tobas.jp>
2026-07-11 11:04:48 +0900

    Heartbeatにシーケンス番号を追加

--- tobas_core/tobas_connection_monitor/nodes/heartbeat_sender.cpp
+++ tobas_core/tobas_connection_monitor/nodes/heartbeat_sender.cpp
@@ -12,8 +12,6 @@ namespace tobas
 {
 class HeartbeatSenderNode : public BaseNode
 {
-  static constexpr auto kPublishPeriod = 1s;
-
   using self = HeartbeatSenderNode;
   using super = BaseNode;
 
@@ -21,6 +19,7 @@ public:
   explicit HeartbeatSenderNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
+  uint64_t sequence_number_ = 0;
   ros2::PublisherPtr<tobas_msgs::msg::Heartbeat> heartbeat_pub_;
   ros2::TimerPtr main_timer_;
 
@@ -31,12 +30,14 @@ HeartbeatSenderNode::HeartbeatSenderNode(const rclcpp::NodeOptions& options)
   : super("heartbeat_sender", nodeOptions_Default(options))
 {
   heartbeat_pub_ = createPublisher<tobas_msgs::msg::Heartbeat>(topic::kHeartbeat);
-  main_timer_ = createTimer(kPublishPeriod, &self::mainTimerCb, this);
+  main_timer_ = createTimer(1s, &self::mainTimerCb, this);
 }
 
 void HeartbeatSenderNode::mainTimerCb()
 {
   auto heartbeat = std::make_unique<tobas_msgs::msg::Heartbeat>();
+  heartbeat->header.stamp = now();
+  heartbeat->sequence_number = sequence_number_++;
   heartbeat_pub_->publish(std::move(heartbeat));
 }
 }  // namespace tobas
