4bf2c2bfa28988140f558b4844c58bdb0f44fb21
Masayoshi Dohi <m.dohi@tobas.jp>
2026-07-23 10:46:24 +0900

    fc1xx -> raspbian

--- tobas_deb/create_deb_raspbian.sh
+++ tobas_deb/create_deb_raspbian.sh
@@ -3,5 +3,5 @@
 set -e
 
 TOBAS_DEB=$(realpath $(dirname "$0"))
-WORKSPACE=$(realpath ${TOBAS_DEB}/fc1xx)
+WORKSPACE=$(realpath ${TOBAS_DEB}/raspbian)
 fakeroot dpkg-deb --build ${WORKSPACE} ${TOBAS_DEB}
